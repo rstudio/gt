953cf2b624c3feaa36d270c1dc269f7c16e06b07
Richard Iannone <riannone@me.com>
2018-03-23 16:10:54 -0400

    Formalize division of main table components

--- R/html_table.R
+++ R/html_table.R
@@ -65,9 +65,32 @@ build_html_table <- function(tbl) {
     dplyr::select_if(
       .predicate = function(x) class(x) != "list")
 
+  # Reorder `rowgroup` and `rowname` columns
+  if ("rowname" %in% colnames(tbl)) {
+    tbl <- tbl %>% dplyr::select(rowname, everything())
+    rowname_available <- TRUE
+  } else {
+    rowname_available <- FALSE
+  }
+
+  if ("rowgroup" %in% colnames(tbl)) {
+    tbl <- tbl %>% dplyr::select(rowgroup, everything())
+    rowgroup_available <- TRUE
+  } else {
+    rowgroup_available <- FALSE
+  }
+
+  # Get the column names for the data columns
+  data_col_names <-
+    colnames(tbl) %>% setdiff(c("rowname", "rowgroup"))
+
+  # Get the column indices for the data columns
+  data_col_indices <-
+    (colnames(tbl) %in% data_col_names) %>% which()
+
   # Obtain a vector of column types from `tbl`
-  tbl_classes <-
-    seq(ncol(tbl)) %>%
+  data_col_classes <-
+    data_col_indices %>%
     purrr::map_chr(
       .f = function(x) tbl[[x]] %>% class())
 
@@ -75,6 +98,8 @@ build_html_table <- function(tbl) {
   # for the `table` component of the table
   table_table <-
     dplyr::tibble(
+      t_part = NA_character_,
+      t_subpart = NA_character_,
       content = NA_character_,
       type = "character",
       row = -3L,
@@ -85,6 +110,8 @@ build_html_table <- function(tbl) {
   # for the `thead` component of the table
   table_thead <-
     dplyr::tibble(
+      t_part = NA_character_,
+      t_subpart = NA_character_,
       content = NA_character_,
       type = "character",
       row = -2L,
@@ -95,6 +122,8 @@ build_html_table <- function(tbl) {
   # for the `tbody` component of the table
   table_tbody <-
     dplyr::tibble(
+      t_part = NA_character_,
+      t_subpart = NA_character_,
       content = NA_character_,
       type = "character",
       row = -1L,
@@ -102,28 +131,28 @@ build_html_table <- function(tbl) {
       column_name = "_tbody_")
 
   # Create a tibble that contains basic metadata
-  # for the table header component of the table
+  # for the boxhead component of the table
   table_heading <-
-    tbl %>%
-    names() %>%
-    dplyr::as_tibble() %>%
-    dplyr::rename(content = value) %>%
+    dplyr::tibble(content = data_col_names) %>%
     dplyr::mutate(type = "character") %>%
     dplyr::mutate(row = 0L) %>%
     dplyr::mutate(column = 1:nrow(.)) %>%
-    dplyr::mutate(column_name = content)
+    dplyr::mutate(column_name = content) %>%
+    dplyr::mutate(t_part = "boxhead") %>%
+    dplyr::mutate(t_subpart = "col_heading") %>%
+    dplyr::select(t_part, t_subpart, everything())
 
   # Create a tibble that contains basic metadata
   # for the table body component of the table
   table_body <-
-    seq(nrow(tbl)) %>% as.integer() %>%
+    seq(nrow(tbl)) %>%
     purrr::map_df(
       .f = function(x) {
-        tbl[x, ] %>% t() %>%
+        tbl[x, data_col_indices] %>% t() %>%
           dplyr::as_tibble() %>%
-          dplyr::mutate(V1 = as.character(V1)) %>%
           dplyr::rename_at(.vars = 1, .funs = function(x) "content") %>%
-          dplyr::mutate(type = tbl_classes) %>%
+          dplyr::mutate(content = as.character(content)) %>%
+          dplyr::mutate(type = data_col_classes) %>%
           dplyr::mutate(row = x) %>%
           dplyr::mutate(column = 1:nrow(.))})
 
@@ -133,7 +162,39 @@ build_html_table <- function(tbl) {
     table_body %>%
     dplyr::inner_join(
       table_heading %>% select(column, column_name),
-      by = "column")
+      by = "column") %>%
+    dplyr::mutate(t_part = "field") %>%
+    dplyr::mutate(t_subpart = NA_character_) %>%
+    dplyr::select(t_part, t_subpart, everything())
+
+  # Create a tibble that contains basic metadata
+  # for the table stub
+  if (rowname_available) {
+
+    table_stub <-
+      dplyr::tibble(
+        t_part = "stub",
+        t_subpart = "row_caption",
+        content = tbl %>% pull(rowname),
+        type = "character",
+        row = 1:nrow(tbl),
+        column = 0L,
+        column_name = NA_character_)
+
+    table_stub_head <-
+      dplyr::tibble(
+        t_part = "stub",
+        t_subpart = "stub_head",
+        content = "",
+        type = "character",
+        row = 0L,
+        column = 0L,
+        column_name = NA_character_)
+
+    table_stub <-
+      dplyr::bind_rows(table_stub_head, table_stub)
+
+  }
 
   # Bind rows from `table_heading` and `table_body`
   html_table <-
@@ -144,8 +205,18 @@ build_html_table <- function(tbl) {
       table_heading,
       table_body)
 
+  # Bind the `table_stub` df if one is available
+  if (exists("table_stub")) {
+
+    html_table <-
+      html_table %>%
+      dplyr::bind_rows(table_stub) %>%
+      dplyr::arrange(row, column)
+  }
+
   # Create the list object for the html table
-  html_table <- list(html_table = html_table)
+  html_table <-
+    list(html_table = html_table)
 
   # Apply the `html_table` class
   attr(html_table, "class") <- "html_table"
