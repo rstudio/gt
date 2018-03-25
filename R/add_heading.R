bd990281ae0d1facd1928fa060a48e5f45399b1c
Richard Iannone <riannone@me.com>
2018-03-25 02:23:12 -0400

    Modify `mutate()` statement

--- R/add_heading.R
+++ R/add_heading.R
@@ -11,7 +11,7 @@
 #' @param table_number a number to use as the
 #' table number.
 #' @return an object of class \code{html_table}.
-#' @importFrom dplyr mutate case_when
+#' @importFrom dplyr mutate
 #' @export
 add_heading <- function(html_tbl,
                         title = NULL,
@@ -19,27 +19,32 @@ add_heading <- function(html_tbl,
                         table_number = NULL) {
 
 
- if (!is.null(title)) {
+  if (!is.null(title)) {
 
-  title <- title %>% as.character()
+    title_str <- title %>% as.character()
 
-  html_tbl[["html_heading"]] <-
-   html_tbl[["html_heading"]] %>%
-   dplyr::mutate(content = case_when(
-    t_subpart == "title" ~ title,
-    is.character(t_subpart) ~ content))
- }
+    html_tbl[["heading"]] <-
+      html_tbl[["heading"]] %>%
+      dplyr::mutate(title = title_str)
+  }
 
- if (!is.null(headnote)) {
+  if (!is.null(headnote)) {
 
-  headnote <- headnote %>% as.character()
+    headnote_str <- headnote %>% as.character()
 
-  html_tbl[["html_heading"]] <-
-   html_tbl[["html_heading"]] %>%
-   dplyr::mutate(content = case_when(
-    t_subpart == "headnote" ~ headnote,
-    is.character(t_subpart) ~ content))
- }
+    html_tbl[["heading"]] <-
+      html_tbl[["heading"]] %>%
+      dplyr::mutate(headnote = headnote_str)
+  }
 
- html_tbl
+  if (!is.null(table_number)) {
+
+    table_number_str <- table_number %>% as.character()
+
+    html_tbl[["heading"]] <-
+      html_tbl[["heading"]] %>%
+      dplyr::mutate(table_number = table_number_str)
+  }
+
+  html_tbl
 }
