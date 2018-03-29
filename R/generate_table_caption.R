4100fbcd83f40ae419bcaa6e555c82708930065f
Richard Iannone <riannone@me.com>
2018-03-29 08:48:06 -0700

    Reorder several sections within function

--- R/generate_table_caption.R
+++ R/generate_table_caption.R
@@ -34,35 +34,35 @@ generate_table_caption <- function(title,
                                    headnote_padding = "3px") {
 
 
- if (!is.null(border_top)) {
-  border_top <-
-   paste(border_top, collapse = " ") %>%
-   as.character() %>%
-   paste0("border-top:", ., ";")
- } else {
-  border_top <- NA_character_
- }
+  if (!is.null(border_top)) {
+    border_top <-
+      paste(border_top, collapse = " ") %>%
+      as.character() %>%
+      paste0("border-top:", ., ";")
+  } else {
+    border_top <- NA_character_
+  }
 
- if (!is.null(border_bottom)) {
-  border_bottom <-
-   paste(border_bottom, collapse = " ") %>%
-   as.character() %>%
-   paste0("border-bottom:", ., ";")
- } else {
-  border_bottom <- NA_character_
- }
+  if (!is.null(border_bottom)) {
+    border_bottom <-
+      paste(border_bottom, collapse = " ") %>%
+      as.character() %>%
+      paste0("border-bottom:", ., ";")
+  } else {
+    border_bottom <- NA_character_
+  }
 
- if (!is.null(title_font_size)) {
-  title_font_size <-
-   paste(title_font_size, collapse = " ") %>%
-   as.character() %>%
-   paste0("font-size:", ., ";")
- } else {
-  title_font_size <- NA_character_
- }
+  if (!is.null(title_font_size)) {
+    title_font_size <-
+      paste(title_font_size, collapse = " ") %>%
+      as.character() %>%
+      paste0("font-size:", ., ";")
+  } else {
+    title_font_size <- NA_character_
+  }
 
- if (!is.null(title_align)) {
-   title_align <-
+  if (!is.null(title_align)) {
+    title_align <-
       paste(title_align, collapse = " ") %>%
       as.character() %>%
       paste0("text-align:", ., ";")
@@ -70,23 +70,23 @@ generate_table_caption <- function(title,
     title_align <- NA_character_
   }
 
- if (!is.null(title_padding)) {
-  title_padding <-
-   paste(title_padding, collapse = " ") %>%
-   as.character() %>%
-   paste0("padding:", ., ";")
- } else {
-  title_padding <- NA_character_
- }
+  if (!is.null(title_padding)) {
+    title_padding <-
+      paste(title_padding, collapse = " ") %>%
+      as.character() %>%
+      paste0("padding:", ., ";")
+  } else {
+    title_padding <- NA_character_
+  }
 
- if (!is.null(headnote_font_size)) {
-  headnote_font_size <-
-   paste(headnote_font_size, collapse = " ") %>%
-   as.character() %>%
-   paste0("font-size:", ., ";")
- } else {
-  headnote_font_size <- NA_character_
- }
+  if (!is.null(headnote_font_size)) {
+    headnote_font_size <-
+      paste(headnote_font_size, collapse = " ") %>%
+      as.character() %>%
+      paste0("font-size:", ., ";")
+  } else {
+    headnote_font_size <- NA_character_
+  }
 
   if (!is.null(headnote_align)) {
     headnote_align <-
@@ -97,72 +97,72 @@ generate_table_caption <- function(title,
     headnote_align <- NA_character_
   }
 
- if (!is.null(headnote_padding)) {
-  headnote_padding <-
-   paste(headnote_padding, collapse = " ") %>%
-   as.character() %>%
-   paste0("padding:", ., ";")
- } else {
-  headnote_padding <- NA_character_
- }
-
- # Generate title <caption> statement
- if (is.null(headnote)) {
-
-  if (!all(is.na(c(border_top, border_bottom, title_font_size, title_align, title_padding)))) {
-   caption <-
-    paste(
-     c(border_top, border_bottom, title_font_size, title_align, title_padding)[
-      !is.na(c(border_top, border_bottom, title_font_size, title_align, title_padding))],
-     collapse = "") %>%
-    paste0("<caption style=\"", ., "\">", title, "</caption>\n")
+  if (!is.null(headnote_padding)) {
+    headnote_padding <-
+      paste(headnote_padding, collapse = " ") %>%
+      as.character() %>%
+      paste0("padding:", ., ";")
   } else {
-   caption <- paste0("<caption>", title, "</caption>\n")
+    headnote_padding <- NA_character_
   }
- }
 
- if (!is.null(headnote) || headnote != "") {
+  # Generate title <caption> statement
+  if (is.null(headnote)) {
 
-  if (!all(is.na(c(border_top, title_font_size, title_align, title_padding)))) {
-
-   caption <-
-    paste(
-     c(border_top, title_font_size, title_align, title_padding)[
-      !is.na(c(border_top, title_font_size, title_align, title_padding))],
-     collapse = "") %>%
-    paste0("<caption style=\"", ., "\">", title, "</caption>\n")
-
-  } else {
-
-   caption <- paste0("<caption>", title, "</caption>\n")
-
+    if (!all(is.na(c(border_top, border_bottom, title_font_size, title_align, title_padding)))) {
+      caption <-
+        paste(
+          c(border_top, border_bottom, title_font_size, title_align, title_padding)[
+            !is.na(c(border_top, border_bottom, title_font_size, title_align, title_padding))],
+          collapse = "") %>%
+        paste0("<caption style=\"", ., "\">", title, "</caption>\n")
+    } else {
+      caption <- paste0("<caption>", title, "</caption>\n")
+    }
   }
- }
 
- # Generate headnote <caption> statement
- if (!is.null(headnote)) {
+  if (!is.null(headnote) || headnote != "") {
 
-  if (!all(is.na(c(border_bottom, headnote_font_size, headnote_padding)))) {
+    if (!all(is.na(c(border_top, title_font_size, title_align, title_padding)))) {
 
-   caption <-
-    paste(
-     caption,
-     paste(
-      c(border_bottom, headnote_font_size, headnote_align, headnote_padding)[
-       !is.na(c(border_bottom, headnote_font_size, headnote_align, headnote_padding))],
-      collapse = "") %>%
-      paste0("<caption style=\"", ., "\">", headnote, "</caption>\n"),
-     collapse = "")
+      caption <-
+        paste(
+          c(border_top, title_font_size, title_align, title_padding)[
+            !is.na(c(border_top, title_font_size, title_align, title_padding))],
+          collapse = "") %>%
+        paste0("<caption style=\"", ., "\">", title, "</caption>\n")
 
-  } else {
+    } else {
 
-   caption <-
-    paste(
-     caption,
-     paste0("<caption>", headnote, "</caption>\n"),
-     collapse = "")
+      caption <- paste0("<caption>", title, "</caption>\n")
+
+    }
   }
- }
 
- caption
+  # Generate headnote <caption> statement
+  if (!is.null(headnote)) {
+
+    if (!all(is.na(c(border_bottom, headnote_font_size, headnote_padding)))) {
+
+      caption <-
+        paste(
+          caption,
+          paste(
+            c(border_bottom, headnote_font_size, headnote_align, headnote_padding)[
+              !is.na(c(border_bottom, headnote_font_size, headnote_align, headnote_padding))],
+            collapse = "") %>%
+            paste0("<caption style=\"", ., "\">", headnote, "</caption>\n"),
+          collapse = "")
+
+    } else {
+
+      caption <-
+        paste(
+          caption,
+          paste0("<caption>", headnote, "</caption>\n"),
+          collapse = "")
+    }
+  }
+
+  caption
 }
