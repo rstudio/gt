f7c6be500d8d87dd5b7464b94b4f4320cc45334c
Richard Iannone <riannone@me.com>
2018-03-29 17:18:23 -0700

    Add internal HTML table transform functions

---
+++ R/html_table_transforms.R
@@ -0,0 +1,83 @@
+#' HTT: apply_alignment_left()
+#' @param html_tbl an HTML table object that is
+#' created using the \code{gt()} function.
+#' @return an object of class \code{html_table}.
+#' @noRd
+htt_alignment_left <- function(html_tbl,
+                               transform) {
+
+  if (is.na(transform)) {
+
+    html_tbl <-
+      html_tbl %>%
+      add_column_style(
+        property = "text-align",
+        values = "left")
+
+    return(html_tbl)
+  }
+}
+
+#' HTT: apply_alignment_center()
+#' @param html_tbl an HTML table object that is
+#' created using the \code{gt()} function.
+#' @return an object of class \code{html_table}.
+#' @noRd
+htt_alignment_center <- function(html_tbl,
+                                 transform) {
+
+  if (is.na(transform)) {
+
+    html_tbl <-
+      html_tbl %>%
+      add_column_style(
+        property = "text-align",
+        values = "center")
+
+    return(html_tbl)
+  }
+}
+
+
+#' HTT: apply_theme_striped()
+#' @param html_tbl an HTML table object that is
+#' created using the \code{gt()} function.
+#' @return an object of class \code{html_table}.
+#' @noRd
+htt_theme_striped <- function(html_tbl) {
+
+  html_tbl %>%
+    add_table_style(
+      property = "border-collapse",
+      values = "collapse") %>%
+    add_table_style(
+      property = "width",
+      values = "100%") %>%
+    add_style_to_row_0(
+      property = "border-bottom",
+      values = "2px solid #A8A8A8") %>%
+    add_style_to_row_0(
+      property = "border-top",
+      values = "2px solid #A8A8A8") %>%
+    add_style_to_row_0(
+      property = "padding",
+      values = "10px") %>%
+    add_style_to_row_0(
+      property = "margin",
+      values = "10px") %>%
+    add_style_to_rows(
+      property = "padding",
+      values = "10px") %>%
+    add_style_to_rows(
+      property = "padding",
+      values = "10px") %>%
+    add_style_to_row_n(
+      property = "border-bottom",
+      values = "2px solid #A8A8A8") %>%
+    add_style_every_n_rows(
+      every_n = 2,
+      property = "background",
+      values = "#E5E6EB")
+}
+
+
