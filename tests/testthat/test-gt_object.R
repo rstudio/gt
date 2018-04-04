713122491b6f5aac7b320f94639a4d68b67bc4ea
Richard Iannone <riannone@me.com>
2018-04-03 22:48:22 -0700

    Modify tests to reflect change in main object

--- tests/testthat/test-gt_object.R
+++ tests/testthat/test-gt_object.R
@@ -8,15 +8,16 @@ test_that("an HTML table object contains the correct components", {
     tibble(a = 1:5, b = letters[1:5]) %>%
     gt()
 
-  # Expect that the list object has 9 components
+  # Expect that the list object has 11 components
   expect_equal(
-    length(table_object), 9)
+    length(table_object), 11)
 
   # Expect certain named components
   expect_equal(
     table_object %>% names(),
-    c("source_tbl",
-      "transform_opts", "heading", "footnote", "source_note", "stubhead_caption",
+    c("transform_directives", "aesthetics",
+      "source_tbl", "modified_tbl",
+      "heading", "footnote", "source_note", "stubhead_caption",
       "boxhead_panel",
       "html_head", "html_table"))
 })
