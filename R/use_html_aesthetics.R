e531afcbd95acf2d35c935ea6a27dbebd5b0b281
Richard Iannone <riannone@me.com>
2018-04-05 23:32:09 -0700

    Move set of stmts into separate function

---
+++ R/use_html_aesthetics.R
@@ -0,0 +1,78 @@
+#' Apply the aesthetics available in \code{aesthetics_tbl}
+#' to the \code{html_table}.
+#' @param html_tbl the internal table called
+#' \code{html_table}.
+#' @param aesthetics_tbl the internal table called
+#' \code{aesthetics}.
+#' @importFrom dplyr filter pull
+#' @noRd
+use_html_aesthetics <- function(html_tbl, aesthetics_tbl) {
+
+  # Apply theme if any theme setting is applied
+  if ("theme" %in% aesthetics_tbl$type) {
+
+    theme_name <-
+      aesthetics_tbl %>%
+      dplyr::filter(type == "theme") %>%
+      dplyr::pull(options)
+
+    if (theme_name[1] == "striped") {
+      html_tbl <- htt_theme_striped(html_tbl = html_tbl)
+    }
+
+    if (theme_name[1] == "minimal") {
+      html_tbl <- htt_theme_minimal(html_tbl = html_tbl)
+    }
+
+    if (theme_name[1] == "classical") {
+      html_tbl <- htt_theme_classical(html_tbl = html_tbl)
+    }
+  }
+
+  # Apply center alignment if any such transformations are applied
+  if ("alignment_center" %in% aesthetics_tbl$type) {
+
+    options <-
+      aesthetics_tbl %>%
+      dplyr::filter(type == "alignment_center") %>%
+      dplyr::pull(options)
+
+    html_tbl <-
+      htt_alignment(
+        html_tbl = html_tbl,
+        transform = options[1],
+        align_type = "center")
+  }
+
+  # Apply left alignment if any such transformations are applied
+  if ("alignment_left" %in% aesthetics_tbl$type) {
+
+    options <-
+      aesthetics_tbl %>%
+      dplyr::filter(type == "alignment_left") %>%
+      dplyr::pull(options)
+
+    html_tbl <-
+      htt_alignment(
+        html_tbl = html_tbl,
+        transform = options[1],
+        align_type = "left")
+  }
+
+  # Apply right alignment if any such transformations are applied
+  if ("alignment_right" %in% aesthetics_tbl$type) {
+
+    options <-
+      aesthetics_tbl %>%
+      dplyr::filter(type == "alignment_right") %>%
+      dplyr::pull(options)
+
+    html_tbl <-
+      htt_alignment(
+        html_tbl = html_tbl,
+        transform = options[1],
+        align_type = "right")
+  }
+
+  html_tbl
+}
