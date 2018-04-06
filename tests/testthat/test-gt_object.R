eaa43041664b15d0a45bb7341525a0009bd54fbc
Richard Iannone <riannone@me.com>
2018-04-05 23:35:35 -0700

    Modify tests to reflect changed object

--- tests/testthat/test-gt_object.R
+++ tests/testthat/test-gt_object.R
@@ -3,21 +3,19 @@ context("test-gt_object.R")
 test_that("an HTML table object contains the correct components", {
 
   # Create an HTML table object with `gt()`
-  # and the `iris` dataset
   table_object <-
     tibble(a = 1:5, b = letters[1:5]) %>%
     gt()
 
-  # Expect that the list object has 11 components
+  # Expect that the list object has 13 components
   expect_equal(
-    length(table_object), 11)
+    length(table_object), 13)
 
   # Expect certain named components
   expect_equal(
     table_object %>% names(),
-    c("transform_directives", "aesthetics",
-      "source_tbl", "modified_tbl",
-      "heading", "footnote", "source_note", "stubhead_caption",
-      "boxhead_panel",
+    c("transforms", "formats", "aesthetics",
+      "source_tbl", "modified_tbl", "content_tbl",
+      "heading", "footnote", "source_note", "stubhead_caption", "boxhead_panel",
       "html_head", "html_table"))
 })
