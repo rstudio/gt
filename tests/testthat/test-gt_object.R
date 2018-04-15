fa90f9708c43921941138499c5d7a4ad773ff457
Richard Iannone <riannone@me.com>
2018-04-15 02:03:39 -0700

    Modify test to reflect new object properties

--- tests/testthat/test-gt_object.R
+++ tests/testthat/test-gt_object.R
@@ -4,12 +4,12 @@ test_that("an HTML table object contains the correct components", {
 
   # Create an HTML table object with `gt()`
   table_object <-
-    tibble(a = 1:5, b = letters[1:5]) %>%
+    dplyr::tibble(a = 1:5, b = letters[1:5]) %>%
     gt()
 
-  # Expect that the list object has 13 components
+  # Expect that the list object has 14 components
   expect_equal(
-    length(table_object), 13)
+    length(table_object), 14)
 
   # Expect certain named components
   expect_equal(
@@ -17,5 +17,5 @@ test_that("an HTML table object contains the correct components", {
     c("transforms", "formats", "aesthetics",
       "source_tbl", "modified_tbl", "content_tbl",
       "heading", "footnote", "source_note", "stubhead_caption", "boxhead_panel",
-      "html_head", "html_table"))
+      "html_head", "html_table", "html_tbl_text"))
 })
