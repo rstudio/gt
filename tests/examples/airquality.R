6eb9e5b35bafe8ac67044247e54fef66ac8b6771
Richard Iannone <riannone@me.com>
2018-04-16 16:50:06 -0700

    Modify .R example scripts

--- tests/examples/airquality.R
+++ tests/examples/airquality.R
@@ -6,15 +6,17 @@ library(tidyverse)
 
 # 1 - we take the `datasets::airquality` data.frame
 # 2 - the 'minimal' theme is applied
-# 3 - all column values are right aligned
-# 4 - we are formatting numbers in the `Wind` column to have 1 decimal place
+# 3 - apply the Helvetica font to all columns
+# 4 - all column values are right aligned
+# 5 - we are formatting numbers in the `Wind` column to have 1 decimal place
 airquality_tbl <-
   gt(tbl = airquality) %>%  # 1
   apply_theme_minimal() %>%  # 2
-  apply_alignment_right() %>%  # 3
+  apply_font(font = "Helvetica") %>%  # 3
+  apply_alignment_right() %>%  # 4
   format_as_number(
     columns = "Wind",
-    decimals = 1)  # 4
+    decimals = 1)  # 5
 
 # Display the table in the Viewer
 airquality_tbl
