73582590c992247a30eee8fdc7a51d3f3605ee9f
Richard Iannone <riannone@me.com>
2018-04-16 13:06:10 -0700

    Revise print to handle processing steps

--- R/print.R
+++ R/print.R
@@ -1,15 +1,45 @@
 #' Print the table
-#' @description This function will provide a
-#' print out of the HTML table.
-#' @param x an object of class
-#' \code{html_table}.
+#' @description This facilitates printing of the
+#' HTML table to the R console.
+#' @param x an object of class \code{html_table}.
 #' @keywords internal
 #' @export
+print.html_table <- function(x, ..., view = interactive()) {
 
-print.html_table <- function(x, ...) {
+  # [1] Creation of the content table
+  x[["content_tbl"]] <-
+    create_content_tbl(tbl = x[["modified_tbl"]])
 
-  args <- list(...)
-  args <- NULL
+  # [2] Modification of the content table
+  x <-
+    all_tbl_format_steps(
+      html_tbl = x)
 
-  x[["html_tbl_text"]]
+  # [3] Processing of the content table
+  x[["content_tbl"]] <-
+    process_content_tbl(
+      tbl = x[["content_tbl"]])
+
+  # [4] Creation of the HTML table
+  x[["html_table"]] <-
+    create_html_table_tbl(
+      tbl = x[["modified_tbl"]])
+
+  # [5] Join in formatted content
+  x[["html_table"]] <-
+    use_html_content(
+      html_tbl = x[["html_table"]],
+      content_tbl = x[["content_tbl"]])
+
+  # [6] Apply HTML aesthetics
+  x <-
+    use_html_aesthetics(
+      html_tbl = x,
+      aesthetics_tbl = x[["aesthetics"]])
+
+  # [7] Generatation of table HTML
+  html_output <- gt:::create_html(html_tbl = x)
+
+  # Use `print()` to print to the console
+  print(html_output, browse = view, ...)
 }
