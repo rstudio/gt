1e984843ed903399c0bb88cfafc9debda63a1b9e
Richard Iannone <riannone@me.com>
2018-04-16 14:02:51 -0700

    Modify example .R script

--- tests/examples/mtcars.R
+++ tests/examples/mtcars.R
@@ -4,23 +4,24 @@ library(tidyverse)
 # Create a presentation table based on `mtcars`
 # Motor Trend Car Road Tests
 
-# 1 - we take a portion of `datasets::mtcars`
+# 1 - we take the `datasets::mtcars` data.frame
 # 2 - the 'striped' theme is applied
-# 3 - we are formatting numbers in the `mpg` column to have 1 decimal place
-# 4 - we are formatting numbers in the `cyl` column to have 2 decimal places
+# 3 - we are formatting numbers in 4 columns to have 1 decimal place
+# 4 - we are formatting numbers in the `wt` column to have 3 decimal places
 # 5 - all `numeric` values are right aligned
-# 6 - the dataset has rownames, they are moved to the stub; we supply a stubhead caption
+# 6 - the dataset has rownames so they've been moved to the stub;
+#     here, we supply a stubhead caption
 # 7 - a heading is added
 # 8,9 - two source notes are added
 mtcars_tbl <-
-  gt(tbl = mtcars[1:6, 1:3]) %>%  # 1
+  gt(tbl = mtcars) %>%  # 1
   apply_theme_striped() %>%  # 2
   format_as_number(
-    columns = "mpg",
+    columns = c("mpg", "disp", "drat", "qsec"),
     decimals = 1) %>%  # 3
   format_as_number(
-    columns = "cyl",
-    decimals = 2) %>%  # 4
+    columns = "wt",
+    decimals = 3) %>%  # 4
   apply_alignment_right(types = "numeric") %>%  # 5
   add_stubhead_caption(caption = "car model") %>%  # 6
   add_heading(
@@ -33,8 +34,5 @@ mtcars_tbl <-
     source_note = "Motor Trend Magazine (1974).",
     lead_in = "Original Data: ")  # 9
 
-# Render the table in the Viewer
-mtcars_tbl %>% render_table()
-
-# Render with the different table parts highlighted
-# mtcars_tbl %>% highlight_tbl_parts()
+# Display the table in the Viewer
+mtcars_tbl
