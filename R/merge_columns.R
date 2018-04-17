a7ec53a66d112ee085df963d1a3c12611c397a86
Richard Iannone <riannone@me.com>
2018-04-17 00:21:35 -0700

    Add function 'merge_columns'

---
+++ R/merge_columns.R
@@ -0,0 +1,50 @@
+#' Merge columns according to transform directives
+#' @param html_tbl an HTML table object that
+#' is created using the \code{gt()} function.
+#' @importFrom dplyr filter bind_rows arrange mutate pull select arrange
+#' @importFrom rlang UQ
+#' @export
+merge_columns <- function(html_tbl) {
+
+  if ("add_uncertainty" %in% html_tbl[["transforms"]]$transform_type) {
+
+    add_uncert_transforms <-
+      html_tbl[["transforms"]] %>%
+      dplyr::filter(transform_type == "add_uncertainty")
+
+    value_column <- add_uncert_transforms$transform_v1
+    uncert_column <- add_uncert_transforms$transform_v2
+
+    for (i in seq(value_column)) {
+
+      html_tbl[["html_table"]] <-
+        dplyr::bind_rows(
+          html_tbl[["html_table"]] %>%
+            dplyr::filter(
+              column_name == rlang::UQ(value_column[i]) &
+                t_part == "field") %>%
+            dplyr::arrange(row) %>%
+            dplyr::mutate(
+              content_2 =
+                html_tbl[["html_table"]] %>%
+                dplyr::filter(
+                  column_name == rlang::UQ(uncert_column[i]) &
+                    t_part == "field") %>%
+                dplyr::pull(content)) %>%
+            dplyr::mutate(content = paste(content, "&#177;", content_2)) %>%
+            dplyr::select(-content_2),
+          html_tbl[["html_table"]] %>%
+            dplyr::filter(
+              column_name == rlang::UQ(value_column[i]) &
+                t_part != "field"),
+          html_tbl[["html_table"]] %>%
+            dplyr::filter(
+              column_name != rlang::UQ(value_column[i]) &
+                column_name != rlang::UQ(uncert_column[i]))) %>%
+        dplyr::arrange(row, column)
+    }
+  }
+
+  html_tbl
+}
+
