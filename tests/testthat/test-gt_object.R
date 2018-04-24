3eff8d46f05b173586b33a1cb167ed4257ff433d
Richard Iannone <riannone@me.com>
2018-04-23 19:08:47 -0700

    Modify tests to account for modified object

--- tests/testthat/test-gt_object.R
+++ tests/testthat/test-gt_object.R
@@ -7,15 +7,15 @@ test_that("an HTML table object contains the correct components", {
     dplyr::tibble(a = 1:5, b = letters[1:5]) %>%
     gt()
 
-  # Expect that the list object has 14 components
+  # Expect that the list object has 13 components
   expect_equal(
-    length(table_object), 12)
+    length(table_object), 13)
 
   # Expect certain named components
   expect_equal(
     table_object %>% names(),
     c("transforms", "formats", "aesthetics",
-      "source_tbl", "modified_tbl", "content_tbl",
+      "source_tbl", "modified_tbl", "summary_tbl", "content_tbl",
       "heading", "footnote", "source_note", "stubhead_caption", "boxhead_panel",
       "html_head"))
 })
