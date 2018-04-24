f056af42784ce9d0d6d1c89a7c7668142fbe95f9
Richard Iannone <riannone@me.com>
2018-04-23 19:16:06 -0700

    Add `summary_lines` .R example script

---
+++ tests/examples/summary_lines.R
@@ -0,0 +1,36 @@
+library(gt)
+library(tidyverse)
+
+# Create a table that can be summarized
+tbl <-
+  tibble::tribble(
+    ~date,        ~value_1,   ~value_2,
+    "2018-02-10",     20.4,      361.1,
+    "2018-02-11",     25.8,      184.3,
+    "2018-02-10",     16.3,      342.3,
+    "2018-02-11",     28.3,      234.9,
+    "2018-02-11",     75.5,      190.9,
+    "2018-02-10",     10.9,      743.3,
+    "2018-02-11",     63.1,      252.3,
+    "2018-02-10",     34.6,      344.7,
+    "2018-02-11",     76.2,      197.2,
+    "2018-02-11",     55.3,      284.6)
+
+# Create a table
+summary_tbl <-
+  gt(tbl = tbl) %>%
+  create_summary_lines(
+    groups = "date",
+    columns = "value_1",
+    fcn = "mean",
+    summary_labels = c("mean_1", "mean_2")) %>%
+  apply_theme_striped() %>%
+  apply_alignment_right(types = "numeric") %>%
+  apply_alignment_left(types = "character") %>%
+  format_as_number(
+    columns = c("value_1", "value_2"),
+    decimals = 1) %>%
+  add_stubhead_caption(caption = "statistic")
+
+# Display the table in the Viewer
+summary_tbl
