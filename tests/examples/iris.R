4523a93a4e5344c4674bc21c9e680e58c991c6cc
Richard Iannone <riannone@me.com>
2018-04-23 23:10:35 -0700

    Modify .R script `iris` example

--- tests/examples/iris.R
+++ tests/examples/iris.R
@@ -4,30 +4,31 @@ library(tidyverse)
 # Create a presentation table based on `iris`
 # Edgar Anderson's Iris Data
 
-# 1 - we take the `datasets::iris` data.frame and move `Species` to front during ingest
-# 2 - the 'striped' theme is applied
-# 3 - we are taking the column naming format `[heading].[caption]` and
+# 1 - we take the `datasets::iris` data.frame
+# 2 - the `Species` column is moved to the start of the column series
+# 3 - the 'striped' theme is applied
+# 4 - we are taking the column naming format `[heading].[caption]` and
 #     automatically creating spanner headings to add structure
-# 4 - all column values are center aligned
-# 5 - we are formatting numbers in the named columns to have 1 decimal place
-# 6 - a heading is added
-# 7 - a source note is added
+# 5 - all column values are center aligned
+# 6 - we are formatting numbers in the named columns to have 1 decimal place
+# 7 - a heading is added
+# 8 - a source note is added
 iris_tbl <-
-  gt(tbl = iris %>%
-       dplyr::select(Species, everything())) %>%  # 1
-  apply_theme_striped() %>%  # 2
-  apply_spanner_headings() %>%  # 3
-  apply_alignment_center() %>%  # 4
+  gt(tbl = iris) %>%  # 1
+  move_columns_to_start(columns = "Species") %>%  # 2
+  apply_theme_striped() %>%  # 3
+  apply_spanner_headings(use_names = TRUE) %>%  # 4
+  apply_alignment_center() %>%  # 5
   format_as_number(
     columns = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"),
-    decimals = 1) %>%  # 5
+    decimals = 1) %>%  # 6
   add_heading(
     title = "The `iris` dataset",
     headnote = "[A rather famous dataset about Iris setosa, versicolor, and virginica]",
-    table_number = 1) %>%  # 6
+    table_number = 1) %>%  # 7
   add_source_note(
     source_note = "Anderson, Edgar (1935).",
-    lead_in = "The data were collected by ")  # 7
+    lead_in = "The data were collected by ")  # 8
 
 # Display the table in the Viewer
 iris_tbl
