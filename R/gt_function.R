7770299219057705ad9876745b95366f0615ad7a
Richard Iannone <riannone@me.com>
2018-04-23 19:04:48 -0700

    Add `summary_tbl` list component

--- R/gt_function.R
+++ R/gt_function.R
@@ -119,6 +119,7 @@ gt <- function(tbl) {
       aesthetics = aesthetics,
       source_tbl = tbl,
       modified_tbl = NULL,
+      summary_tbl = NULL,
       content_tbl = NULL,
       heading = heading,
       footnote = footnote,
