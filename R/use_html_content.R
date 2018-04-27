7c45326889d3301e82b9831a0b102190d0715bc4
Richard Iannone <riannone@me.com>
2018-04-27 02:22:52 -0700

    Modify type of join and add `filter()` stmt

--- R/use_html_content.R
+++ R/use_html_content.R
@@ -4,20 +4,28 @@
 #' \code{html_table}.
 #' @param content_tbl the internal table called
 #' \code{content_tbl}.
-#' @importFrom dplyr bind_rows filter inner_join select rename arrange
+#' @importFrom dplyr bind_rows filter right_join select rename arrange distinct
 #' @noRd
-use_html_content <- function(html_tbl, content_tbl) {
+use_html_content <- function(html_tbl,
+                             content_tbl) {
 
   dplyr::bind_rows(
     html_tbl %>%
-      dplyr::filter(t_part %in% c("boxhead", "stub")),
-    dplyr::inner_join(
+      dplyr::filter(t_part == "stub"),
+    html_tbl %>%
+      dplyr::filter(!(row %in% (content_tbl$row %>% unique()))),
+    dplyr::right_join(
       html_tbl %>%
-        dplyr::filter(t_part != "boxhead") %>%
+        dplyr::filter(row %in% (content_tbl$row %>% unique())) %>%
+        dplyr::filter(column %in% (content_tbl$col %>% unique())) %>%
         dplyr::select(-content),
       content_tbl %>%
         dplyr::select(row, column_name, content_formatted) %>%
         dplyr::rename(content = content_formatted),
       by = c("row", "column_name"))) %>%
-    dplyr::arrange(row, column)
+    dplyr::distinct() %>%
+    dplyr::arrange(row, column) %>%
+    dplyr::filter(!is.na(t_part))
 }
+
+
