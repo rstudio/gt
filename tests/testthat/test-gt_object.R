dc2938f7e621b456aa891576b2a3c243c4f3089f
Richard Iannone <riannone@me.com>
2018-05-05 21:47:43 -0700

    Modify tests to account for reconfigured object

--- tests/testthat/test-gt_object.R
+++ tests/testthat/test-gt_object.R
@@ -7,15 +7,16 @@ test_that("an HTML table object contains the correct components", {
     dplyr::tibble(a = 1:5, b = letters[1:5]) %>%
     tab_create()
 
-  # Expect that the list object has 13 components
+  # Expect that the list object has 14 components
   expect_equal(
-    length(table_object), 13)
+    length(table_object), 14)
 
   # Expect certain named components
   expect_equal(
     table_object %>% names(),
     c("transforms", "formats", "aesthetics", "fonts",
       "source_tbl", "modified_tbl", "content_tbl",
-      "heading", "footnote", "source_note", "stubhead_caption", "boxhead_panel",
+      "heading", "footnote", "source_note",
+      "stubhead_caption", "boxhead_panel", "stub_block",
       "html_head"))
 })
