3da0deedd8aad6bf8ab9a67bb2f0b582fc9ea79d
Richard Iannone <riannone@me.com>
2018-05-08 08:54:57 -0700

    Modify roxygen documentation

--- R/cell_selection_helpers.R
+++ R/cell_selection_helpers.R
@@ -1,7 +1,10 @@
-#' Define a table cell
-#' @param row an integer representing the table
-#' row to target (as part of the \code{row}-\code{column}
-#' dyad).
+#' Define a single table cell
+#' @param row an integer representing the
+#' table row to target (as part of the
+#' \code{row}-\code{column} dyad).
+#' @param column an integer representing the
+#' table column to target (as part of the
+#' \code{row}-\code{column} dyad).
 #' @export
 cell <- function(row, column) {
 
