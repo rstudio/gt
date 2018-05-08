1e238c8493252a760faab27e6197556dad23e594
Richard Iannone <riannone@me.com>
2018-05-08 11:59:35 -0700

    Rename helper function: `cell()` -> `cells()`

--- R/helpers_cells.R
+++ R/helpers_cells.R
@@ -1,12 +1,19 @@
-#' Define a single table cell
+#' Define one or more table cells
 #' @param row an integer representing the
 #' table row to target (as part of the
 #' \code{row}-\code{column} dyad).
 #' @param column an integer representing the
 #' table column to target (as part of the
 #' \code{row}-\code{column} dyad).
+#' @return a list object of class \code{helper_cells}
 #' @export
-cell <- function(row, column) {
+cells <- function(row, column) {
 
-  list(row = row, column = column)
+  # Create the list object
+  cells <- list(row = row, column = column)
+
+  # Apply the `helper_cells` class
+  attr(cells, "class") <- "helper_cells"
+
+  cells
 }
