b61ba17414b794278b5a3ce2aa63a0cf4149c236
Richard Iannone <riannone@me.com>
2018-05-08 09:49:57 -0700

    Modify roxygen documentation

--- R/helpers_rownames.R
+++ R/helpers_rownames.R
@@ -1,3 +1,6 @@
+#' Obtain rownames that fit a naming pattern
+#' @param pattern a matching pattern for
+#' targeting rownames.
 #' @importFrom stringr str_match
 #' @export
 rownames_with <- function(pattern) {
