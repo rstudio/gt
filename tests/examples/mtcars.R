965c14ff2e192458c0ca3fc6a805f71b292fb37d
Richard Iannone <riannone@me.com>
2018-05-14 11:55:09 -0700

    Update `mtcars.R`

--- tests/examples/mtcars.R
+++ tests/examples/mtcars.R
@@ -10,36 +10,79 @@ library(tidyverse)
 # 4 - we are formatting numbers in the `wt` column to have 3 decimal places
 # 5 - all `numeric` values are right aligned
 # 6 - all `character` values are left aligned
-# 7 - the dataset has rownames so they've been moved to the stub;
-#     here, we supply a stubhead caption
-# 8 - a heading is added
-# 9,10 - two source notes are added
-mtcars_tbl <-
+# 7 - we create stub blocks, grouping together rows into separate
+#     blocks of rows with stub headings
+# 8 - we supply a stubhead caption
+# 9 - a heading is added
+# 10 - two source notes are added
+# 11 - we add footnotes that target several cell values
+# 12 - renaming of column headers
+html_tbl <-
   tab_create(tbl = mtcars) %>%  # 1
-  theme_striped() %>%  # 2
+  theme_horizontal() %>%  # 2
   fmt_number(
     columns = c("mpg", "disp", "drat", "qsec"),
     decimals = 1
-    ) %>%  # 3
+  ) %>%  # 3
   fmt_number(
     columns = "wt",
     decimals = 3
-    ) %>%  # 4
-  cols_align_right(types = "numeric") %>%  # 5
+  ) %>%  # 4
   cols_align_left(types = "character") %>%  # 6
-  tab_stubhead_caption(caption = "car model") %>%  # 7
+  tab_stub_block(
+    "Mazdas" = rownames_with("Mazda"),
+    "Mercs" = rownames_with("Merc"),
+    "Hornets" = rownames_with("Hornet"),
+    "Toyotas" = rownames_with("Toyota"),
+    "Supercars" = c(
+      "Ferrari Dino","Maserati Bora",
+      "Porsche 914-2", "Ford Pantera L",
+      "Camaro Z28")
+  ) %>% # 7
+  tab_stubhead_caption(caption = "car model") %>%  # 8
   tab_heading(
     title = "Excerpt from the **mtcars** dataset",
     headnote = "[A rather famous *Motor Trend* table]"
-    ) %>%  # 8
+  ) %>%  # 9
   tab_source_note(
-    source_note = "Henderson and Velleman (1981).",
-    lead_in = "Main Source of Data: "
-    ) %>%  # 9
+    source_note = "Main Source of Data: *Henderson and Velleman* (1981).",
+  ) %>%  # 10
   tab_source_note(
-    source_note = "Motor Trend Magazine (1974).",
-    lead_in = "Original Data: "
-    )  # 10
+    source_note = "Original Data: *Motor Trend Magazine* (1974).",
+  ) %>%  # 10
+  tab_footnote(
+    "Examples of poor gas mileage." =
+      cells(row = c(22, 23), column = 2)
+  ) %>%  # 11
+  tab_footnote(
+    "Really fast quarter mile" =
+      c(18, 8)
+  ) %>%  # 11
+  tab_footnote(
+    "Excellent gas mileage." =
+      cells(row = 12, column = 2)
+  ) %>%  # 11
+  tab_footnote(
+    "Worst speed ever." =
+      cells(row = 4, column = 8)
+  ) %>%  # 11
+  tab_footnote(
+    "Massive **hp**." =
+      cells(row = 18, column = 5)
+  ) %>% # 11
+  cols_rename(
+    "*MPG*" = "mpg",
+    "C" = "cyl",
+    "D" = "disp",
+    "HP" = "hp",
+    "DR" = "drat",
+    "Wt." = "wt",
+    "QMT" = "qsec",
+    "VS" = "vs",
+    "AM" = "am",
+    "Gears" = "gear",
+    "Cs" = "carb") # 12
 
 # Display the table in the Viewer
-mtcars_tbl
+html_tbl
+
