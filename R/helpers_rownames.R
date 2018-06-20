4f7b66be63ceb3ad3242a94222aca3722b50a940
Richard Iannone <riannone@me.com>
2018-06-20 02:23:49 -0400

    Redesign rowname helper as matching fcn set

--- R/helpers_rownames.R
+++ R/helpers_rownames.R
@@ -1,17 +1,34 @@
-#' Obtain rownames that fit a naming pattern
+#' Provide a directive to get rows that fit a
+#' naming pattern
 #' @param pattern a matching pattern for
 #' targeting rownames.
-#' @importFrom stringr str_match
 #' @export
 rownames_with <- function(pattern) {
 
-  # Create bindings for specific variables
-  #rowname <- NULL
+  paste0("::rownames_with::", pattern)
+}
 
+#' Get rownames that fit a naming pattern
+#' @param html_tbl an HTML table object that is
+#' created using the \code{tab_create()} function.
+#' @param directive a string-based directive supplied
+#' by the \code{rownames_with} helper function.
+#' @importFrom stringr str_match
+#' @importFrom dplyr pull
+#' @noRd
+get_rownames_with <- function(html_tbl, directive) {
+
+  # Create bindings for specific variables
+  rowname <- NULL
+
+  # Extract the matching pattern
+  pattern <- gsub("::rownames_with::", "", directive)
+
+  # Get the matching rows
   matching_rows <-
     !is.na(
       stringr::str_match(
-        string = rowname,
+        string = html_tbl[["modified_tbl"]] %>% dplyr::pull(rowname),
         pattern = pattern) %>%
         as.character())
 
