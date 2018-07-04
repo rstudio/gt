0d17cb8703ae5ddfa896a9152d673e0b14c4e615
Richard Iannone <riannone@me.com>
2018-07-04 01:47:35 -0700

    Reorganize functions in files

--- R/print.R
+++ R/print.R
@@ -1,53 +1,25 @@
 #' Print the table
 #' @description This facilitates printing of the
 #' HTML table to the R console.
-#' @param x an object of class \code{html_table}.
+#' @param x an object of class \code{gt_tbl}.
 #' @keywords internal
-#' @export
-print.html_table <- function(x, ..., view = interactive()) {
+#' @importFrom htmltools htmlDependency attachDependencies HTML
+print.gt_tbl <- function(x, ..., view = interactive()) {
 
-  # [0.5] Creation of stub block groups
-  x <-
-    create_stub_block_groups(html_tbl = x)
+  # Generation of the HTML table
+  html_table <- process_html(tbl = x)
 
-  # [1] Creation of the content table
-  x <-
-    create_content_tbl(html_tbl = x)
+  # Create the htmlDependency
+  dep <- htmltools::htmlDependency(
+    name = 'gt',
+    version = '0.1',
+    src = system_file(file = 'css'),
+    stylesheet = 'gt.css')
 
-  # [2] Modification of the content table
-  x <-
-    all_tbl_format_steps(
-      html_tbl = x)
-
-  # [3] Processing of the content table
-  x <-
-    process_content_tbl(
-      html_tbl = x)
-
-  # [4] Creation of the HTML table
-  x <-
-    create_html_table_tbl(
-      html_tbl = x)
-
-  # [5] Join in formatted content
-  x <-
-    use_html_content(
-      html_tbl = x)
-
-  # [6] Merge columns
-  x <-
-    merge_columns(
-      html_tbl = x)
-
-  # [7] Apply HTML aesthetics
-  x <-
-    use_html_aesthetics(
-      html_tbl = x,
-      aesthetics_tbl = x[["aesthetics"]])
-
-  # [8] Generation of table HTML
-  html_output <- create_html(html_tbl = x)
+  # Attach the dependency to the HTML table
+  html_tbl <-
+    htmltools::attachDependencies(htmltools::HTML(html_table), dep)
 
   # Use `print()` to print to the console
-  print(html_output, browse = view, ...)
+  print(html_tbl, browse = view, ...)
 }
