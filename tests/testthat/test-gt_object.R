3c4b11ed16c8da840ffd3dac2893d7b7d374afd3
Richard Iannone <riannone@me.com>
2018-07-04 01:50:17 -0700

    Modify testthat tests

--- tests/testthat/test-gt_object.R
+++ tests/testthat/test-gt_object.R
@@ -1,22 +1,20 @@
 context("test-gt_object.R")
 
-test_that("an HTML table object contains the correct components", {
+test_that("an gt table object contains the correct components", {
 
-  # Create an HTML table object with `tab_create()`
+  library(dplyr)
+
+  # Create an `gt_tbl` object with `gt()`
   table_object <-
     dplyr::tibble(a = 1:5, b = letters[1:5]) %>%
-    tab_create()
+    gt()
 
-  # Expect that the list object has 14 components
+  # Expect that the list object has 2 components
   expect_equal(
-    length(table_object), 14)
+    length(table_object), 2)
 
   # Expect certain named components
   expect_equal(
     table_object %>% names(),
-    c("transforms", "formats", "aesthetics", "fonts",
-      "source_tbl", "modified_tbl", "content_tbl",
-      "heading", "footnote", "source_note",
-      "stubhead_caption", "boxhead_panel", "stub_block",
-      "html_head"))
+    c("data", "formats"))
 })
