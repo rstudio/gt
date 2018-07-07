b5748b05b60d79cf3561fcfbe990700252cafa93
Richard Iannone <riannone@me.com>
2018-07-06 17:30:43 -0700

    Update the `airquality.R` example

--- tests/examples/airquality.R
+++ tests/examples/airquality.R
@@ -1,26 +1,30 @@
 library(gt)
-library(tidyverse)
 
 # Create a presentation table based on `airquality`
 # New York Air Quality Measurements
 
 # 1 - we take the `datasets::airquality` data.frame
-# 2 - all column values are right aligned
-# 3 - the `Month` and `Day` columns are moved to the
+# 2 - the `Month` and `Day` columns are moved to the
 #     beginning of the column series
+# 3 - the `Solar.R` column is given a display label
 # 4 - we are formatting numbers in the `Wind` column
 #     to have 1 decimal place
-# 5 - we replace missing (NA) values in all columns
-#     with a string replacement
+# 5 - a spanner is added above `Month` and `Day`
+# 6 - we replace missing (NA) values in all columns
+#     with a string replacement (default is an em dash)
 airquality_tbl <-
   gt(data = airquality) %>%  # 1
-  cols_align_right() %>%  # 2
-  cols_move_to_start(columns = c("Month", "Day")) %>% # 3
+  cols_move_to_start(columns = c("Month", "Day")) %>% # 2
+  cols_label(c("Solar.R" = "Solar<br>Radiation")) %>% # 3
   fmt_number(
     columns = "Wind",
     decimals = 1
     )  %>% # 4
-  replace_missing(replacement = "&lt; 0.1") # TODO: Doesn't yet work
+  tab_boxhead_panel(
+    spanner = "Measurement Period",
+    columns = c("Month", "Day")
+    ) %>% # 5
+  fmt_missing() # 6
 
 # Display the table in the Viewer
 airquality_tbl
