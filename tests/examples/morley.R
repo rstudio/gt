cdc9840fc3c707d278980cd04f05747fbac60adc
Richard Iannone <riannone@me.com>
2018-07-09 01:49:58 -0700

    Update `morley` script example

--- tests/examples/morley.R
+++ tests/examples/morley.R
@@ -5,16 +5,16 @@ library(tidyverse)
 # Michelson Speed of Light Data
 
 # 1 - we take the `datasets::morley` data.frame and make `Speed` numeric
-# 3 - we are formatting numbers in the `Speed` column to have commas (but no decimal places)
-# 4 - all `integer` columns values are left aligned
+# 2 - we are formatting numbers in the `Speed` column to have commas (but no decimal places)
+# 3 - all `integer` columns values are left aligned
 morley_tbl <-
   gt(data = morley %>% dplyr::mutate(Speed = Speed %>% as.numeric())) %>%  # 1
   fmt_number(
-    columns = "Speed",
-    decimals = 0,
+    columns = Speed,
+    decimals = 2,
     sep_mark = ","
-    ) %>%  # 3
-  cols_align_left(types = "integer")  # 4
+    ) %>% # 2
+  cols_align_left(columns = Speed & Run) # 3
 
 # Display the table in the Viewer
 morley_tbl
