fb8ef2af3c39e20feeecd4ecd5c0d0100f8e8445
Richard Iannone <riannone@me.com>
2018-07-09 02:22:30 -0700

    Update the `mtcars` example script

--- tests/examples/mtcars.R
+++ tests/examples/mtcars.R
@@ -1,83 +1,69 @@
 library(gt)
-library(tidyverse)
 
-# Create a presentation table based on `mtcars`
+# Create a display table based on `mtcars`
 # Motor Trend Car Road Tests
 
 # 1 - we take the `datasets::mtcars` data.frame
-# 2 - the 'striped' theme is applied
-# 3 - we are formatting numbers in 4 columns to have 1 decimal place
-# 4 - we are formatting numbers in the `wt` column to have 3 decimal places
-# 5 - all `numeric` values are right aligned
-# 6 - all `character` values are left aligned
-# 7 - we create stub blocks, grouping together rows into separate
-#     blocks of rows with stub headings
-# 8 - we supply a stubhead caption
-# 9 - a heading is added
-# 10 - two source notes are added
+# 2 - the `disp` and `vs` column values are right aligned
+# 3 - creation of two boxhead panels
+# 4 - columns are being moved around in several ways
+# 5 - two stub blocks are created (`Supercars` and `Others`)
+# 6 - the stub blocks are arranged
+# 7 - we are formatting numbers in different columns to specific decimal places
+# 8 - a heading is added
+# 9 - two source notes are added
+# 10 - a stubhead caption is added
 # 11 - we add footnotes that target several cell values
-# 12 - column headings are provided with new names
+# 12 - column heading labels are provided (i.e., display names)
 mtcars_tbl <-
-  gt(mtcars, rownames_to_stub = TRUE) %>%  # 1
-  theme_striped() %>%  # 2
-  fmt_number(
-    columns = c("mpg", "disp", "drat", "qsec"),
-    decimals = 1
-  ) %>%  # 3
-  fmt_number(
-    columns = "wt",
-    decimals = 3
-  ) %>%  # 4
-  cols_align_left(types = "character") %>%  # 6
+  gt(mtcars, rownames_to_stub = TRUE) %>% # 1
+  cols_align_right(
+    columns = disp & vs) %>% # 2
+  tab_boxhead_panel(
+    group = md("*group_a*"),
+    columns = tgt(mpg, cyl, disp, hp)) %>% # 3
+  tab_boxhead_panel(
+    group = md("*group_b*"),
+    columns = tgt(drat, wt, qsec, vs, am, gear, carb)) %>% # 3
+  cols_move_to_start(columns = hp) %>% # 4
+  cols_move_to_end(columns = am & gear) %>% # 4
+  cols_remove(columns = carb) %>% # 4
+  cols_move(columns = wt & carb & qsec, after = gear) %>% # 4
   tab_stub_block(
-    "Mazdas" = rownames_with("Mazda"),
-    "Mercs" = rownames_with("Merc"),
-    "Hornets" = rownames_with("Hornet"),
-    "Toyotas" = rownames_with("Toyota"),
-    "Supercars" = c(
-      "Ferrari Dino", "Maserati Bora",
-      "Porsche 914-2", "Ford Pantera L",
-      "Camaro Z28")
-  ) %>% # 7
-  tab_stubhead_caption(caption = md("*car* model")) %>%  # 8
+    group = "Supercars",
+    rows = `Ferrari Dino` & `Maserati Bora` & `Porsche 914-2` & `Ford Pantera L`) %>% # 5
+  blocks_arrange(
+    groups = tgt(Supercars, Others)) %>% # 6
+  fmt_number(
+    columns = disp & drat & wt, decimals = 3) %>% # 7
+  fmt_number(
+    columns = qsec, decimals = 2) %>% # 7
+  fmt_number(
+    columns = mpg, decimals = 1) %>% # 7
   tab_heading(
     title = md("The **mtcars** dataset"),
-    headnote = md("[A rather famous *Motor Trend* table]")
-  ) %>%  # 9
+    headnote = md("[A rather famous *Motor Trend* table]")) %>% # 8
   tab_source_note(
-    source_note = md("Main Source of Data: *Henderson and Velleman* (1981)."),
-  ) %>%  # 10
+    source_note = md("Main Source of Data: *Henderson and Velleman* (1981).")) %>% # 9
   tab_source_note(
-    source_note = md("Original Data: *Motor Trend Magazine* (1974)."),
-  ) %>%  # 10
+    source_note = md("Original Data: *Motor Trend Magazine* (1974).")) %>% # 9
+  tab_stubhead_caption(
+    caption = md("*car*")) %>% # 10
   tab_footnote(
-    "Examples of poor gas mileage." =
-      cells(row = c(22, 23), column = 2)
-  ) %>%  # 11
+    footnote = md("*Really* fast quarter mile."),
+    location = target_cell(row = "Ford Pantera L", column = "qsec")) %>% # 11
   tab_footnote(
-    "Really fast quarter mile" =
-      c(18, 8)
-  ) %>%  # 11
+    footnote = "Massive hp.",
+    location = target_cell(row = "Ford Pantera L", column = "hp")) %>% # 11
   tab_footnote(
-    "Excellent gas mileage." =
-      cells(row = 12, column = 2)
-  ) %>%  # 11
+    footnote = "Excellent gas mileage.",
+    location = target_cell("Toyota Corolla", column = 1)) %>% # 11
   tab_footnote(
-    "Worst speed ever." =
-      cells(row = 4, column = 8)
-  ) %>%  # 11
-  tab_footnote(
-    "Massive hp." =
-      cells(row = 18, column = 5)
-  ) %>% # 11
-  cols_rename(
-    "C" = "cyl", "D" = "disp", "HP" = "hp",
-    "DR" = "drat", "Wt." = "wt", "QMT" = "qsec",
-    "V/S" = "vs", "A/M" = "am", "Gears" = "gear",
-    "Carbs" = "carb", "MPG" = "mpg"
-    )
+    footnote = md("Worst speed *ever*."),
+    location = target_cell("Merc 230", "qsec")) %>% # 11
+  cols_label(
+    labels = col_labels(
+      hp = md("*HP*"), qsec = "QMT, seconds")) # 12
 
 # Display the table in the Viewer
 mtcars_tbl
-
-profvis::profvis(print(mtcars_tbl))
