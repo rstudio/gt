28d15107faa1e69f4cf612a349261f4c0d4d64c5
Richard Iannone <riannone@me.com>
2018-07-09 02:03:36 -0700

    Update `emailing_table` script example

--- tests/examples/emailing_table.R
+++ tests/examples/emailing_table.R
@@ -1,103 +1,64 @@
 library(gt)
 library(blastula)
-library(tidyverse)
 
-
-# Create a ggplot plot
-plot <-
-  ggplot(
-    data = mtcars,
-    aes(x = disp, y = hp,
-        color = wt, size = mpg)) +
-  geom_point()
-
-plot_html <-
-  add_ggplot(
-    plot,
-    height = 5, width = 7)
-
+# Create an HTML table that is suitable for
+# emailing; this requires `as_raw_html()` to
+# generate inline CSS styles, which are absolutely
+# essential (i.e., they won't be stripped away)
 html_tbl <-
-  tab_create(tbl = mtcars) %>%  # 1
-  theme_horizontal() %>%  # 2
+  gt(data = airquality) %>%
+  cols_move_to_start(columns = Month & Day) %>%
+  cols_label(labels = col_labels(Solar.R = html("Solar<br>Radiation"))) %>%
   fmt_number(
-    columns = c("mpg", "disp", "drat", "qsec"),
-    decimals = 1
-  ) %>%  # 3
-  fmt_number(
-    columns = "wt",
-    decimals = 3
-  ) %>%  # 4
-  cols_align_left(types = "character") %>%  # 6
-  tab_stub_block(
-    "Mazdas" = rownames_with("Mazda"),
-    "Mercs" = rownames_with("Merc"),
-    "Hornets" = rownames_with("Hornet"),
-    "Toyotas" = rownames_with("Toyota"),
-    "Supercars" = c(
-      "Ferrari Dino","Maserati Bora",
-      "Porsche 914-2", "Ford Pantera L",
-      "Camaro Z28")
-  ) %>% # 7
-  tab_stubhead_caption(caption = md("*car* model")) %>%  # 8
-  tab_heading(
-    title = md("Excerpt from the **mtcars** dataset"),
-    headnote = md("[A rather famous *Motor Trend* table]")
-  ) %>%  # 9
-  tab_source_note(
-    source_note = md("Main Source of Data: *Henderson and Velleman* (1981)."),
-  ) %>%  # 10
-  tab_source_note(
-    source_note = md("Original Data: *Motor Trend Magazine* (1974)."),
-  ) %>%  # 10
-  tab_footnote(
-    "Examples of poor gas mileage." =
-      cells(row = c(22, 23), column = 2)
-  ) %>%  # 11
-  tab_footnote(
-    "Really fast quarter mile" =
-      c(18, 8)
-  ) %>%  # 11
-  tab_footnote(
-    "Excellent gas mileage." =
-      cells(row = 12, column = 2)
-  ) %>%  # 11
-  tab_footnote(
-    "Worst speed ever." =
-      cells(row = 4, column = 8)
-  ) %>%  # 11
-  tab_footnote(
-    "Massive hp." =
-      cells(row = 18, column = 5)
-  ) %>% # 11
-  cols_rename(
-    "MPG" = "mpg",
-    "C" = "cyl",
-    "D" = "disp",
-    "HP" = "hp",
-    "DR" = "drat",
-    "Wt." = "wt",
-    "QMT" = "qsec",
-    "VS" = "vs",
-    "AM" = "am",
-    "Gears" = "gear",
-    "Cs" = "carb") # 12
+    columns = Wind,
+    decimals = 2
+  )  %>%
+  tab_boxhead_panel(
+    group = "Measurement Period",
+    columns = Month & Day
+  ) %>%
+  fmt_missing() %>%
+  as_raw_html()
 
+# Create an email message using the
+# `compose_email()` function from the
+# blastula package
 email <-
   compose_email(
     body = "
   Hello,
 
+  This is an email generated and sent \\
+  using the `blastula` R package.
+
   We can use Markdown formatting \\
   to **embolden** text or to add \\
   *emphasis*.
 
-  Here is a plot:
+  Here is a `gt` table based on the \\
+  `airquality` dataset:
 
-  {plot_html}
+  {html_tbl}
+  "
+)
 
-  Here is a table:
+# Preview the email in the RStudio Viewer
+email %>% preview_email()
 
-  {html_tbl %>% get_html()}
-  ")
+# Create a credentials file for sending
+# this message through Gmail
+blastula::create_email_creds_file(
+  user = "someone@gmail.com",
+  password = "<password>",
+  provider = "gmail",
+  sender = "The Sender",
+  creds_file_name = "<creds_file>")
 
-preview_email(email = email)
+# Send the email message out with
+# `send_email_out()`
+send_email_out(
+  message = email,
+  from = "someone@gmail.com",
+  to = "somebody@gmail.com",
+  subject = "Email with Table Test",
+  creds_file = "<creds_file>")
