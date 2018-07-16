f4f9d4c97036e2a965215ac8895dc2d396823ede
Richard Iannone <riannone@me.com>
2018-07-15 22:07:15 -0700

    Modify `values_uncert` example R script

--- tests/examples/values_uncert.R
+++ tests/examples/values_uncert.R
@@ -7,16 +7,17 @@ tbl <-
     ~value,   ~uncertainty,  ~type,
     0.352,    0.10,          "a",
     0.983,    0.13,          "a",
-    0.639,    0.12,          "b",
-    0.283,    0.17,          "b")
+    0.639,    NA_real_,      "b",
+    NA_real_,    0.17,       "b")
 
 # Create a table
-uncert_tbl <-
+tab <-
   gt(data = tbl) %>%
-  cols_align_center() %>%
-  #fmt_uncertainty(value_column = "value", uncert_column = "uncertainty") %>%
-  fmt_number(columns = c("value", "uncertainty"), decimals = 2) #%>%
-  #fmt_font(font = "Helvetica")
+  fmt_uncertainty(
+    col_val = value,
+    col_uncert = uncertainty) %>%
+  fmt_number(columns = tgt(uncertainty, value), decimals = 3)
 
 # Display the table in the Viewer
-uncert_tbl
+tab
+
