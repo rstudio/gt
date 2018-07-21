180feab04b1c39dfe332481a2a923a61e15e580a
Richard Iannone <riannone@me.com>
2018-07-21 01:11:04 -0700

    Modify tests to reflect restructured object

--- tests/testthat/test-gt_object.R
+++ tests/testthat/test-gt_object.R
@@ -9,13 +9,13 @@ test_that("an gt table object contains the correct components", {
     dplyr::tibble(a = 1:5, b = letters[1:5]) %>%
     gt()
 
-  # Expect that the list object has 8 components
+  # Expect that the object has 9 attributes
   expect_equal(
-    length(table_object), 8)
+    length(attributes(table_object)), 9)
 
-  # Expect certain named components
+  # Expect certain named attributes
   expect_equal(
-    table_object %>% names(),
-    c("input_df", "boxh_df", "stub_df", "fmts_df",
-      "foot_df", "output_df", "formats", "decorators"))
+    names(attributes(table_object)),
+    c("class", "names", "row.names", "boxh_df", "stub_df",
+      "fmts_df", "foot_df", "formats", "decorators"))
 })
