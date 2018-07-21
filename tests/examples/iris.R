d377a957de3fbd944fe895b44731fceacd4daead
Richard Iannone <riannone@me.com>
2018-07-21 01:53:06 -0700

    Modify the `iris` R example script

--- tests/examples/iris.R
+++ tests/examples/iris.R
@@ -4,24 +4,25 @@ library(gt)
 # Edgar Anderson's Iris Data
 
 # 1 - we take the `datasets::iris` data.frame
-#   - the dot-notation column naming format (i.e., `[heading].[caption]`)
-#     is used to automatically creating spanner headings to add structure
-# 2 - the `Species` column is moved to the start of the column series
-# 3 - we are formatting numbers in the named columns to have 1 decimal place
-# 4 - a heading is added
-# 5 - a source note is added
+# 2 - the dot-notation column naming format (i.e., `[heading].[caption]`)
+#     is used to automatically create spanner headings to add structure
+# 3 - the `Species` column is moved to the start of the column series
+# 4 - we are formatting numbers in the named columns to have 1 decimal place
+# 5 - a heading is added
+# 6 - a source note is added
 iris_tbl <-
-  gt(data = iris, split_cols_dots = TRUE) %>% # 1
-  cols_move_to_start(columns = "Species") %>% # 2
+  gt(data = iris) %>% # 1
+  cols_split_delim(delim = ".") %>% # 2
+  cols_move_to_start(columns = "Species") %>% # 3
   fmt_number(
     columns = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"),
     decimals = 1
-    ) %>% # 3
+    ) %>% # 4
   tab_heading(
     title = md("The **iris** dataset"),
-    headnote = md("[All about *Iris setosa*, *versicolor*, and *virginica*]")) %>% # 4
+    headnote = md("[All about *Iris setosa*, *versicolor*, and *virginica*]")) %>% # 5
   tab_source_note(
-    source_note = md("The data were collected by *Anderson* (1935).")) # 5
+    source_note = md("The data were collected by *Anderson* (1935).")) # 6
 
 # Display the table in the Viewer
 iris_tbl
