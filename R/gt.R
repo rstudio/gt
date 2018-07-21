1dc23a6e4fdd96029d967021b86c1de0c649d300
Richard Iannone <riannone@me.com>
2018-07-21 00:52:56 -0700

    Remove `split_cols_dots` argument

--- R/gt.R
+++ R/gt.R
@@ -9,12 +9,6 @@
 #' @param rownames_to_stub an option to take
 #' rownames from the input \code{data} table
 #' as row captions in the stub.
-#' @param split_cols_dots an option to split
-#' dot-separated column names such that the
-#' first component is promoted to a spanner and
-#' subsequent components represent the column label.
-#' Please note that reference to individual columns
-#' must still be via the original column names.
 #' @return an object of class \code{gt_tbl}.
 #' @examples
 #' # Create a table object using the
@@ -37,8 +31,7 @@
 #' @importFrom tibble rownames_to_column
 #' @export
 gt <- function(data,
-               rownames_to_stub = FALSE,
-               split_cols_dots = FALSE) {
+               rownames_to_stub = FALSE) {
 
   if (rownames_to_stub) {
 
@@ -60,34 +53,22 @@ gt <- function(data,
   data_tbl <- data %>% as.data.frame(stringsAsFactors = FALSE)
   rownames(data_tbl) <- NULL
 
-  output_df <- data_tbl
-  output_df[] <- NA_character_
+  empty_df <- data_tbl
+  empty_df[] <- NA_character_
 
   boxh_df <-
-    output_df[0, ] %>%
+    empty_df[0, ] %>%
     tibble::add_row() %>%  # spanner_name
     tibble::add_row() %>%  # column_name (relabeled)
     tibble::add_row()      # alignment
 
-  if (split_cols_dots) {
-
-    colnames <- colnames(data_tbl)
-
-    for (i in seq(colnames)) {
-      if (grepl(".+\\..+", colnames[i])) {
-        split_colname <- strsplit(colnames[i], "\\.") %>% unlist()
-        boxh_df[1, i] <- split_colname[1]
-        boxh_df[2, i] <-
-          paste0(split_colname[2:length(split_colname)], collapse = ".")
-      }
-    }
-  }
-
   attr(data_tbl, "boxh_df") <- boxh_df
   attr(data_tbl, "stub_df") <- stub_df
-  attr(data_tbl, "fmts_df") <- output_df
-  attr(data_tbl, "foot_df") <- output_df
-  attr(data_tbl, "output_df") <- output_df
+
+  attr(data_tbl, "fmts_df") <- empty_df
+  attr(data_tbl, "foot_df") <- empty_df
+  attr(data_tbl, "output_df") <- empty_df
+
   attr(data_tbl, "formats") <- list()
   attr(data_tbl, "decorators") <- list()
 
