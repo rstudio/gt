a9de31900c844ab9a284f703f8379669d342f201
Richard Iannone <riannone@me.com>
2018-07-21 02:49:37 -0700

    Update `values_uncert` R example script

--- tests/examples/values_uncert.R
+++ tests/examples/values_uncert.R
@@ -4,11 +4,11 @@ library(tidyverse)
 # Create a table with values and uncertainties
 tbl <-
   dplyr::tribble(
-    ~value,   ~uncertainty,  ~type,
-    0.352,    0.10,          "a",
-    0.983,    0.13,          "a",
-    0.639,    NA_real_,      "b",
-    NA_real_,    0.17,       "b")
+    ~value,   ~uncertainty,  ~value_2, ~uncertainty_2,
+    0.352,    0.10,          0.32,     NA_real_,
+    0.983,    0.13,          NA_real_, NA_real_,
+    0.639,    NA_real_,      0.21,     0.10,
+    NA_real_, 0.17,          0.74,     0.15)
 
 # Create a table
 tab <-
@@ -16,7 +16,11 @@ tab <-
   fmt_uncertainty(
     col_val = value,
     col_uncert = uncertainty) %>%
-  fmt_number(columns = tgt(uncertainty, value), decimals = 3)
+  fmt_uncertainty(
+    col_val = value_2,
+    col_uncert = uncertainty_2) %>%
+  fmt_number(columns = vars(uncertainty, value), decimals = 3) %>%
+  fmt_missing()
 
 # Display the table in the Viewer
 tab
