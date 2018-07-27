e27aa0a36c5001383058c2715745cad84ead0438
Richard Iannone <riannone@me.com>
2018-07-26 19:08:07 -0700

    Modify function name

--- R/summary_lines.R
+++ R/summary_lines.R
@@ -1,9 +1,9 @@
-#' Add summary lines based on simple aggregations
+#' Add summary rows based on simple aggregations
 #'
-#' Add a summary lines to one or more stub blocks by using the input data
+#' Add a summary rows to one or more stub blocks by using the input data
 #' already provided in the \code{gt()} function.
 #' @param data a table object that is created using the \code{gt()} function.
-#' @param groups the stub block groups heading names for which summary lines
+#' @param groups the stub block groups heading names for which summary rows
 #' will be added.
 #' @param columns the columns for which the summaries should be calculated.
 #' @param agg a vector of aggregate function names. This can include any of
@@ -22,17 +22,17 @@
 #'   tab_stub_block(
 #'     group = "Mercs",
 #'     rows = rownames_with("Merc")) %>%
-#'   summary_lines(
+#'   summary_rows(
 #'     agg = "mean")
 #' @export
-summary_lines <- function(data,
-                          groups = NULL,
-                          columns = NULL,
-                          agg,
-                          decimals = 2,
-                          sep_mark = "",
-                          dec_mark = ".",
-                          tint = NULL) {
+summary_rows <- function(data,
+                         groups = NULL,
+                         columns = NULL,
+                         agg,
+                         decimals = 2,
+                         sep_mark = "",
+                         dec_mark = ".",
+                         tint = NULL) {
 
   if ("summary_auto" %in% names(attributes(data))) {
 
