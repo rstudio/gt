c25041df4b538903d63a17513f101a922d358cba
Richard Iannone <riannone@me.com>
2018-07-26 17:52:47 -0700

    Update testthat tests

--- tests/testthat/test-gt_object.R
+++ tests/testthat/test-gt_object.R
@@ -9,13 +9,13 @@ test_that("an gt table object contains the correct components", {
     dplyr::tibble(a = 1:5, b = letters[1:5]) %>%
     gt()
 
-  # Expect that the object has 11 attributes
+  # Expect that the object has 8 attributes
   expect_equal(
-    length(attributes(table_object)), 11)
+    length(attributes(table_object)), 8)
 
   # Expect certain named attributes
   expect_equal(
     names(attributes(table_object)),
-    c("class", "names", "row.names", "boxh_df", "stub_df", "fmts_df", "foot_df",
-      "formats_html", "formats_rtf", "formats_text", "decorators"))
+    c("class", "names", "row.names", "boxh_df", "stub_df",
+      "fmts_df", "foot_df", "formats"))
 })
