bc54ffa09994aa85227e23be4bffe1365c00e770
Richard Iannone <riannone@me.com>
2018-07-29 23:27:32 -0700

    Modify several R example scripts

--- tests/examples/mtcars.R
+++ tests/examples/mtcars.R
@@ -56,16 +56,16 @@ mtcars_tbl <-
     caption = md("*car*")) %>% # 10
   tab_footnote(
     footnote = md("*Really* fast quarter mile."),
-    location = target_cell(row = `Ford Pantera L`, column = qsec)) %>% # 11
+    location = target_cell(row = "Ford Pantera L", column = vars(qsec))) %>% # 11
   tab_footnote(
     footnote = "Massive hp.",
-    location = target_cell(row = "Ford Pantera L", column = hp)) %>% # 11
+    location = target_cell(row = "Ford Pantera L", column = vars(hp))) %>% # 11
   tab_footnote(
     footnote = "Excellent gas mileage.",
-    location = target_cell(`Toyota Corolla`, column = 2)) %>% # 11
+    location = target_cell("Toyota Corolla", column = 2)) %>% # 11
   tab_footnote(
     footnote = md("Worst speed *ever*."),
-    location = target_cell("Merc 230", qsec)) %>% # 11
+    location = target_cell("Merc 230", vars(qsec))) %>% # 11
   cols_label(
     labels = col_labels(
       hp = md("*HP*"), qsec = "QMT, seconds")) # 12
