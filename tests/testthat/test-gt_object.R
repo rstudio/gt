3bb939d2a0e23c3ad6b0b3995352eb92ee4ebdd3
Richard Iannone <riannone@me.com>
2018-08-07 23:01:32 -0700

    Add tests for `gt()` and `rownames_to_stub`

--- tests/testthat/test-gt_object.R
+++ tests/testthat/test-gt_object.R
@@ -2,7 +2,7 @@ context("test-gt_object.R")
 
 test_that("a gt table object contains the correct components", {
 
-  # Create an `gt_tbl` object with `gt()`
+  # Create a `gt_tbl` object with `gt()`
   tab <- gt(data = iris)
 
   # Expect that the object has 8 attributes
@@ -52,4 +52,128 @@ test_that("a gt table object contains the correct components", {
   expect_equal(
     tab %>% as.data.frame() %>% nrow(),
     nrow(iris))
+
+  # Expect that the `rowname` column of the `stub_df`
+  # object is entirely filled with NAs
+  expect_true(
+    all(is.na(attr(tab, "stub_df")[["rowname"]])))
+
+  # Expect that the `groupname` column of the `stub_df`
+  # object is entirely filled with NAs
+  expect_true(
+    all(is.na(attr(tab, "stub_df")[["groupname"]])))
+
+  # Expect that the column names of the `stub_df` object
+  # are `groupname` and `rowname`
+  expect_equal(
+    colnames(attr(tab, "stub_df")),
+    c("groupname", "rowname"))
+
+  # Expect that the column names of the `boxh_df` object
+  # are the same as those of the original dataset
+  expect_equal(
+    colnames(attr(tab, "boxh_df")),
+    colnames(iris))
+
+  # Expect that the column names of the `fmts_df` object
+  # are the same as those of the original dataset
+  expect_equal(
+    colnames(attr(tab, "fmts_df")),
+    colnames(iris))
+
+  # Expect that the column names of the `foot_df` object
+  # are the same as those of the original dataset
+  expect_equal(
+    colnames(attr(tab, "foot_df")),
+    colnames(iris))
+})
+
+test_that("a gt table can be made to use the rownames of a data frame", {
+
+  # Create a `gt_tbl` object with `gt()` and use the
+  # data frame's row names as row names in the stub
+  tab <- gt(data = mtcars, rownames_to_stub = TRUE)
+
+  # Expect that the object has 8 attributes
+  expect_equal(
+    length(attributes(tab)), 8)
+
+  # Expect that the object has the correct classes
+  expect_is(tab, c("gt_tbl", "data.frame"))
+
+  # Expect certain named attributes
+  expect_true(
+    all(
+      names(attributes(tab)) %in%
+        c("names", "class", "row.names",
+          "boxh_df", "stub_df",
+          "fmts_df", "foot_df", "formats")))
+
+  # Expect that the attribute obejcts are of certain classes
+  expect_is(attr(tab, "boxh_df"), "data.frame")
+  expect_is(attr(tab, "stub_df"), "data.frame")
+  expect_is(attr(tab, "fmts_df"), "data.frame")
+  expect_is(attr(tab, "foot_df"), "data.frame")
+  expect_is(attr(tab, "formats"), "list")
+
+  # Expect that the attribute objects are of the
+  # correct dimensions
+  expect_equal(dim(attr(tab, "boxh_df")), c(3, 11))
+  expect_equal(dim(attr(tab, "stub_df")), c(32, 2))
+  expect_equal(dim(attr(tab, "fmts_df")), c(32, 11))
+  expect_equal(dim(attr(tab, "foot_df")), c(32, 11))
+  expect_equal(length(attr(tab, "formats")), 0)
+
+  # Expect that extracted df has the same column
+  # names as the original dataset
+  expect_equal(
+    tab %>% as.data.frame() %>% colnames(),
+    colnames(mtcars))
+
+  # Expect that extracted df has the same column
+  # classes as the original dataset
+  expect_equal(
+    tab %>% as.data.frame() %>% sapply(class) %>% as.character(),
+    mtcars %>% as.data.frame() %>% sapply(class) %>% as.character())
+
+  # Expect that extracted df has the same number of
+  # rows as the original dataset
+  expect_equal(
+    tab %>% as.data.frame() %>% nrow(),
+    nrow(mtcars))
+
+  # Expect that the row names from the original dataset
+  # populate the `rowname` column of the `stub_df` object
+  expect_equal(
+    attr(tab, "stub_df")[["rowname"]],
+    rownames(mtcars))
+
+  # Expect that the `groupname` column of the `stub_df`
+  # object is entirely filled with NAs
+  expect_true(
+    all(is.na(attr(tab, "stub_df")[["groupname"]])))
+
+  # Expect that the column names of the `stub_df` object
+  # are `groupname` and `rowname`
+  expect_equal(
+    colnames(attr(tab, "stub_df")),
+    c("groupname", "rowname"))
+
+  # Expect that the column names of the `boxh_df` object
+  # are the same as those of the original dataset
+  expect_equal(
+    colnames(attr(tab, "boxh_df")),
+    colnames(mtcars))
+
+  # Expect that the column names of the `fmts_df` object
+  # are the same as those of the original dataset
+  expect_equal(
+    colnames(attr(tab, "fmts_df")),
+    colnames(mtcars))
+
+  # Expect that the column names of the `foot_df` object
+  # are the same as those of the original dataset
+  expect_equal(
+    colnames(attr(tab, "foot_df")),
+    colnames(mtcars))
 })
