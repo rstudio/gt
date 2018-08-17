c068d193a4e634b84951eb3d0d0b5285494d6f93
Richard Iannone <riannone@me.com>
2018-08-17 02:13:38 -0700

    Modify print methods

--- R/knit_print.R
+++ R/knit_print.R
@@ -11,12 +11,16 @@ knit_print.gt_tbl <- function(x, ...) {
   # Generate the HTML table
   html_table <- render_as_html(data = x)
 
+  # Generate the SCSS lines, compile, and write the CSS
+  # to disk as `compiled.css`
+  create_css(data = x)
+
   # Create the htmlDependency
   dep <- htmltools::htmlDependency(
-    name = 'gt',
-    version = '0.1',
-    src = system_file(file = 'css'),
-    stylesheet = 'gt.css')
+    name = "gt",
+    version = "0.1",
+    src = c(file = getwd()),
+    stylesheet = "compiled.css")
 
   # Attach the dependency to the HTML table
   html_tbl <-
