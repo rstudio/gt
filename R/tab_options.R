183bebf6a2d081f890bf8afe1d4173a9f32aa705
Richard Iannone <riannone@me.com>
2018-08-17 15:10:52 -0700

    Add conditionals for each option

--- R/tab_options.R
+++ R/tab_options.R
@@ -2,17 +2,14 @@
 #'
 #' Modify the options available in a table theme.
 #' @inheritParams fmt_number
+#' @param table.font.size the table's font size.
 #' @param table.background.color the table's background color.
 #' @param table.width the width of the table.
 #' @param heading.background.color the background color of the table heading.
 #' @param heading.title.font.size the font size of the title in the table
 #' heading.
-#' @param heading.title.background.color the background color of the title in
-#' the table heading.
 #' @param heading.headnote.font.size the font size of the headnote in the table
 #' heading.
-#' @param heading.headnote.background.color the background color of the headnote
-#' in the table heading.
 #' @param heading.border.top the parameters for the top border of the table
 #' heading.
 #' @param heading.border.bottom the parameters for the bottom border of the
@@ -53,13 +50,12 @@
 #' @family table-part creation/modification functions
 #' @export
 tab_options <- function(data,
+                        table.font.size = NULL,
                         table.background.color = NULL,
                         table.width = NULL,
                         heading.background.color = NULL,
                         heading.title.font.size = NULL,
-                        heading.title.background.color = NULL,
                         heading.headnote.font.size = NULL,
-                        heading.headnote.background.color = NULL,
                         heading.border.top = NULL,
                         heading.border.bottom = NULL,
                         stub_heading.background.color = NULL,
@@ -86,20 +82,200 @@ tab_options <- function(data,
   # Extract the `opts_df` data frame object from `data`
   opts_df <- attr(data, "opts_df", exact = TRUE)
 
+  # table.font.size
+  if (!is.null(table.font.size)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "table_font_size", table.font.size)
+  }
+
   # table.background.color
   if (!is.null(table.background.color)) {
 
-    opts_df[
-      which(opts_df$parameter == "table_background_color"), 3] <-
-      table.background.color
+    opts_df <- opts_df_set(
+      opts_df, "table_background_color", table.background.color)
+  }
+
+  # table.width
+  if (!is.null(table.width)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "table_width", table.width)
+  }
+
+  # heading.background.color
+  if (!is.null(heading.background.color)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "heading_background_color", heading.background.color)
+  }
+
+  # heading.title.font.size
+  if (!is.null(heading.title.font.size)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "heading_title_font_size", heading.title.font.size)
+  }
+
+  # heading.headnote.font.size
+  if (!is.null(heading.headnote.font.size)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "heading_headnote_font_size", heading.headnote.font.size)
+  }
+
+  # heading.border.top
+  if (!is.null(heading.border.top)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "heading_border_top", heading.border.top)
+  }
+
+  # heading.border.bottom
+  if (!is.null(heading.border.bottom)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "heading_border_bottom", heading.border.bottom)
+  }
+
+  # stub_heading.background.color
+  if (!is.null(stub_heading.background.color)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "stub_heading_background_color", stub_heading.background.color)
+  }
+
+  # stub_heading.border.top
+  if (!is.null(stub_heading.border.top)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "stub_heading_border_top", stub_heading.border.top)
+  }
+
+  # stub_heading.border.bottom
+  if (!is.null(stub_heading.border.bottom)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "stub_heading_border_bottom", stub_heading.border.bottom)
+  }
+
+  # stub_heading_field.background.color
+  if (!is.null(stub_heading_field.background.color)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "stub_heading_field_background_color", stub_heading_field.background.color)
+  }
+
+  # stub_heading_field.border.top
+  if (!is.null(stub_heading_field.border.top)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "stub_heading_field_border_top", stub_heading_field.border.top)
+  }
+
+  # stub_heading_field.border.bottom
+  if (!is.null(stub_heading_field.border.bottom)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "stub_heading_field_border_bottom", stub_heading_field.border.bottom)
+  }
+
+  # field.border.top
+  if (!is.null(field.border.top)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "field_border_top", field.border.top)
+  }
+
+  # field.border.bottom
+  if (!is.null(field.border.bottom)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "field_border_bottom", field.border.bottom)
+  }
+
+  # row.padding
+  if (!is.null(row.padding)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "row_padding", row.padding)
+  }
+
+  # row.margin
+  if (!is.null(row.margin)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "row_margin", row.margin)
   }
 
   # row.striping.background.color
   if (!is.null(row.striping.background.color)) {
 
-    opts_df[
-      which(opts_df$parameter == "row_striping_background_color"), 3] <-
-      row.striping.background.color
+    opts_df <- opts_df_set(
+      opts_df, "row_striping_background_color", row.striping.background.color)
+  }
+
+  # row.striping.include.stub
+  if (!is.null(row.striping.include.stub)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "row_striping_include_stub", row.striping.include.stub)
+  }
+
+  # row.striping.include_field
+  if (!is.null(row.striping.include_field)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "row_striping_include_field", row.striping.include_field)
+  }
+
+  # summary_row.background.color
+  if (!is.null(summary_row.background.color)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "summary_row_background_color", summary_row.background.color)
+  }
+
+  # summary_row.padding
+  if (!is.null(summary_row.padding)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "summary_row_padding", summary_row.padding)
+  }
+
+  # summary_row.text_transform
+  if (!is.null(summary_row.text_transform)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "summary_row_text_transform", summary_row.text_transform)
+  }
+
+  # footnote.font.size
+  if (!is.null(footnote.font.size)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "footnote_font_size", footnote.font.size)
+  }
+
+  # footnote.padding
+  if (!is.null(footnote.padding)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "footnote_padding", footnote.padding)
+  }
+
+  # sourcenote.font.size
+  if (!is.null(sourcenote.font.size)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "sourcenote_font_size", sourcenote.font.size)
+  }
+
+  # sourcenote.padding
+  if (!is.null(sourcenote.padding)) {
+
+    opts_df <- opts_df_set(
+      opts_df, "sourcenote_padding", sourcenote.padding)
   }
 
   # Write the modified `opts_df` to the `data` attribute
