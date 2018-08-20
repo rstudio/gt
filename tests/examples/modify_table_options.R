07ee72c91265e73da8c72448c358d675c8445198
Richard Iannone <riannone@me.com>
2018-08-19 19:22:17 -0700

    Update 'modify_table_options' R example script

--- tests/examples/modify_table_options.R
+++ tests/examples/modify_table_options.R
@@ -5,16 +5,16 @@ library(gt)
 tbl <-
   dplyr::tribble(
     ~groupname,    ~rowname, ~col_1, ~col_2, ~col_3, ~col_4,
-    "2018-02-10",  "1",        20.4,  361.1,  361.1,  361.1,
-    "2018-02-10",  "2",        10.9,  743.3,  743.3,  743.3,
-    "2018-02-10",  "3",        34.6,  344.7,  234.2,  344.7,
-    "2018-02-10",  "4",         8.3,  342.3,  342.3,  342.3,
-    "2018-02-11",  "5",        28.3,  234.9,   23.3,  234.9,
-    "2018-02-11",  "6",        75.5,  190.9,   10.9,  190.9,
-    "2018-02-11",  "7",        63.1,    2.3,   22.2,    2.3,
-    "2018-02-11",  "8",        25.8,  184.3,  184.3,  184.3,
-    "2018-02-11",  "9",         5.2,  197.2,  197.2,  197.2,
-    "2018-02-11",  "10",       55.3,  284.6,  284.6,  284.6)
+    "2018-02-10",  "1",       767.6,  928.1,  382.0,  674.5,
+    "2018-02-10",  "2",       403.3,  461.5,   15.1,  242.8,
+    "2018-02-10",  "3",       686.4,   54.1,  282.7,   56.3,
+    "2018-02-10",  "4",       662.6,  148.8,  984.6,  928.1,
+    "2018-02-11",  "5",       198.5,   65.1,  127.4,  219.3,
+    "2018-02-11",  "6",       132.1,  118.1,   91.2,  874.3,
+    "2018-02-11",  "7",       349.7,  307.1,  566.7,  542.9,
+    "2018-02-11",  "8",        63.7,  504.3,  152.0,  724.5,
+    "2018-02-11",  "9",       105.4,  729.8,  962.4,  336.4,
+    "2018-02-11",  "10",      924.2,  424.6,  740.8,  104.2)
 
 data <-
   gt(tbl) %>%
@@ -31,51 +31,57 @@ data <-
     footnote = "Footnote #1",
     location = data_cells(row = 1, column = 1)) %>%
   tab_footnote(
-    footnote = "Footnote below",
-    location = data_cells(row = 2, column = 1)) %>%
+    footnote = "Footnote #2",
+    location = data_cells(row = 2, column = 2)) %>%
+  tab_footnote(
+    footnote = "Footnote #3",
+    location = data_cells(row = 3, column = 3)) %>%
+  tab_footnote(
+    footnote = "Footnote #4",
+    location = data_cells(row = 4, column = 4)) %>%
   tab_source_note("Source Note") %>%
   tab_options(
-    table.font.size = 14,                # Entire table's font size
+    table.font.size = px(14),            # Entire table's font size
     table.background.color = NULL,       # Entire table's background color
-    table.width = "100%",                # Entire table's width
+    table.width = pct(100),              # Entire table's width
     table.border.top.style = "solid",    # Top line of table - style
-    table.border.top.width = 3,          # Top line of table - width
+    table.border.top.width = px(3),      # Top line of table - width
     table.border.top.color = "orange",   # Top line of table - color
     heading.background.color = "lightblue",   # Heading background color
-    heading.title.font.size = 18,        # Title of heading font size
-    heading.headnote.font.size = 14,     # Headnote font size
+    heading.title.font.size = px(18),    # Title of heading font size
+    heading.headnote.font.size = px(14), # Headnote font size
     heading.border.bottom.style = "solid",     # Bottom line of heading - style
-    heading.border.bottom.width = 5,           # Bottom line of heading - width
+    heading.border.bottom.width = px(5),       # Bottom line of heading - width
     heading.border.bottom.color = "purple",    # Bottom line of heading - color
-    boxhead.font.size = 16,                    # Boxhead labels - font size
+    boxhead.font.size = px(16),                # Boxhead labels - font size
     boxhead.font.weight = "normal",            # Boxhead labels - font weight
     boxhead.background.color = "gray",         # Boxhead background color
     stub_group.background.color = "green",     # Stub group background color
-    stub_group.font.size = 14,                 # Stub group labels - font size
-    stub_group.font.weight = 800,              # Stub group labels - font weight
+    stub_group.font.size = px(14),             # Stub group labels - font size
+    stub_group.font.weight = "800",            # Stub group labels - font weight
     stub_group.border.top.style = "solid",     # Top line of stub group - style
-    stub_group.border.top.width = 5,           # Top line of stub group - width
+    stub_group.border.top.width = px(5),       # Top line of stub group - width
     stub_group.border.top.color = "blue",      # Top line of stub group - color
     stub_group.border.bottom.style = "solid",     # Bottom line of stub group - style
-    stub_group.border.bottom.width = 4,           # Bottom line of stub group - width
+    stub_group.border.bottom.width = px(4),       # Bottom line of stub group - width
     stub_group.border.bottom.color = "orange",    # Bottom line of stub group - color
     field.border.top.style = "solid",          # Top line of field - style
-    field.border.top.width = 5,                # Top line of field - width
+    field.border.top.width = px(5),            # Top line of field - width
     field.border.top.color = "indianred",      # Top line of field - color
     field.border.bottom.style = "solid",       # Bottom line of field - style
-    field.border.bottom.width = 5,             # Bottom line of field - width
+    field.border.bottom.width = px(5),         # Bottom line of field - width
     field.border.bottom.color = "turquoise",   # Bottom line of field - color
-    row.padding = 8,                           # padding of all data rows (stub & field)
+    row.padding = px(8),                       # Padding of all data rows (stub & field)
     row.striping.background.color = "red",     # -- does nothing currently
     row.striping.include.stub = FALSE,         # -- does nothing currently
     row.striping.include_field = TRUE,         # -- does nothing currently
     summary_row.background.color = "pink", # -- background color of all summary rows (stub & field)
-    summary_row.padding = 3,                   # padding of all summary rows (stub & field)
+    summary_row.padding = px(3),               # padding of all summary rows (stub & field)
     summary_row.text_transform = "lowercase",  # text transform on all summary row labels
-    footnote.font.size = 12,                   # text size of the footnotes block
-    footnote.padding = 3,                      # padding of the footnotes block
-    sourcenote.font.size = 12,                 # text size of the source notes block
-    sourcenote.padding = 3                     # padding of the source note block
+    footnote.font.size = px(12),               # text size of the footnotes block
+    footnote.padding = px(3),                  # padding of the footnotes block
+    sourcenote.font.size = px(12),             # text size of the source notes block
+    sourcenote.padding = px(3)                 # padding of the source note block
   )
 
 data
