635af54471bdbc25d7cffda6e51647eed1077d58
Richard Iannone <riannone@me.com>
2018-08-23 23:24:51 -0700

    Register `knit_print` S3 method

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,31 @@
+register_s3_method <- function(pkg, generic, class, fun = NULL) {
+  stopifnot(is.character(pkg), length(pkg) == 1)
+  stopifnot(is.character(generic), length(generic) == 1)
+  stopifnot(is.character(class), length(class) == 1)
+
+  if (is.null(fun)) {
+    fun <- get(paste0(generic, ".", class), envir = parent.frame())
+  } else {
+    stopifnot(is.function(fun))
+  }
+
+  if (pkg %in% loadedNamespaces()) {
+    registerS3method(generic, class, fun, envir = asNamespace(pkg))
+  }
+
+  # Always register hook in case package is later unloaded & reloaded
+  setHook(
+    packageEvent(pkg, "onLoad"),
+    function(...) {
+      registerS3method(generic, class, fun, envir = asNamespace(pkg))
+    }
+  )
+}
+
+.onLoad <- function(...) {
+  register_s3_method("knitr", "knit_print", "gt_tbl")
+}
+
 #' @importFrom utils globalVariables
 globalVariables(
   c(
@@ -23,3 +51,5 @@ globalVariables(
     "value"
     )
   )
+
+
