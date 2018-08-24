0a478b4a239f3667670bbf5e6b49a657ee8c56d8
Richard Iannone <riannone@me.com>
2018-08-23 23:25:59 -0700

    Use `taglist()` to hold CSS

--- R/print.R
+++ R/print.R
@@ -6,31 +6,41 @@
 #' @export
 print.gt_tbl <- function(x, ..., view = interactive()) {
 
+  html_tbl <- as.tags.gt_tbl(x, ...)
+
+  # Use `print()` to print to the console
+  print(html_tbl, browse = view, ...)
+}
+
+#' Knit print the table
+#' @description This facilitates printing of the HTML table within a knitr
+#' code chunk.
+#' @param x an object of class \code{gt_tbl}.
+#' @keywords internal
+#' @importFrom htmltools tagList htmlDependency attachDependencies HTML
+knit_print.gt_tbl <- function(x, ...) {
+
+  html_tbl <- as.tags.gt_tbl(x, ...)
+
+  # Use `knit_print()` to print in a code chunk
+  knitr::knit_print(html_tbl, ...)
+}
+
+#' @export
+as.tags.gt_tbl <- function(x, ...) {
+
   # Generate the HTML table
   html_table <- render_as_html(data = x)
 
   # Generate the SCSS lines, compile, and write the CSS
   # to disk as `compiled.css`
-  compile_css(data = x)
-
-  # Create the htmlDependency
-  dep <- htmltools::htmlDependency(
-    name = "gt",
-    version = "0.1",
-    src = c(file = getwd()),
-    stylesheet = "compiled.css")
+  css <- compile_css(data = x)
 
   # Attach the dependency to the HTML table
-  html_tbl <-
-    htmltools::attachDependencies(
-      htmltools::tagList(
-        htmltools::HTML(html_table)), dep)
+  html_tbl <- tagList(
+    htmltools::tags$style(htmltools::HTML(css)),
+    htmltools::HTML(html_table)
+  )
 
-  # Use `print()` to print to the console
-  print(html_tbl, browse = view, ...)
-
-  # Remove the `compiled.css` file
-  file.remove("compiled.css")
-
-  invisible(x)
+  html_tbl
 }
