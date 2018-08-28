db59d9ec7534be971debbddd732e4a45ed5eb4b3
Richard Iannone <riannone@me.com>
2018-08-28 04:09:46 -0700

    Remove older resolving functions

--- R/resolver.R
+++ R/resolver.R
@@ -1,62 +1,3 @@
-#' @import rlang
-#' @importFrom tibble rownames_to_column
-#' @importFrom dplyr filter
-#' @noRd
-resolve_rows <- function(data,
-                         rows) {
-
-  if (inherits(rows, "rownames_with")) {
-
-    rows <-
-      which(
-        grepl(paste(rows$pattern, collapse = "|"),
-              attr(data, "stub_df", exact = TRUE)[["rowname"]]))
-
-  } else if (inherits(rows, "quosure")) {
-
-    rows <-
-      (data %>%
-         tibble::rownames_to_column() %>%
-         dplyr::filter(rlang::UQ(rows)))[["rowname"]] %>%
-      as.numeric()
-
-  } else if (is.numeric(rows)) {
-
-    rows <- rows[rows %in% 1:nrow(data)]
-
-  } else if (is.character(rows) &&
-             all(!is.na(attr(data, "stub_df")[["rowname"]]))) {
-
-    rows <- which(attr(data, "stub_df")[["rowname"]] %in% rows)
-  }
-
-  rows
-}
-
-#' @noRd
-resolve_columns <- function(data, columns) {
-
-  if (inherits(columns, "quosures")) {
-    columns <- columns %>% lapply(`[[`, 2) %>% as.character()
-  }
-
-  if (inherits(columns, "columns_with")) {
-
-    columns <- colnames(data)[
-      which(grepl(paste(columns$pattern, collapse = "|"), colnames(data)))]
-
-  } else if (is.numeric(columns)) {
-
-    columns <- colnames(data)[columns]
-
-  } else if (is.character(columns)) {
-
-    columns <- columns[columns %in% colnames(data)]
-  }
-
-  columns
-}
-
 #' Helper for targeting multiple data cells
 #'
 #' This function is to be used to target individual data cells through the
@@ -280,29 +221,7 @@ resolve_vars <- function(var_expr, var_names) {
 }
 
 
-#' @noRd
-column_row_resolution <- function(data, columns, rows) {
 
-  # If nothing is provided for rows, assume
-  # that all rows are in the selection
-  if (is.null(rows)) {
-    rows <- TRUE
-  }
-
-  # Resolve the columns to be targeted
-  columns <- resolve_columns(data = data, columns = columns)
-
-  # Resolve the rows to be targeted
-  rows <- resolve_rows(data = data, rows = rows)
-
-  resolved <- list(columns = columns, rows = rows)
-
-  if (length(rows) == 0 || length(columns) == 0) {
-    class(resolved) <- c("no_intersection", class(resolved))
-  }
-
-  resolved
-}
 
 #' @noRd
 no_intersection <- function(resolved) {
