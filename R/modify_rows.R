69887942b2dea01f255250dd17251045218f80d5
Richard Iannone <riannone@me.com>
2018-08-28 16:20:53 -0700

    Modify roxygen documentation

--- R/modify_rows.R
+++ R/modify_rows.R
@@ -1,5 +1,5 @@
 #' Modify the ordering of the stub block groups
-#' @param data a table object that is created using the \code{gt()} function.
+#' @inheritParams cols_align
 #' @param groups a vector of stub block group heading names.
 #' @return an object of class \code{gt_tbl}.
 #' @family row modification functions
