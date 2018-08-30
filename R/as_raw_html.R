63da0bb81afea3a76ef326ba5a55a6e8c43ac08b
Richard Iannone <riannone@me.com>
2018-08-29 23:34:06 -0700

    Modify roxygen documentation

--- R/as_raw_html.R
+++ R/as_raw_html.R
@@ -1,12 +1,12 @@
 #' Get the HTML content of a gt table
 #'
 #' Get the HTML content string from a \code{gt_tbl} object as a single-length
-#' character vector. There is also the option to transform the HTML such that
-#' CSS styles are inlined. This option is preferable when using the output HTML
-#' table in an emailing context.
+#'   character vector. There is also the option to transform the HTML such that
+#'   CSS styles are inlined. This option is preferable when using the output
+#'   HTML table in an emailing context.
 #' @param data a table object that is created using the \code{gt()} function.
 #' @param inline_css an option to supply styles to table elements as inlined
-#' CSS styles.
+#'   CSS styles.
 #' @export
 as_raw_html <- function(data, inline_css = TRUE) {
 
