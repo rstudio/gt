485de9b0703240bdaf7d79230f81f08b4917a758
Richard Iannone <riannone@me.com>
2018-08-31 09:28:12 -0700

    Add `resolve_*()` util functions

--- R/resolver.R
+++ R/resolver.R
@@ -19,15 +19,11 @@ resolve_data_cells <- function(data,
   object_columns <- object$columns
   object_rows <- object$rows
 
-  # Collect the column names and column indices
-  # from `data_df`
+  # Collect the column names from `data_df`
   colnames <- names(data_df)
-  colnums <- seq(names(data_df))
 
-  # Collect the rownames and row indices
-  # from `stub_df`
+  # Collect the rownames from `stub_df`
   rownames <- stub_df$rowname
-  rownums <- seq(nrow(stub_df))
 
   #
   # Resolution of columns and rows as integer vectors
@@ -46,11 +42,88 @@ resolve_data_cells <- function(data,
   # Create a list object
   cells_resolved <- list(columns = expansion[[1]], rows = expansion[[2]])
 
-  # Apply the `data_cells` class
+  # Apply the `data_cells_resolved` class
   attr(cells_resolved, "class") <- "data_cells_resolved"
 
   cells_resolved
 }
+
+#' Resolve the `stub_rows` object once it has access to the `data` object
+#' @param data a table object that is created using the \code{gt()} function.
+#' @param object the list object created by the \code{stub_rows()} function.
+#' @import rlang
+#' @importFrom tibble rownames_to_column
+#' @importFrom dplyr filter arrange distinct
+#' @noRd
+resolve_stub_rows <- function(data,
+                              object) {
+
+  # Get the `data_df` data frame from `data`
+  data_df <- as.data.frame(data)
+
+  # Get the `stub_df` data frame from `data`
+  stub_df <- attr(data, "stub_df", exact = TRUE)
+
+  # Obtain the `columns` and `rows` components of
+  # object created by the `data_cells()` function
+  object_rows <- object$rows
+
+  # Collect the rownames from `stub_df`
+  rownames <- stub_df$rowname
+
+  #
+  # Resolution of rows as integer vectors
+  # providing the positions of the matched variables
+  #
+
+  resolved_rows <- resolve_vars(object_rows, rownames, data_df)
+
+  # Create a list object
+  resolved_rows <- list(rows = resolved_rows)
+
+  # Apply the `stub_rows_resolved` class
+  attr(resolved_rows, "class") <- "stub_rows_resolved"
+
+  resolved_rows
+}
+
+#' Resolve the `boxhead_columns` object once it has access to the `data` object
+#' @param data a table object that is created using the \code{gt()} function.
+#' @param object the list object created by the \code{boxhead_columns()}
+#'   function.
+#' @import rlang
+#' @importFrom tibble rownames_to_column
+#' @importFrom dplyr filter arrange distinct
+#' @noRd
+resolve_boxhead_columns <- function(data,
+                                    object) {
+
+  # Get the `data_df` data frame from `data`
+  data_df <- as.data.frame(data)
+
+  # Collect the column names from `data_df`
+  colnames <- names(data_df)
+
+  # Obtain the `columns` component of object created
+  # by the `boxhead_columns()` function
+  object_columns <- object$columns
+
+  #
+  # Resolution of columns as integer vectors
+  # providing the positions of the matched variables
+  #
+
+  resolved_columns <- resolve_vars(object_columns, colnames, data_df)
+
+  # Create a list object
+  resolved_columns <- list(columns = resolved_columns)
+
+  # Apply the `boxhead_columns_resolved` class
+  attr(resolved_columns, "class") <- "boxhead_columns_resolved"
+
+  resolved_columns
+}
+
 
 #' Resolve variables for rows and columns using expressions
 #' @import tidyselect
@@ -59,7 +132,9 @@ resolve_vars <- function(var_expr, var_names, data_df) {
 
   stopifnot(rlang::is_quosure(var_expr))
 
-  resolved <- with_vars(var_names, rlang::eval_tidy(var_expr, data_df, env = NULL))
+  resolved <-
+    tidyselect::with_vars(
+      var_names, rlang::eval_tidy(var_expr, data_df, env = NULL))
 
   if (is.null(resolved)) {
 
