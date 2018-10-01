9bb71bab3be0260907fe1b3e0b766b27d4e03072
Richard Iannone <riannone@me.com>
2018-09-30 21:25:18 -0700

    Add testthat tests for Base64 functions

---
+++ tests/testthat/test-base_64.R
@@ -0,0 +1,31 @@
+context("Testing of Base64 functions")
+
+test_that("the Base64 functions work correctly", {
+
+  # Expect that filenames with various extensions are
+  # work with `get_file_ext()` to return the file extension
+  get_file_ext(file = "file.svg") %>% expect_equal("svg")
+  get_file_ext(file = "file.001.svg") %>% expect_equal("svg")
+  get_file_ext(file = "file.001..svg") %>% expect_equal("svg")
+  get_file_ext(file = "_file.jpg") %>% expect_equal("jpg")
+  get_file_ext(file = "file.png") %>% expect_equal("png")
+  get_file_ext(file = "file.gif") %>% expect_equal("gif")
+
+  # Expect that the `get_mime_type()` function returns
+  # a mime-type string for svg and jpg files
+  get_mime_type(file = "file.svg") %>% expect_equal("image/svg+xml")
+  get_mime_type(file = "file.jpg") %>% expect_equal("image/jpeg")
+
+  # Expect that the image URI string based on a PNG test image
+  # matches a known good image URI created from that image
+  expect_equal(
+    get_image_uri(file = system_file("./graphics/test_image.png")),
+    readLines(system_file("./graphics/test_image_png_uri.txt"), warn = FALSE))
+
+  # Expect that the beginning of the PNG-based image URI has the MIME
+  # type and the encoding type
+  grepl(
+    "^data:image/png;base64,",
+    get_image_uri(file = system_file("./graphics/test_image.png"))) %>%
+    expect_true()
+})
