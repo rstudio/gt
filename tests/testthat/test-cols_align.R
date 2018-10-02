87599cdab471917fc77292af433522789f4b58e6
Richard Iannone <riannone@me.com>
2018-10-02 12:02:29 -0700

    Modify testthat tests

--- tests/testthat/test-cols_align.R
+++ tests/testthat/test-cols_align.R
@@ -9,128 +9,6 @@ check_suggests <- function() {
   skip_if_not_installed("xml2")
 }
 
-test_that("column data can be aligned to the left", {
-
-  # Check that specific suggested packages are available
-  check_suggests()
-
-  # Create a `tbl_html` object with `gt()`; the `mpg`,
-  # `cyl`, and `drat` columns are aligned left
-  tbl_html <-
-    gt(data = mtcars_short) %>%
-    cols_align_left(columns = vars(mpg, cyl, drat)) %>%
-    render_as_html() %>%
-    xml2::read_html()
-
-  # Expect that the columns with class `col_heading left`
-  # are those columns that were aligned left
-  tbl_html %>%
-    rvest::html_nodes("[class='col_heading left']") %>%
-    rvest::html_text() %>%
-    expect_equal(c("mpg", "cyl", "drat"))
-
-  # Expect that all other columns are center-aligned
-  tbl_html %>%
-    rvest::html_nodes("[class='col_heading center']") %>%
-    rvest::html_text() %>%
-    expect_equal(base::setdiff(colnames(mtcars_short), c("mpg", "cyl", "drat")))
-
-  # Create a `tbl_html` object with `gt()`; the `mpg`,
-  # column is hidden and an attempt to align that column
-  # to the left doesn't cause an error
-  tbl_html <-
-    gt(data = mtcars_short) %>%
-    cols_hide(vars(mpg)) %>%
-    cols_align_left(columns = vars(mpg)) %>%
-    render_as_html() %>%
-    xml2::read_html()
-
-  # Expect that all remaining columns are center-aligned
-  tbl_html %>%
-    rvest::html_nodes("[class='col_heading center']") %>%
-    rvest::html_text() %>%
-    expect_equal(base::setdiff(colnames(mtcars_short), "mpg"))
-})
-
-test_that("column data can be aligned to the right", {
-
-  # Check that specific suggested packages are available
-  check_suggests()
-
-  # Create a `tbl_html` object with `gt()`; the `mpg`,
-  # `cyl`, and `drat` columns are aligned right
-  tbl_html <-
-    gt(data = mtcars_short) %>%
-    cols_align_right(columns = vars(mpg, cyl, drat)) %>%
-    render_as_html() %>%
-    xml2::read_html()
-
-  # Expect that the columns with class `col_heading right`
-  # are those columns that were aligned right
-  tbl_html %>%
-    rvest::html_nodes("[class='col_heading right']") %>%
-    rvest::html_text() %>%
-    expect_equal(c("mpg", "cyl", "drat"))
-
-  # Expect that all other columns are center-aligned
-  tbl_html %>%
-    rvest::html_nodes("[class='col_heading center']") %>%
-    rvest::html_text() %>%
-    expect_equal(base::setdiff(colnames(mtcars_short), c("mpg", "cyl", "drat")))
-
-  # Create a `tbl_html` object with `gt()`; the `mpg`,
-  # column is hidden and an attempt to align that column
-  # to the right doesn't cause an error
-  tbl_html <-
-    gt(data = mtcars_short) %>%
-    cols_hide(vars(mpg)) %>%
-    cols_align_right(columns = vars(mpg)) %>%
-    render_as_html() %>%
-    xml2::read_html()
-
-  # Expect that all remaining columns are center-aligned
-  tbl_html %>%
-    rvest::html_nodes("[class='col_heading center']") %>%
-    rvest::html_text() %>%
-    expect_equal(base::setdiff(colnames(mtcars_short), "mpg"))
-})
-
-test_that("column data can be center-aligned", {
-
-  # Check that specific suggested packages are available
-  check_suggests()
-
-  # Create a `tbl_html` object with `gt()`; the `mpg`,
-  # `cyl`, and `drat` columns are center-aligned
-  tbl_html <-
-    gt(data = mtcars_short) %>%
-    cols_align_center(columns = vars(mpg, cyl, drat)) %>%
-    render_as_html() %>%
-    xml2::read_html()
-
-  # Expect that all columns have the class `col_heading center`
-  tbl_html %>%
-    rvest::html_nodes("[class='col_heading center']") %>%
-    rvest::html_text() %>%
-    expect_equal(colnames(mtcars_short))
-
-  # Create a `tbl_html` object with `gt()`; the `mpg`,
-  # column is hidden and an attempt to center-align that column
-  # doesn't cause an error
-  tbl_html <-
-    gt(data = mtcars_short) %>%
-    cols_hide(vars(mpg)) %>%
-    cols_align_center(columns = vars(mpg)) %>%
-    render_as_html() %>%
-    xml2::read_html()
-
-  # Expect that all remaining columns are center-aligned
-  tbl_html %>%
-    rvest::html_nodes("[class='col_heading center']") %>%
-    rvest::html_text() %>%
-    expect_equal(base::setdiff(colnames(mtcars_short), "mpg"))
-})
-
 test_that("the function `cols_align()` works as expected", {
 
   # Check that specific suggested packages are available
@@ -158,52 +36,6 @@ test_that("the function `cols_align()` works as expected", {
     expect_equal(base::setdiff(colnames(mtcars_short), c("mpg", "cyl", "drat")))
 
   # Create a `tbl_html` object with `gt()`; the `mpg`,
-  # column is hidden and an attempt to align that column
-  # to the left doesn't cause an error
-  tbl_html <-
-    gt(data = mtcars_short) %>%
-    cols_hide(vars(mpg)) %>%
-    cols_align(align = "left", columns = vars(mpg)) %>%
-    render_as_html() %>%
-    xml2::read_html()
-
-  # Expect that all remaining columns are center-aligned
-  tbl_html %>%
-    rvest::html_nodes("[class='col_heading center']") %>%
-    rvest::html_text() %>%
-    expect_equal(base::setdiff(colnames(mtcars_short), "mpg"))
-
-  # Create a `tbl_html` object with `gt()`; the `mpg`,
-  # `cyl`, and `drat` columns are center-aligned
-  tbl_html <-
-    gt(data = mtcars_short) %>%
-    cols_align(align = "center", columns = vars(mpg, cyl, drat)) %>%
-    render_as_html() %>%
-    xml2::read_html()
-
-  # Expect that all columns have the class `col_heading center`
-  tbl_html %>%
-    rvest::html_nodes("[class='col_heading center']") %>%
-    rvest::html_text() %>%
-    expect_equal(colnames(mtcars_short))
-
-  # Create a `tbl_html` object with `gt()`; the `mpg`,
-  # column is hidden and an attempt to center-align that column
-  # doesn't cause an error
-  tbl_html <-
-    gt(data = mtcars_short) %>%
-    cols_hide(vars(mpg)) %>%
-    cols_align(align = "center", columns = vars(mpg)) %>%
-    render_as_html() %>%
-    xml2::read_html()
-
-  # Expect that all remaining columns are center-aligned
-  tbl_html %>%
-    rvest::html_nodes("[class='col_heading center']") %>%
-    rvest::html_text() %>%
-    expect_equal(base::setdiff(colnames(mtcars_short), "mpg"))
-
-  # Create a `tbl_html` object with `gt()`; the `mpg`,
   # `cyl`, and `drat` columns are aligned right
   tbl_html <-
     gt(data = mtcars_short) %>%
@@ -224,19 +56,15 @@ test_that("the function `cols_align()` works as expected", {
     rvest::html_text() %>%
     expect_equal(base::setdiff(colnames(mtcars_short), c("mpg", "cyl", "drat")))
 
-  # Create a `tbl_html` object with `gt()`; the `mpg`,
-  # column is hidden and an attempt to align that column
-  # to the right doesn't cause an error
-  tbl_html <-
+  # Expect that supplying an `align` value that is not `left`, `center`,
+  # or `right` will result in an error
+  expect_error(
     gt(data = mtcars_short) %>%
-    cols_hide(vars(mpg)) %>%
-    cols_align(align = "right", columns = vars(mpg)) %>%
-    render_as_html() %>%
-    xml2::read_html()
+      cols_align(align = "right", columns = vars(mpg, cyl, drat)))
 
-  # Expect that all remaining columns are center-aligned
-  tbl_html %>%
-    rvest::html_nodes("[class='col_heading center']") %>%
-    rvest::html_text() %>%
-    expect_equal(base::setdiff(colnames(mtcars_short), "mpg"))
+  # Expect that supplying a column name that doesn't exist in the
+  # table boxhead will result in an error
+  expect_error(
+    gt(data = mtcars_short) %>%
+      cols_align(align = "right", columns = vars(car)))
 })
