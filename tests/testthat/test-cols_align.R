4637bd39ff97d3d956aa17cb807609c7ed9bb2cb
Richard Iannone <riannone@me.com>
2018-10-02 21:07:48 -0700

    Revise testthat tests for `cols_align()`

--- tests/testthat/test-cols_align.R
+++ tests/testthat/test-cols_align.R
@@ -3,6 +3,12 @@ context("Ensuring that the `cols_align*()` work as expected")
 # Create a shorter version of `mtcars`
 mtcars_short <- mtcars[1:5, ]
 
+# Create a data frame based on the internal `sp500.csv`
+sp500 <-
+  read.csv(
+    system.file("extdata", "sp500.csv", package = "gt"),
+    stringsAsFactors = FALSE)
+
 # Function to skip tests if Suggested packages not available on system
 check_suggests <- function() {
   skip_if_not_installed("rvest")
@@ -60,11 +66,85 @@ test_that("the function `cols_align()` works as expected", {
   # or `right` will result in an error
   expect_error(
     gt(data = mtcars_short) %>%
-      cols_align(align = "right", columns = vars(mpg, cyl, drat)))
+      cols_align(align = "righter", columns = vars(mpg, cyl, drat)))
 
   # Expect that supplying a column name that doesn't exist in the
   # table boxhead will result in an error
   expect_error(
     gt(data = mtcars_short) %>%
       cols_align(align = "right", columns = vars(car)))
+
+  # Create a `tbl_html` object with `gt()`; align all
+  # columns to the left
+  tbl_html <-
+    gt(data = mtcars_short) %>%
+    cols_align(align = "left") %>%
+    render_as_html() %>%
+    xml2::read_html()
+
+  # Expect that the columns with class `col_heading left`
+  # includes all columns in `mtcars_short`
+  tbl_html %>%
+    rvest::html_nodes("[class='col_heading left']") %>%
+    rvest::html_text() %>%
+    expect_equal(colnames(mtcars_short))
+
+  # Create a `tbl_html` object with `gt()`; align all
+  # columns (using `columns = TRUE`) to the left
+  tbl_html <-
+    gt(data = mtcars_short) %>%
+    cols_align(align = "left", columns = TRUE) %>%
+    render_as_html() %>%
+    xml2::read_html()
+
+  # Expect that the columns with class `col_heading left`
+  # includes all columns in `mtcars_short`
+  tbl_html %>%
+    rvest::html_nodes("[class='col_heading left']") %>%
+    rvest::html_text() %>%
+    expect_equal(colnames(mtcars_short))
+
+  # Create a `tbl_html` object with the `sp500` data
+  # frame and `auto`-align all columns
+  tbl_html <-
+    gt(data = sp500) %>%
+    cols_align(align = "auto") %>%
+    render_as_html() %>%
+    xml2::read_html()
+
+  # Expect that the `Date` column is left-formatted because
+  # the column is of the `character` class
+  tbl_html %>%
+    rvest::html_nodes("[class='col_heading left']") %>%
+    rvest::html_text() %>%
+    expect_equal("Date")
+
+  # Expect that the all other columns are right-formatted because
+  # those columns are of the `numeric` class
+  tbl_html %>%
+    rvest::html_nodes("[class='col_heading right']") %>%
+    rvest::html_text() %>%
+    expect_equal(base::setdiff(colnames(sp500), "Date"))
+
+  # Create a `tbl_html` object with the `sp500` data
+  # frame and `auto`-align just the `Low` and `Close` columns
+  tbl_html <-
+    gt(data = sp500) %>%
+    cols_align(align = "auto", columns = vars(Low, Close)) %>%
+    render_as_html() %>%
+    xml2::read_html()
+
+  # Expect that the `Low` and `Close` columns are right-formatted because
+  # these columns are of the `numeric` class
+  tbl_html %>%
+    rvest::html_nodes("[class='col_heading right']") %>%
+    rvest::html_text() %>%
+    expect_equal(c("Low", "Close"))
+
+  # Expect that all other columns are center-formatted because
+  # that is the default alignment
+  tbl_html %>%
+    rvest::html_nodes("[class='col_heading center']") %>%
+    rvest::html_text() %>%
+    expect_equal(base::setdiff(colnames(sp500), c("Low", "Close")))
 })
