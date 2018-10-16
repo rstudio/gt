f28d052477ac1c6521ff64ff596e956dcefd67cd
Richard Iannone <riannone@me.com>
2018-10-16 17:24:31 -0400

    Rename file containing Latex utility fcns

--- R/utils_render_latex.R
+++ R/utils_render_latex.R
@@ -40,6 +40,101 @@ create_footnote_component_latex <- function(footnotes_resolved,
 }
 
 #' @noRd
+create_boxhead_component_latex <- function(boxh_df,
+                                           output_df,
+                                           stub_available,
+                                           spanners_present,
+                                           stubhead_caption,
+                                           col_alignment) {
+
+  # Get the headings
+  headings <- names(output_df)
+
+  # Merge the heading labels
+  headings_rev <- headings %>% rev()
+  labels_rev <- boxh_df[2, ] %>% unname() %>% t() %>% as.vector() %>% rev()
+
+  for (i in seq(labels_rev)) {
+    headings_rev[i] <- labels_rev[i]
+  }
+  headings <- rev(headings_rev)
+
+  # If `stub_available` == TRUE, then replace with a set stubhead
+  #   caption or nothing
+  if (stub_available &&
+      length(stubhead_caption) > 0 &&
+      "rowname" %in% headings) {
+
+    headings[which(headings == "rowname")] <- stubhead_caption$stubhead_caption
+
+  } else if ("rowname" %in% headings) {
+
+    headings[which(headings == "rowname")] <- ""
+  }
+
+  table_col_headings <-
+    paste0(latex_heading_row(content = headings), collapse = "")
+
+  if (spanners_present) {
+
+    # spanners
+    spanners <- boxh_df[1, ] %>% t() %>% as.vector()
+
+    if (stub_available) {
+      spanners <- c(NA_character_, spanners)
+    }
+
+    for (i in seq(spanners)) {
+      if (is.na(spanners[i])) {
+        spanners[i] <- headings[i]
+      }
+    }
+
+    spanners_lengths <- rle(spanners)
+
+    multicol <- c()
+    cmidrule <- c()
+
+    for (i in seq(spanners_lengths$lengths)) {
+
+      if (spanners_lengths$lengths[i] > 1) {
+
+        multicol <-
+          c(multicol,
+            paste0(
+              "\\multicolumn{", spanners_lengths$lengths[i],
+              "}{c}{",
+              spanners_lengths$values[i] %>% tidy_gsub("_", "\\\\_"),
+              "}"))
+
+        cmidrule <-
+          c(cmidrule,
+            paste0(
+              "\\cmidrule{",
+              sum(spanners_lengths$lengths[1:i]) - spanners_lengths$lengths[i] + 1,
+              "-",
+              sum(spanners_lengths$lengths[1:i]),
+              "}"))
+
+      } else {
+        multicol <- c(multicol, " & ")
+      }
+
+    }
+
+    multicol <- paste0(paste(multicol, collapse = ""), "\\\\ \n")
+    cmidrule <- paste0(paste(cmidrule, collapse = ""), "\n")
+
+    table_col_spanners <- paste(multicol, cmidrule, collapse = "")
+  } else {
+
+    table_col_spanners <- ""
+  }
+
+  paste0(table_col_spanners, table_col_headings)
+}
+
+#' @noRd
 latex_body_row <- function(content,
                            type) {
 
@@ -103,6 +198,7 @@ latex_tabular <- function(col_alignment) {
 
   paste0(
     "\\centering",
+    "\\vspace*{-3mm}",
     "\\begin{tabular}{",
     col_alignment %>% substr(1, 1) %>% paste(collapse = ""),
     "}\n",
