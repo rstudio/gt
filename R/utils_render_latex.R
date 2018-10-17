65cf7f1a9cdff42375544ce569fcf8473aad6bf5
Richard Iannone <riannone@me.com>
2018-10-17 00:04:41 -0400

    Add the `create_body_component_latex()` fcn

--- R/utils_render_latex.R
+++ R/utils_render_latex.R
@@ -126,12 +126,113 @@ create_boxhead_component_latex <- function(boxh_df,
     cmidrule <- paste0(paste(cmidrule, collapse = ""), "\n")
 
     table_col_spanners <- paste(multicol, cmidrule, collapse = "")
+
   } else {
 
     table_col_spanners <- ""
   }
 
   paste0(table_col_spanners, table_col_headings)
+}
+
+#' @importFrom dplyr mutate filter pull
+#' @noRd
+create_body_component_latex <- function(row_splits,
+                                        groups_rows_df,
+                                        col_alignment,
+                                        stub_available,
+                                        summaries_present,
+                                        list_of_summaries,
+                                        n_rows,
+                                        n_cols) {
+
+  # Replace an NA group with an empty string
+  if (any(is.na(groups_rows_df$group))) {
+
+    groups_rows_df <-
+      groups_rows_df %>%
+      dplyr::mutate(
+        group_label = ifelse(
+          is.na(group_label), "\\vspace*{-5mm}", group_label)) %>%
+      dplyr::mutate(
+        group_label = gsub("^NA", "\\textemdash", group_label))
+  }
+
+  body_rows <- c()
+  for (i in 1:n_rows) {
+
+    # Process group rows
+    if (!is.null(groups_rows_df) &&
+        i %in% groups_rows_df$row) {
+
+      if (i == 1) {
+
+        body_rows <-
+          c(body_rows,
+            latex_group_row(
+              groups_rows_df[which(groups_rows_df$row %in% i), "group_label"][[1]],
+              top_border = FALSE))
+
+      } else if (i == n_rows) {
+
+        body_rows <-
+          c(body_rows,
+            latex_group_row(
+              groups_rows_df[which(groups_rows_df$row %in% i), "group_label"][[1]],
+              bottom_border = FALSE))
+
+      } else {
+
+        body_rows <-
+          c(body_rows,
+            latex_group_row(
+              groups_rows_df[which(groups_rows_df$row %in% i), "group_label"][[1]]))
+      }
+    }
+
+    # Process "data" rows
+    body_rows <-
+      c(body_rows, latex_body_row(row_splits[[i]], type = "row"))
+
+    # Process summary rows
+    if (stub_available && summaries_present &&
+        i %in% groups_rows_df$row_end) {
+
+      group <-
+        groups_rows_df %>%
+        dplyr::filter(row_end == i) %>%
+        dplyr::pull(group)
+
+      if (group %in% names(list_of_summaries$summary_df_display_list)) {
+
+        summary_df <-
+          list_of_summaries$summary_df_display_list[[
+            which(names(list_of_summaries$summary_df_display_list) == group)]] %>%
+          as.data.frame(stringsAsFactors = FALSE)
+
+        body_content_summary <-
+          as.vector(t(summary_df)) %>%
+          tidy_gsub("\u2014", "-")
+
+        row_splits_summary <-
+          split_body_content(
+            body_content = body_content_summary,
+            n_cols = n_cols)
+
+        for (j in seq(length(row_splits_summary))) {
+
+          if (j == 1) {
+            body_rows <- c(body_rows, "\\midrule \n")
+          }
+
+          body_rows <-
+            c(body_rows, latex_body_row(row_splits_summary[[j]], type = "row"))
+        }
+      }
+    }
+  }
+
+  paste0(body_rows, collapse = "")
 }
 
 #' @noRd
