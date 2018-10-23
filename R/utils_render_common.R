e542bd9095d400a2c080b02bc64f73efc20e43b8
Richard Iannone <riannone@me.com>
2018-10-23 00:44:44 -0700

    Remove test helper function from within /R dir

--- R/utils_render_common.R
+++ R/utils_render_common.R
@@ -71,48 +71,6 @@ render_formats <- function(output_df,
   output_df
 }
 
-# Testable version of the `render_formats()` function
-render_formats_test <- function(data,
-                                context) {
-
-  data_attr <- attributes(data)
-
-  # Move original data frame to `data_df`
-  data_df <- as.data.frame(data)
-
-  # Initialize `output_df`
-  output_df <- initialize_output_df(data_df)
-
-  # Get the `formats` list
-  formats <- data_attr$formats
-
-  # Render input data to output data where formatting
-  # is specified
-  for (fmt in formats)  {
-
-    # Determine if the formatter has a function relevant
-    # to the context; if not, use the `default` function
-    # (which should always be present)
-    if (context %in% names(fmt$func)) {
-      eval_func <- context
-    } else {
-      eval_func <- "default"
-    }
-
-    for (col in fmt[["cols"]]) {
-
-      # Perform rendering but only do so if the column is present
-      if (col %in% colnames(data_df)) {
-
-        output_df[[col]][fmt$rows] <-
-          fmt$func[[eval_func]](data_df[[col]][fmt$rows])
-      }
-    }
-  }
-
-  output_df
-}
-
 # Move input data cells to `output_df` that didn't have any rendering applied
 # during the `render_formats()` call
 migrate_unformatted_to_output <- function(data_df,
