056d03f0dd97fa662034fd1eff1a4eb041d17860
Richard Iannone <riannone@me.com>
2018-10-26 19:31:01 -0700

    Make changes to the processing of output text (#48)
    * Add `markdown_to_text()` util function
    
    * Modify `tidy_gsub()` call
    
    * Modify `process_text()` to handle multiple contexts
    
    * Add `n_cols` argument
    
    * Remove uses of text processing functions
    
    * Extract `source_note` as vector
    
    * Add comments
    
    * Add and use text-handling functions
    
    * Add to NAMESPACE
    
    * Remove some `process_text()` calls
    
    * Use `rlang::enquo()` (and `!!`) for all `rows`
    
    * Add tests for conditional formatting on rows
    
    * Move text-processing fcns to a separate file
    
    * Use `exact = TRUE` when `attr()` is getter
    
    * Remove TODO comment

--- R/table_parts.R
+++ R/table_parts.R
@@ -23,14 +23,9 @@ tab_heading <- function(data,
                         title,
                         headnote = NULL) {
 
-  # Process the `title` text
-  title <- process_text(title)
-
-  # Process the `headnote` text
+  # Handle the optional `headnote` text
   if (is.null(headnote)) {
     headnote <- ""
-  } else {
-    headnote <- process_text(headnote)
   }
 
   attr(data, "heading") <-
@@ -64,7 +59,7 @@ tab_stubhead_caption <- function(data,
                                  caption) {
 
   attr(data, "stubhead_caption") <-
-    list(stubhead_caption = process_text(caption))
+    list(stubhead_caption = caption)
 
   data
 }
@@ -135,13 +130,14 @@ tab_stub_block <- function(data,
     # Insert the group into the `arrange_groups` component
     if (!("arrange_groups" %in% names(attributes(data)))) {
 
-      if (any(is.na(attr(data, "stub_df")$groupname))) {
+      if (any(is.na(attr(data, "stub_df", exact = TRUE)$groupname))) {
 
         attr(data, "arrange_groups") <-
           list(groups = c(process_text(group[1]), NA_character_))
 
       } else {
-        attr(data, "arrange_groups") <- list(groups = process_text(group[1]))
+        attr(data, "arrange_groups") <-
+          list(groups = process_text(group[1]))
       }
 
     } else {
@@ -149,12 +145,14 @@ tab_stub_block <- function(data,
       if (any(is.na(attr(data, "stub_df")$groupname))) {
 
         attr(data, "arrange_groups")[["groups"]] <-
-          c(attr(data, "arrange_groups")[["groups"]], process_text(group[1]), NA_character_) %>%
+          c(attr(data, "arrange_groups", exact = TRUE)[["groups"]],
+            process_text(group[1]), NA_character_) %>%
           unique()
 
       } else {
         attr(data, "arrange_groups")[["groups"]] <-
-          c(attr(data, "arrange_groups")[["groups"]], process_text(group[1]))
+          c(attr(data, "arrange_groups", exact = TRUE)[["groups"]],
+            process_text(group[1]))
       }
     }
   }
@@ -229,17 +227,16 @@ tab_boxhead_panel <- function(data,
 tab_source_note <- function(data,
                             source_note) {
 
-  source_note <- process_text(source_note)
-
   if ("source_note" %in% names(attributes(data))) {
 
-    attr(data, "source_note")[["source_note"]] <-
-      c(attr(data, "source_note")[["source_note"]], source_note)
+    attr(data, "source_note") <-
+      c(attr(data, "source_note", exact = TRUE),
+        list(source_note))
 
   } else {
 
     attr(data, "source_note") <-
-      list(source_note = source_note)
+      list(source_note)
   }
 
   data
