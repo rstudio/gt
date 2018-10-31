4e41a88bb23cb3762714de149919a47e36870bdf
Richard Iannone <riannone@me.com>
2018-10-31 02:10:47 -0700

    Store column group labels in internal list obj

--- R/table_parts.R
+++ R/table_parts.R
@@ -196,14 +196,22 @@ tab_boxhead_panel <- function(data,
 
   # Filter the vector of column names by the
   # column names actually in `input_df`
-  columns <-
-    columns[which(columns %in% colnames(data))]
+  columns <- columns[which(columns %in% colnames(data))]
 
   if (length(columns) == 0) {
     return(data)
   }
 
-  attr(data, "boxh_df")["group_label", columns] <- process_text(group)
+  # Extract the `grp_labels` list from `data`
+  grp_labels <- attr(data, "grp_labels", exact = TRUE)
+
+  for (i in seq(columns)) {
+    grp_labels[[columns[i]]] <- group
+  }
+
+  # Set the `grp_labels` attr with the `grp_labels` object
+  attr(data, "grp_labels") <- grp_labels
+
   data
 }
 
