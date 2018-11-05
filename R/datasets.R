c68e7a1b5d3f873cf8b4feadc24b56a170bb1818
Richard Iannone <riannone@me.com>
2018-11-05 01:37:23 -0800

    Modify documentation for `countrypops`

--- R/datasets.R
+++ R/datasets.R
@@ -1,11 +1,12 @@
-#' Yearly populations of countries from 1960 to 2017 (Wide Format)
+#' Yearly populations of countries from 1960 to 2017
 #'
-#' A dataset that presents yearly populations of countries. The
-#' country identifiers include the English country names, and the 2- and
-#' 3-letter ISO 3166-1 country codes. Each row contains a \code{population}
-#' value for a given \code{year} (from 1960 to 2017). Any \code{NA} values
-#' for \code{populations} indicate the non-existence of the country during
-#' that year.
+#' A dataset that presents yearly, total populations of countries. Total
+#' population is based on counts of all residents regardless of legal status or
+#' citizenship. Country identifiers include the English-language country names,
+#' and the 2- and 3-letter ISO 3166-1 country codes. Each row contains a
+#' \code{population} value for a given \code{year} (from 1960 to 2017). Any
+#' \code{NA} values for \code{populations} indicate the non-existence of the
+#' country during that year.
 #'
 #' @format A tibble with 12470 rows and 5 variables:
 #' \describe{
