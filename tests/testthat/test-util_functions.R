968957e506673f010d97691ca7e2279668c6d763
Richard Iannone <riannone@me.com>
2018-12-07 00:01:03 -0500

    Update pkgdown site

--- tests/testthat/test-util_functions.R
+++ tests/testthat/test-util_functions.R
@@ -380,7 +380,7 @@ test_that("the `get_css_tbl()` function works correctly", {
 
   css_tbl %>% expect_is(c("tbl_df", "tbl", "data.frame"))
 
-  css_tbl %>% dim() %>% expect_equal(c(96, 4))
+  css_tbl %>% dim() %>% expect_equal(c(97, 4))
 
   css_tbl %>%
     colnames() %>%
@@ -403,7 +403,7 @@ test_that("the `inline_html_styles()` function works correctly", {
   # Expect that certain portions of `inlined_html` have
   # inlined CSS rules
   expect_true(
-    grepl("style=\"font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', Arial, sans-serif;border-collapse:collapse;margin-left:auto;margin-right:auto;color:#000000;font-size:16px;background-color:#FFFFFF;width:auto;border-top-style:solid;border-top-width:2px;border-top-color:#A8A8A8;\"", inlined_html)
+    grepl("style=\"font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', Arial, sans-serif;display:table;border-collapse:collapse;margin-left:auto;margin-right:auto;color:#000000;font-size:16px;background-color:#FFFFFF;width:auto;border-top-style:solid;border-top-width:2px;border-top-color:#A8A8A8;\"", inlined_html)
   )
 
   expect_true(
