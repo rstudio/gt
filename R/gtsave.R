6b54c98817210fdcc31fdff43814f30a1e7788e3
Richard Iannone <riannone@me.com>
2019-01-10 14:11:39 -0500

    Remove `save_fn` arg from `gtsave()`

--- R/gtsave.R
+++ R/gtsave.R
@@ -2,29 +2,20 @@
 #'
 #' The \code{gtsave()} function makes it easy to save a \pkg{gt} table to a
 #' file. The function guesses the file type by the extension provided in the
-#' output filename. It's also possible to create a custom output solution by
-#' providing a function to the \code{save_fn} argument. Just be sure to use
-#' \code{(data, filename, ...)} in the function signature.
+#' output filename.
+#'
 #' @param data a table object that is created using the \code{\link{gt}()}
 #'   function.
 #' @param filename the file name to create on disk. Ensure that an extension
 #'   compatible with the output types is provided (\code{.html}, \code{.tex},
 #'   \code{.ltx}, \code{.rtf}). If a custom save function is provided then the
 #'   file extension is disregarded.
-#' @param save_fn a custom save function to allow for saving the table object
-#'   through some means not already provided.
-#' @param ... all other options passed to the \code{save_fn} function.
+#' @param ... all other options passed to the next method.
 #' @export
-gtsave <- function(data, filename, save_fn = NULL, ...) {
+gtsave <- function(data, filename, ...) {
 
   # Input object validation
   stop_if_not_gt(data)
-
-  # Filename parsing and method selection
-  if (is.function(save_fn)) {
-
-    return(save_fn(data, filename, ...))
-  }
 
   # Attach extension as the first class value
   class(data) <- c(gtsave_file_ext(filename), class(data))
@@ -32,7 +23,9 @@ gtsave <- function(data, filename, save_fn = NULL, ...) {
   UseMethod("gtsave", data)
 }
 
-gtsave.default <- gtsave.gt_tbl <- function(data, filename, ...) {
+#' @export
+#' @noRd
+gtsave.gt_tbl <- function(data, filename, ...) {
 
   stop("`gtsave.", gtsave_file_ext(filename),
        "(data, filename, ...)` has not been implemented.",
