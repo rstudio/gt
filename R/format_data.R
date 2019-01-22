99370eeb513a7cccda955ae1ddbf9cd07cc2b55a
Steve Putman <steve.putman@gmail.com>
2019-01-22 17:17:32 -0600

    Changes to fmt_number for Latex alignment using parens

--- R/format_data.R
+++ R/format_data.R
@@ -178,6 +178,45 @@ fmt_number <- function(data,
           pre_post_txt <- get_pre_post_txt(pattern)
           x_str[non_na_x] <- paste0(pre_post_txt[1], x_str[non_na_x], pre_post_txt[2])
           x_str
+        },
+        latex = function(x) {
+          
+          # Determine which of `x` are not NA
+          non_na_x <- !is.na(x)
+          
+          # Create `x_str` with same length as `x`
+          x_str <- rep(NA_character_, length(x))
+          
+          # Format all non-NA x values
+          x_str[non_na_x] <-
+            formatC(
+              x = x[non_na_x] * scale_by,
+              digits = decimals,
+              mode = "double",
+              big.mark = sep_mark,
+              decimal.mark = dec_mark,
+              format = "f",
+              drop0trailing = drop_trailing_zeros)
+          
+          # Handle negative values
+          if (negative_val == "parens") {
+            
+            # Determine which of `x` are not NA and also negative
+            negative_x <- x < 0 & !is.na(x)
+            
+            # Apply parentheses to the formatted value and remove
+            # the minus sign
+            x_str[negative_x] <- paste0("(", gsub("^-", "", x_str[negative_x]), ")")
+            
+            # Add phantom space to non-negative numbers for alignment
+            nonneg_x <- x >= 0 & !is.na(x)
+            x_str[nonneg_x] <- paste0(x_str[nonneg_x], "\hphantom{)}")
+          }
+          
+          # Handle formatting of pattern
+          pre_post_txt <- get_pre_post_txt(pattern)
+          x_str[non_na_x] <- paste0(pre_post_txt[1], x_str[non_na_x], pre_post_txt[2])
+          x_str
         }
       ))
 }
