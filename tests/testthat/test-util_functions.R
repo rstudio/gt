d1a64a2c215d0dcf2664a1fc752906d88dd1ddd8
Richard Iannone <riannone@me.com>
2019-02-22 00:39:18 -0500

    Modify an existing testthat test

--- tests/testthat/test-util_functions.R
+++ tests/testthat/test-util_functions.R
@@ -380,7 +380,7 @@ test_that("the `get_css_tbl()` function works correctly", {
 
   css_tbl %>% expect_is(c("tbl_df", "tbl", "data.frame"))
 
-  css_tbl %>% dim() %>% expect_equal(c(101, 4))
+  css_tbl %>% dim() %>% expect_equal(c(102, 4))
 
   css_tbl %>%
     colnames() %>%
