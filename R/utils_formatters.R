cd12e46ca1fcf6beeb9e8791b57ee257324a8b15
Richard Iannone <riannone@me.com>
2019-03-19 15:26:37 -0400

    Add new `get_locale_sep_mark()` util function

--- R/utils_formatters.R
+++ R/utils_formatters.R
@@ -41,3 +41,47 @@ pull_table_value_from_column <- function(table, column) {
     dplyr::pull(!!column_enquo)
 }
 
+#' Get the `sep_mark` value based on a locale
+#' @param locale The user-supplied `locale` value, found in several `fmt_*()`
+#'   functions. This is expected as `NULL` if not supplied by the user.
+#' @param default The default value for the `sep_mark`.
+#' @param use_seps A logical value for whether to use separators at all.
+#' @importFrom dplyr filter pull
+#' @noRd
+get_locale_sep_mark <- function(locale = NULL,
+                                default,
+                                use_seps) {
+
+  # If `use_seps` is FALSE, then force
+  # `sep_mark` to be an empty string
+  if (!use_seps) {
+    return("")
+  }
+
+  # If `locale` is NULL then return the
+  # default `sep_mark`
+  if (is.null(locale)) {
+    return(default)
+  }
+
+  # Stop function if the `locale` provided
+  # isn't a valid one
+  if (!(locale %in% locales$base_locale_id)) {
+    stop("The supplied `locale` is not available in the list of supported locales.\n",
+         " * Use the `info_locales()` function to see which locales can be used.",
+         call. = FALSE)
+  }
+
+  # If the locale is supplied and valid, get
+  # the correct `group_sep` value from the
+  # `gt:::locales` lookup table
+  if (locale %in% locales$base_locale_id) {
+
+    sep_mark <-
+      filter_table_to_row(gt:::locales, base_locale_id == locale) %>%
+      pull_table_value_from_column(column = group_sep)
+
+    return(sep_mark)
+  }
+}
+
