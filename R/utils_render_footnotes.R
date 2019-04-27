359d7734362a0bb2b3b7f3f381eeb8eb1736e7d5
Richard Iannone <riannone@me.com>
2019-04-27 01:57:16 -0400

    Add the `set_footnote_glyphs_stubhead_label()` util fcn

--- R/utils_render_footnotes.R
+++ R/utils_render_footnotes.R
@@ -423,6 +423,58 @@ set_footnote_glyphs_columns <- function(footnotes_resolved,
   boxh_df
 }
 
+
+#' @importFrom dplyr filter group_by mutate ungroup select distinct pull
+#' @noRd
+set_footnote_glyphs_stubhead_label <- function(footnotes_resolved,
+                                               stubhead_label,
+                                               output = "html") {
+
+  # Get the resolved footnotes
+  footnotes_tbl <- footnotes_resolved
+
+  if ("stubhead_label" %in% footnotes_tbl$locname) {
+
+    footnotes_tbl <-
+      footnotes_tbl %>%
+      dplyr::filter(locname == "stubhead_label")
+
+    if (nrow(footnotes_tbl) > 0) {
+
+      footnotes_stubhead_label_glyphs <-
+        footnotes_tbl %>%
+        dplyr::group_by(grpname) %>%
+        dplyr::mutate(fs_id_coalesced = paste(fs_id, collapse = ",")) %>%
+        dplyr::ungroup() %>%
+        dplyr::select(grpname, fs_id_coalesced) %>%
+        dplyr::distinct() %>%
+        dplyr::pull(fs_id_coalesced)
+
+      text <- stubhead_label$stubhead_label
+
+      if (output == "html") {
+
+        text <-
+          paste0(text, footnote_glyph_to_html(footnotes_stubhead_label_glyphs))
+
+      } else if (output == "rtf") {
+
+        text <-
+          paste0(text, footnote_glyph_to_rtf(footnotes_stubhead_label_glyphs))
+
+      } else if (output == "latex") {
+
+        text <-
+          paste0(text, footnote_glyph_to_latex(footnotes_stubhead_label_glyphs))
+      }
+
+      stubhead_label$stubhead_label <- text
+    }
+  }
+
+  stubhead_label
+}
+
 # Apply footnotes to the data rows
 #' @importFrom dplyr filter group_by mutate ungroup select distinct
 #' @noRd
