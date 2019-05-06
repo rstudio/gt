bbdfe823c293d83fb0c175b989df4aec1d41fe1d
Richard Iannone <riannone@me.com>
2019-05-06 11:28:48 -0400

    Add several testthat tests

--- tests/testthat/test-gt_object.R
+++ tests/testthat/test-gt_object.R
@@ -5,13 +5,26 @@ test_that("a gt table object contains the correct components", {
   # Create a `gt_tbl` object with `gt()`
   tab <- gt(data = iris)
 
+  # Expect that the `gt_tbl` object has all of the
+  # usual components and that they have all of the
+  # expected dimensions and features
   expect_tab(tab, iris)
 
+  # Expect that the `stub_df` data frame is correctly
+  # formed given the input rownames and groupnames
   expect_tab_colnames(
     tab, df = iris,
     rowname = "NA",
     groupname_is_na = TRUE)
 
+  # Create a `gt_tbl` object with `gt()` and a
+  # grouped version of the `iris` dataset
+  tab <- gt(data = iris %>% dplyr::group_by(Species))
+
+  # Expect that the `gt_tbl` object has all of the
+  # usual components and that they have all of the
+  # expected dimensions and features
+  expect_tab(tab, df = iris %>% dplyr::group_by(Species))
 })
 
 test_that("a gt table can be made to use the rownames of a data frame", {
@@ -20,8 +33,13 @@ test_that("a gt table can be made to use the rownames of a data frame", {
   # data frame's row names as row names in the stub
   tab <- gt(data = mtcars, rownames_to_stub = TRUE)
 
+  # Expect that the `gt_tbl` object has all of the
+  # usual components and that they have all of the
+  # expected dimensions and features
   expect_tab(tab, mtcars)
 
+  # Expect that the `stub_df` data frame is correctly
+  # formed given the input rownames and groupnames
   expect_tab_colnames(
     tab, df = mtcars,
     rowname = "tibble",
@@ -42,8 +60,13 @@ test_that("a gt table can be made with the stub partially or fully populated", {
   # `data_r` dataset
   tab <- gt(data = data_r)
 
+  # Expect that the `gt_tbl` object has all of the
+  # usual components and that they have all of the
+  # expected dimensions and features
   expect_tab(tab, data_r, has_rownames = TRUE)
 
+  # Expect that the `stub_df` data frame is correctly
+  # formed given the input rownames and groupnames
   expect_tab_colnames(
     tab, df = data_r,
     rowname = "col",
@@ -62,11 +85,46 @@ test_that("a gt table can be made with the stub partially or fully populated", {
   # `data_rg` dataset
   tab <- gt(data = data_rg)
 
+  # Expect that the `gt_tbl` object has all of the
+  # usual components and that they have all of the
+  # expected dimensions and features
   expect_tab(
     tab, data_rg, has_groupnames = TRUE, has_rownames = TRUE)
 
+  # Expect that the `stub_df` data frame is correctly
+  # formed given the input rownames and groupnames
   expect_tab_colnames(
     tab, df = data_rg,
     rowname = "col",
     groupname_is_na = FALSE)
+})
+
+test_that("a gt table can be made from a table with no rows", {
+
+  # Create an input data frame based on the exibble
+  # dataset, except with no rows
+  data_e <- exibble %>% head(0)
+
+  # Create a `gt_tbl` object with `gt()` and the
+  # `data_e` dataset
+  tab <- gt(data = data_e)
+
+  # Expect that the `gt_tbl` object has all of the
+  # usual components and that they have all of the
+  # expected dimensions and features
+  expect_tab(tab, data_e)
+
+  # Expect that the `stub_df` data frame is empty
+  attr(tab, "stub_df") %>%
+    nrow() %>%
+    expect_equal(0)
+
+  # Create a `gt_tbl` object with `gt()` and a
+  # grouped version of the `data_e` dataset
+  tab <- gt(data = data_e %>% dplyr::group_by(group))
+
+  # Expect that the `gt_tbl` object has all of the
+  # usual components and that they have all of the
+  # expected dimensions and features
+  expect_tab(tab, df = data_e %>% dplyr::group_by(group))
 })
