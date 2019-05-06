77adaf9a8f03fc123f845397306f1af3d7a86d3a
Richard Iannone <riannone@me.com>
2019-05-06 01:11:04 -0400

    Modify a conditional statement

--- R/utils_render_html.R
+++ R/utils_render_html.R
@@ -706,11 +706,11 @@ create_body_component_h <- function(row_splits_body,
 
   body_rows <- c()
 
-  for (i in 1:n_rows) {
   if (n_rows == 0) {
     n_rows <- 1
   }
 
+  for (i in seq(n_rows)) {
 
     if (!is.null(groups_rows_df) &&
         i %in% groups_rows_df$row) {
