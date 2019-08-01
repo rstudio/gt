77145f40baecc583b8a559ffe449492d32a256d5
Richard Iannone <riannone@me.com>
2019-08-01 15:22:49 -0400

    Modify some testthat tests

--- tests/testthat/test-location_cells.R
+++ tests/testthat/test-location_cells.R
@@ -267,15 +267,14 @@ test_that("the `cells_summary()` function works correctly", {
   # Expect the RHS of the first component formula to contain
   # the vector provided
   helper_cells_summary[[1]] %>%
-    rlang::quo_squash() %>%
+    rlang::eval_tidy() %>%
     expect_equal("group_a")
 
   # Expect the RHS of the second component formula to contain
   # the vector provided
   helper_cells_summary[[2]] %>%
-    rlang::quo_squash() %>%
-    as.character() %>%
-    expect_equal(c("c", "col_1", "col_2"))
+    rlang::eval_tidy() %>%
+    expect_equal(c("col_1", "col_2"))
 
   # Create a `cells_summary` object with
   # columns in `vars()` provided to `columns`
