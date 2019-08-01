95d23d098404ed4feb8053bf553a7b826bd84953
Richard Iannone <riannone@me.com>
2019-08-01 18:44:05 -0400

    Fix for resolving row group cells (#335)
    * Update travis YAML file
    
    * Resolve row groups with a resolving fcn

--- R/tab_footnote.R
+++ R/tab_footnote.R
@@ -165,7 +165,17 @@ set_footnote.cells_column_labels <- function(loc, data, footnote) {
 
 set_footnote.cells_group <- function(loc, data, footnote) {
 
-  groups <- loc$groups %>% rlang::eval_tidy()
+  row_groups <- attr(data, "arrange_groups")$groups
+
+  # Resolve row groups
+  resolved_row_groups_idx <-
+    resolve_data_vals_idx(
+      var_expr = !!loc$groups,
+      data = NULL,
+      vals = row_groups
+    )
+
+  groups <- row_groups[resolved_row_groups_idx]
 
   attr(data, "footnotes_df") <-
     add_location_row_footnotes(
