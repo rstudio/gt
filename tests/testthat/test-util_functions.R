d7deb55b619e930d0588c1c81305edb3033ba3c8
Richard Iannone <riannone@me.com>
2019-08-16 14:39:47 -0400

    Fix a testthat test

--- tests/testthat/test-util_functions.R
+++ tests/testthat/test-util_functions.R
@@ -431,8 +431,8 @@ test_that("the `inline_html_styles()` function works correctly", {
   expect_true(
     grepl(
       paste0(
-        "style=\"padding: 8px; margin: 10px; border-bottom-style: solid; ",
-        "border-bottom-width: thin; border-bottom-color: #D3D3D3; ",
+        "style=\"padding: 8px; margin: 10px; border-top-style: solid; ",
+        "border-top-width: 1px; border-top-color: #D3D3D3; ",
         "vertical-align: middle; overflow-x: hidden; text-align: right; ",
         "font-variant-numeric: tabular-nums; font-size: 10px;\""
       ),
