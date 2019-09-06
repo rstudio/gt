652b215b4cee54bb0dff3be2d82669195553229b
Richard Iannone <riannone@me.com>
2019-09-06 00:41:05 -0400

    Restore RTF rendering support

--- R/utils_render_rtf.R
+++ R/utils_render_rtf.R
@@ -4,24 +4,167 @@ footnote_mark_to_rtf <- function(mark) {
   paste0("{\\super \\i ", mark, "}")
 }
 
-#' @noRd
-create_footnote_component_rtf <- function(footnotes_resolved,
-                                          opts_df,
-                                          body_content) {
+create_body_component_r <- function(data) {
 
-  # If the `footnotes_resolved` object has no
+  boxh <- dt_boxhead_get(data = data)
+  styles_tbl <- dt_styles_get(data = data)
+  body <- dt_body_get(data = data)
+
+  groups_rows_df <- attr(data, "groups_rows_df", exact = TRUE)
+  stub_components <- attr(data, "stub_components", exact = TRUE)
+  list_of_summaries <- attr(data, "list_of_summaries", exact = TRUE)
+  summaries_present <- attr(data, "summaries_present", exact = TRUE)
+
+  n_data_cols <- dt_boxhead_get_vars_default(data = data) %>% length()
+  n_rows <- nrow(body)
+
+  # Get the column alignments for the data columns (this
+  # doesn't include the stub alignment)
+  col_alignment <-
+    boxh %>%
+    dplyr::filter(type == "default") %>%
+    dplyr::pull(column_align)
+
+  # Get the column headings for the visible (e.g., `default`) columns
+  default_vars <- dt_boxhead_get_vars_default(data = data)
+
+  if ("rowname" %in% names(body)) {
+    default_vars <- c("rowname", default_vars)
+  }
+
+  # Determine whether the stub is available through analysis
+  # of the `stub_components`
+  if (is.null(stub_components)) {
+    stub_available <- FALSE
+  } else if (stub_component_is_groupname(stub_components)) {
+    stub_available <- FALSE
+  } else {
+    stub_available <- TRUE
+  }
+
+  if (stub_available) {
+    n_cols <- n_data_cols + 1
+  } else {
+    n_cols <- n_data_cols
+  }
+
+  # Split `body_content` by slices of rows and create data rows
+  body_content <- as.vector(t(body[, default_vars]))
+  row_splits <- split(body_content, ceiling(seq_along(body_content) / n_cols))
+
+  body_rows <- c()
+  for (i in seq_len(n_rows)) {
+
+    # Process group rows
+    if (!is.null(groups_rows_df) &&
+        i %in% groups_rows_df$row) {
+
+      body_rows <-
+        c(body_rows,
+          rtf_body_row(
+            c(
+              groups_rows_df[which(groups_rows_df$row %in% i), 1][[1]],
+              rep("", n_cols - 1)), type = "group"))
+    }
+
+    # Process "data" rows
+    if (i != length(row_splits)) {
+      body_rows <-
+        c(body_rows, rtf_body_row(row_splits[[i]], type = "row"))
+    } else {
+      body_rows <-
+        c(body_rows, rtf_last_body_row(content = row_splits[[i]]))
+    }
+
+    # Process summary rows
+    if (stub_available && summaries_present &&
+        i %in% groups_rows_df$row_end) {
+
+      group <-
+        groups_rows_df %>%
+        dplyr::filter(row_end == i) %>%
+        dplyr::pull(group)
+
+      if (group %in% names(list_of_summaries$summary_df_display_list)) {
+
+        summary_df <-
+          list_of_summaries$summary_df_display_list[[
+            which(names(list_of_summaries$summary_df_display_list) == group)]] %>%
+          as.data.frame(stringsAsFactors = FALSE)
+
+        body_content_summary <-
+          as.vector(t(summary_df)) %>%
+          tidy_gsub("\u2014", "-")
+
+        row_splits_summary <-
+          split_body_content(
+            body_content = body_content_summary,
+            n_cols = n_cols)
+
+        for (j in seq(length(row_splits_summary))) {
+
+          body_rows <-
+            c(body_rows, rtf_body_row(row_splits_summary[[j]], type = "row"))
+        }
+      }
+    }
+  }
+
+  paste0(body_rows, collapse = "")
+}
+
+create_source_notes_component_r <- function(data) {
+
+  source_notes <- dt_source_notes_get(data = data)
+
+  # Create the source note rows and handle any available footnotes
+  if (length(source_notes) != 0) {
+
+    # Create a source note
+    source_note_rows <-
+      paste0(
+        "\\pard\\pardeftab720\\sl288\\slmult1\\partightenfactor0\n",
+        paste0(
+          "\\cf0 \\strokec2 ", remove_html(text = source_notes), "\\\n",
+          collapse = ""),
+        collapse = "")
+  } else {
+    source_note_rows <- ""
+  }
+
+  source_note_rows
+}
+
+#' @noRd
+create_footnotes_component_r <- function(data) {
+
+  body <- dt_body_get(data = data)
+  opts_df <- dt_options_get(data = data)
+  footnotes_tbl <- dt_footnotes_get(data = data)
+
+  # Get the column headings for the visible (e.g., `default`) columns
+  default_vars <- dt_boxhead_get_vars_default(data = data)
+
+  if ("rowname" %in% names(body)) {
+    default_vars <- c("rowname", default_vars)
+  }
+
+  body_content <- as.vector(t(body[, default_vars]))
+
+  # If the `footnotes_tbl` object has no
   # rows, then return an empty footnotes component
-  if (nrow(footnotes_resolved) == 0) {
+  if (nrow(footnotes_tbl) == 0) {
     return("")
   }
 
   footnotes_tbl <-
-    footnotes_resolved %>%
-    dplyr::select(fs_id, text) %>%
+    footnotes_tbl %>%
+    dplyr::select(fs_id, footnotes) %>%
     dplyr::distinct()
 
   # Get the separator option from `opts_df`
-  separator <- opts_df %>%
+  separator <-
+    opts_df %>%
     dplyr::filter(parameter == "footnote_sep") %>%
     dplyr::pull(value)
 
@@ -32,17 +175,18 @@ create_footnote_component_rtf <- function(footnotes_resolved,
     tidy_gsub("&nbsp;", " ")
 
   # Create the footnotes block
-  footnote_component <-
+  footnotes_component <-
     paste0(
       "\\pard\\pardeftab720\\sl288\\slmult1\\partightenfactor0\n",
       paste0(
         # "\\f2\\i\\fs14\\fsmilli7333 \\super \\strokec2 ", footnotes_tbl[["fs_id"]],
         # "\\f0\\i0\\fs22 \\nosupersub \\strokec2 ",
         footnote_mark_to_rtf(footnotes_tbl[["fs_id"]]),
-        footnotes_tbl[["text"]] %>% remove_html(), "\\",
-        collapse = separator), "\n")
+        footnotes_tbl[["footnotes"]] %>% remove_html(), "\\",
+        collapse = separator), "\n"
+    )
 
-  footnote_component
+  footnotes_component
 }
 
 #' @noRd
@@ -363,4 +507,72 @@ split_body_content <- function(body_content,
   }
 
   split(body_content, ceiling(seq_along(body_content) / n_cols))
+}
+
+
+create_columns_component_r <- function(data) {
+
+  boxh <- dt_boxhead_get(data = data)
+  stubh <- dt_stubhead_get(data = data)
+
+  stub_available <- attr(data, "stub_available", exact = TRUE)
+  spanners_present <- attr(data, "spanners_present", exact = TRUE)
+
+  headings_vars <- boxh %>% dplyr::filter(type == "default") %>% dplyr::pull(var)
+  headings_labels <- dt_boxhead_get_vars_labels_default(data = data)
+
+  # If `stub_available` == TRUE, then replace with a set stubhead
+  # label or nothing
+  if (isTRUE(stub_available) && length(stubh$label) > 0) {
+
+    headings_labels <- prepend_vec(headings_labels, stubh$label)
+    headings_vars <- prepend_vec(headings_vars, "::stub")
+
+  } else if (isTRUE(stub_available)) {
+
+    headings_labels <- prepend_vec(headings_labels, "")
+    headings_vars <- prepend_vec(headings_vars, "::stub")
+  }
+
+  # Remove any HTML tags from `headings`
+  headings_labels <- remove_html(headings_labels)
+
+  if (spanners_present == FALSE) {
+
+    columns_component <-
+      paste0(rtf_heading_row(content = headings_labels), collapse = "")
+
+  } else {
+
+    # spanners
+    spanners <- dt_spanners_print(data = data, include_hidden = FALSE)
+
+    # Promote column labels to the group level wherever the
+    # spanner label is NA
+    spanners[is.na(spanners)] <- headings_vars[is.na(spanners)]
+
+    # Remove any HTML tags from `spanners`
+    spanners <- remove_html(spanners)
+
+    if (stub_available) {
+      spanners <- c(NA_character_, spanners)
+    }
+
+    # for (i in seq(spanners)) {
+    #   if (is.na(spanners[i])) {
+    #     spanners[i] <- headings[i]
+    #   }
+    # }
+
+    spanners_lengths <- rle(spanners)
+
+    columns_component <-
+      rtf_heading_group_row(
+        spanners_lengths = spanners_lengths,
+        headings = headings_labels,
+        spanners = spanners
+      )
+  }
+
+  columns_component
 }
