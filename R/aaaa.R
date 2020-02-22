1c9a62d40d47d452eba0c909bbea238bf658e62f
Devin Pastoor <devinp@metrumrg.com>
2020-02-22 16:15:59 -0500

    doc: add missing parameter definitions

--- R/aaaa.R
+++ R/aaaa.R
@@ -19,7 +19,9 @@ reset_latex_cache <- function() {
 #' Additional formatting options for as_latex()
 #' @param shrink The percentage to shrink the width by. Accepts a value < 1. Default is to stretch the table to full width of the page.
 #' @param line.breaks Allow/do not allow line breaks in the table. Default is to allow for line breaks under two circumstances: 1.) When the text of the table at the smallest point size (5pt) cannot fit to the width of the page. 2.) A minimal number of line breaks are required to make point size larger. In the first condition, setting this parameter to FALSE will not affect the output. In the second, the font size will be scaled down to remove the minimal breaks.
-#' @param column.sep set the
+#' @param column.sep set the column separator
+#' @param footnotes.align direction to align footnotes
+#' @param sourcenotes.align direction to align footnotes
 #' @export
 #'
 latex_tab.options <- function(shrink = NULL,
