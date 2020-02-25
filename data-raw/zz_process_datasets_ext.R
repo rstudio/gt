b5ef917be9a100786b6243ef2d97d92a1b6b4630
Richard Iannone <riannone@me.com>
2020-02-24 22:04:18 -0500

    Update zz_process_datasets_ext.R

--- data-raw/zz_process_datasets_ext.R
+++ data-raw/zz_process_datasets_ext.R
@@ -4,10 +4,9 @@ source("data-raw/X01-currencies.R")
 source("data-raw/X02-currency_symbols.R")
 source("data-raw/X03-locales.R")
 source("data-raw/X04-palettes_strips.R")
-source("data-raw/X05-colorized_previews.R")
 
 # Create internal datasets (`sysdata.rda`)
 usethis::use_data(
-  currencies, currency_symbols, locales, palettes_strips, preset_colorized_tbl,
+  currencies, currency_symbols, locales, palettes_strips,
   internal = TRUE, overwrite = TRUE
 )
