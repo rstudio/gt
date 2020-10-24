35355c4b77cb8dbdcbb5237533241be8d83c1764
Richard Iannone <riannone@me.com>
2020-10-23 21:07:31 -0400

    Spanner alignment correction (#662)
    * Add the `dt_boxhead_get_alignment_by_var()` util fcn
    
    * Undo forcing of subordinate column labels to be "center"
    
    * Align integer values to the right
    
    * Rearrange ordering of CSS classes for consistency
    
    * Add testthat test, checking alignments with `tab_spanner()`

--- R/dt_boxhead.R
+++ R/dt_boxhead.R
@@ -184,6 +184,14 @@ dt_boxhead_get_vars_labels_default <- function(data) {
     unlist()
 }
 
+dt_boxhead_get_alignment_by_var <- function(data, var) {
+
+  data %>%
+    dt_boxhead_get() %>%
+    dplyr::filter(var == !!var) %>%
+    magrittr::extract2("column_align")
+}
+
 check_names_dt_boxhead_expr <- function(expr) {
 
   if (!all(names(expr) %in% c(
