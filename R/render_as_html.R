2ff249285235328aef8793e64376a86c05374e2e
Richard Iannone <riannone@me.com>
2020-11-20 15:04:48 -0500

    Update render_as_html.R

--- R/render_as_html.R
+++ R/render_as_html.R
@@ -16,6 +16,8 @@ render_as_html <- function(data) {
   # Upgrade `_styles` to gain a `html_style` column with CSS style rules
   data <- add_css_styles(data = data)
 
+  caption_component <- create_caption_component_h(data = data)
+
   # Create the heading component
   heading_component <- create_heading_component(data = data, context = "html")
 
@@ -33,91 +35,6 @@ render_as_html <- function(data) {
 
   # Get attributes for the gt table
   table_defs <- get_table_defs(data = data)
-
-  # Create the table caption if available
-  table_caption <- dt_options_get_value(data = data, option = "table_caption")
-
-  if (!is.na(table_caption)) {
-
-    # Reference Implementation (knitr): knitr/table.R
-    # kable_caption = function(label, caption, format) {
-    #   # create a label for bookdown if applicable
-    #   if (is.null(label)) label = opts_current$get('label')
-    #   if (!is.null(caption) && !is.na(caption)) caption = paste0(
-    #     create_label('tab:', label, latex = (format == 'latex')), caption
-    #   )
-    #   caption
-    # }
-    #
-    # `create_label()`: knitr/utils.R
-    # # create \label{x} or (\#x); the latter is current an internal hack for bookdown
-    # create_label = function(..., latex = FALSE) {
-    #   if (isTRUE(opts_knit$get('bookdown.internal.label'))) {
-    #     lab1 = '(\\#'; lab2 = ')'
-    #   } else if (latex) {
-    #     lab1 = '\\label{'; lab2 = '}'
-    #   } else {
-    #     return('')  # we don't want the label at all
-    #   }
-    #   paste(c(lab1, ..., lab2), collapse = '')
-    # }
-
-    # Reference Implementation (bookdown): bookdown/html.R
-    # resolve_refs_html = function(content, global = FALSE) {
-    #   content = resolve_ref_links_html(content)
-    #
-    #   res = parse_fig_labels(content, global)
-    #   content = res$content
-    #   ref_table = c(res$ref_table, parse_section_labels(content))
-    #
-    #   # look for @ref(label) and resolve to actual figure/table/section numbers
-    #   m = gregexpr('(?<!\\\\)@ref\\(([-:[:alnum:]]+)\\)', content, perl = TRUE)
-    #   refs = regmatches(content, m)
-    #   for (i in seq_along(refs)) {
-    #     if (length(refs[[i]]) == 0) next
-    #     # strip off html tags when resolve numbers in <img>'s alt attribute because
-    #     # the numbers may contain double quotes, e.g. <img alt="<a
-    #     # href="#foo">1.2</a>"" width=...
-    #     ref = ref_to_number(refs[[i]], ref_table, FALSE)
-    #     if (is_img_line(content[i])) ref = strip_html(ref)
-    #     refs[[i]] = ref
-    #   }
-    #   regmatches(content, m) = refs
-    #   content
-    # }
-    #
-    # `resolve_ref_links_html()`
-    #
-    # `parse_fig_labels()`
-    #
-    # `parse_section_labels()`
-    #
-    # `ref_to_number()`
-    #
-    # `is_img_line()`
-    #
-    # `strip_html()`
-
-    # TODO: Create the <span> tag
-    chunk_name <- "chunk_name" # <- TODO: get the chunk name
-    span_id <- paste0("tab:", chunk_name)
-    table_number <- "1.1" # <- TODO: get the table number
-    span_content <- paste0("Table ", table_number, ": ")
-
-    # TODO: Create the <caption> tag
-    caption_component <-
-      htmltools::tags$caption(
-        htmltools::tags$span(
-          id = span_id,
-          span_content
-        ),
-        table_caption
-      )
-
-    heading_component <- ""
-  } else {
-    caption_component <- ""
-  }
 
   # Compose the HTML table
   as.character(
