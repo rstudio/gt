f94e74a7aad4e1c683a7d52cbfc006e2b321ef45
Richard Iannone <riannone@me.com>
2021-03-02 00:11:13 -0500

    Update test-resolver.R

--- tests/testthat/test-resolver.R
+++ tests/testthat/test-resolver.R
@@ -1,6 +1,4 @@
-test_that("resolve_cols", {
-  # resolve_cols_i
-  # resolve_cols_c
+test_that("`resolve_cols_i()` and `resolve_cols_c()` both work", {
 
   all_cols <- setNames(seq_along(names(exibble)), names(exibble))
   no_cols <- setNames(integer(0), character(0))
@@ -35,13 +33,116 @@ test_that("resolve_cols", {
   expect_resolve_cols(100L, no_cols, strict = FALSE)
 })
 
-test_that("resolve_rows", {
-  # resolve_rows_i
-  # resolve_rows_l
+test_that("`resolve_rows_l()` and `resolve_rows_i()` both work", {
+
+  mtcars_gt <- gt(mtcars, rownames_to_stub = TRUE)
+  iris_gt <- gt(iris, rownames_to_stub = TRUE)
+
+  exibble_gt_1 <- gt(exibble %>% dplyr::mutate(row = ifelse(row == "row_8", "", row)), rowname_col = "row")
+  row_names_1 <- c(paste0("row_", 1:7), "")
+
+  exibble_gt_2 <- gt(dplyr::bind_rows(exibble, exibble), rowname_col = "char")
+  row_names_2 <- rep(exibble %>% dplyr::pull(char), 2)
+
+  #
+  # `resolve_rows_l()`
+  #
+
   expect_identical(resolve_rows_l(1, mtcars), c(TRUE, rep_len(FALSE, nrow(mtcars) - 1)))
   expect_identical(resolve_rows_l("Mazda RX4", mtcars), c(TRUE, rep_len(FALSE, nrow(mtcars) - 1)))
   expect_identical(resolve_rows_l(NULL, mtcars), rep_len(TRUE, nrow(mtcars)))
+
+  expect_identical(resolve_rows_l(1, mtcars_gt), c(TRUE, rep_len(FALSE, nrow(mtcars) - 1)))
+  expect_identical(resolve_rows_l("Mazda RX4", mtcars_gt), c(TRUE, rep_len(FALSE, nrow(mtcars) - 1)))
+  expect_identical(resolve_rows_l(NULL, mtcars_gt), rep_len(TRUE, nrow(mtcars)))
+
+  # Select helpers
   expect_identical(resolve_rows_l(everything(), mtcars), rep_len(TRUE, nrow(mtcars)))
-  # TODO: More test cases
-  # TODO: Test with duplicate stub names
+  expect_identical(resolve_rows_l(starts_with("Merc"), mtcars), c(rep(FALSE, 7), rep(TRUE, 7), rep(FALSE, 18)))
+  expect_identical(resolve_rows_l(ends_with("SE", ignore.case = FALSE), mtcars), c(rep(FALSE, 11), TRUE, rep(FALSE, 20)))
+  expect_identical(resolve_rows_l(matches("RX4"), mtcars), c(rep(TRUE, 2), rep(FALSE, 30)))
+  expect_identical(resolve_rows_l(matches("RX[0-9]"), mtcars), c(rep(TRUE, 2), rep(FALSE, 30)))
+  expect_identical(resolve_rows_l(contains("RX4"), mtcars), c(rep(TRUE, 2), rep(FALSE, 30)))
+  expect_identical(resolve_rows_l(num_range(prefix = "", range = seq_len(nrow(iris))), iris), rep_len(TRUE, nrow(iris)))
+
+  expect_identical(resolve_rows_l(everything(), mtcars_gt), rep_len(TRUE, nrow(mtcars)))
+  expect_identical(resolve_rows_l(starts_with("Merc"), mtcars_gt), c(rep(FALSE, 7), rep(TRUE, 7), rep(FALSE, 18)))
+  expect_identical(resolve_rows_l(ends_with("SE", ignore.case = FALSE), mtcars_gt), c(rep(FALSE, 11), TRUE, rep(FALSE, 20)))
+  expect_identical(resolve_rows_l(matches("RX4"), mtcars_gt), c(rep(TRUE, 2), rep(FALSE, 30)))
+  expect_identical(resolve_rows_l(matches("RX[0-9]"), mtcars_gt), c(rep(TRUE, 2), rep(FALSE, 30)))
+  expect_identical(resolve_rows_l(contains("RX4"), mtcars_gt), c(rep(TRUE, 2), rep(FALSE, 30)))
+  expect_identical(resolve_rows_l(num_range(prefix = "", range = seq_len(nrow(iris))), iris_gt), rep_len(TRUE, nrow(iris)))
+
+  # Using row names with duplicates and NAs
+  expect_identical(resolve_rows_l(1, exibble_gt_1), c(TRUE, rep_len(FALSE, length(row_names_1) - 1)))
+  expect_identical(resolve_rows_l(c(1, 1), exibble_gt_1), c(TRUE, rep_len(FALSE, length(row_names_1) - 1)))
+  expect_identical(resolve_rows_l("row_1", exibble_gt_1), c(TRUE, rep_len(FALSE, length(row_names_1) - 1)))
+  expect_identical(resolve_rows_l(c("row_1", "row_1", "row_2", "row_1"), exibble_gt_1), c(TRUE, TRUE, rep(FALSE, 6)))
+  expect_identical(resolve_rows_l("", exibble_gt_1), c(rep_len(FALSE, length(row_names_1) - 1), TRUE))
+  expect_identical(resolve_rows_l(FALSE, exibble_gt_1), c(rep_len(FALSE, length(row_names_1))))
+  expect_identical(resolve_rows_l(character(0), exibble_gt_1), c(rep_len(FALSE, length(row_names_1))))
+  expect_identical(resolve_rows_l(NULL, exibble_gt_1), rep_len(TRUE, length(row_names_1)))
+  expect_identical(resolve_rows_l(TRUE, exibble_gt_1), rep_len(TRUE, length(row_names_1)))
+  expect_identical(resolve_rows_l(rep(TRUE, 8), exibble_gt_1), rep_len(TRUE, length(row_names_1)))
+
+  expect_error(resolve_rows_l(rep(TRUE, 6), exibble_gt_1))
+  expect_error(resolve_rows_l("not a row", exibble_gt_1))
+  expect_error(resolve_rows_l(c(10, 10, 1), exibble_gt_1))
+  expect_error(resolve_rows_l(c("not", "present"), exibble_gt_1))
+  expect_error(resolve_rows_l(dplyr::tibble(a = 2), exibble_gt_1))
+
+  expect_identical(resolve_rows_l(1, exibble_gt_2), c(TRUE, rep_len(FALSE, length(row_names_2) - 1)))
+  expect_identical(resolve_rows_l("apricot", exibble_gt_2), rep(c(TRUE, rep_len(FALSE, 7)), 2))
+  expect_identical(resolve_rows_l(NULL, exibble_gt_2), rep_len(TRUE, length(row_names_2)))
+
+  #
+  # `resolve_rows_i()`
+  #
+
+  expect_identical(resolve_rows_i(1, mtcars), 1L)
+  expect_identical(resolve_rows_i("Mazda RX4 Wag", mtcars), 2L)
+  expect_identical(resolve_rows_i(NULL, mtcars), seq_len(nrow(mtcars)))
+
+  expect_identical(resolve_rows_i(1, mtcars_gt), 1L)
+  expect_identical(resolve_rows_i("Mazda RX4 Wag", mtcars_gt), 2L)
+  expect_identical(resolve_rows_i(NULL, mtcars_gt), seq_len(nrow(mtcars)))
+
+  # Select helpers
+  expect_identical(resolve_rows_i(everything(), mtcars), seq_len(nrow(mtcars)))
+  expect_identical(resolve_rows_i(starts_with("Merc"), mtcars), 8:14)
+  expect_identical(resolve_rows_i(ends_with("SE", ignore.case = FALSE), mtcars), 12L)
+  expect_identical(resolve_rows_i(matches("RX4"), mtcars), 1:2)
+  expect_identical(resolve_rows_i(matches("RX[0-9]"), mtcars), 1:2)
+  expect_identical(resolve_rows_i(contains("RX4"), mtcars), 1:2)
+  expect_identical(resolve_rows_i(num_range(prefix = "", range = seq_len(nrow(iris))), iris), 1:150)
+
+  expect_identical(resolve_rows_i(everything(), mtcars_gt), seq_len(nrow(mtcars)))
+  expect_identical(resolve_rows_i(starts_with("Merc"), mtcars_gt), 8:14)
+  expect_identical(resolve_rows_i(ends_with("SE", ignore.case = FALSE), mtcars_gt), 12L)
+  expect_identical(resolve_rows_i(matches("RX4"), mtcars_gt), 1:2)
+  expect_identical(resolve_rows_i(matches("RX[0-9]"), mtcars_gt), 1:2)
+  expect_identical(resolve_rows_i(contains("RX4"), mtcars_gt), 1:2)
+  expect_identical(resolve_rows_i(num_range(prefix = "", range = seq_len(nrow(iris))), iris_gt), 1:150)
+
+  # Using row names with duplicates and NAs
+  expect_identical(resolve_rows_i(1, exibble_gt_1), 1L)
+  expect_identical(resolve_rows_i(c(1, 1), exibble_gt_1), 1L)
+  expect_identical(resolve_rows_i("row_1", exibble_gt_1), 1L)
+  expect_identical(resolve_rows_i(c("row_1", "row_1", "row_2", "row_1"), exibble_gt_1), 1:2)
+  expect_identical(resolve_rows_i("", exibble_gt_1), 8L)
+  expect_identical(resolve_rows_i(FALSE, exibble_gt_1), integer(0))
+  expect_identical(resolve_rows_i(character(0), exibble_gt_1), integer(0))
+  expect_identical(resolve_rows_i(NULL, exibble_gt_1), 1:8)
+  expect_identical(resolve_rows_i(TRUE, exibble_gt_1), 1:8)
+  expect_identical(resolve_rows_i(rep(TRUE, 8), exibble_gt_1), 1:8)
+
+  expect_error(resolve_rows_i(rep(TRUE, 6), exibble_gt_1))
+  expect_error(resolve_rows_i("not a row", exibble_gt_1))
+  expect_error(resolve_rows_i(c(10, 10, 1), exibble_gt_1))
+  expect_error(resolve_rows_i(c("not", "present"), exibble_gt_1))
+  expect_error(resolve_rows_i(dplyr::tibble(a = 2), exibble_gt_1))
+
+  expect_identical(resolve_rows_i(1, exibble_gt_2), 1L)
+  expect_identical(resolve_rows_i("apricot", exibble_gt_2), c(1L, 9L))
+  expect_identical(resolve_rows_i(NULL, exibble_gt_2), 1:16)
 })
