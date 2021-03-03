07b819e2355890eb662b2ab8a3c566d9f26b09a6
Richard Iannone <riannone@me.com>
2021-03-03 14:46:13 -0500

    Add several testthat tests

--- tests/testthat/test-resolver.R
+++ tests/testthat/test-resolver.R
@@ -145,4 +145,12 @@ test_that("`resolve_rows_l()` and `resolve_rows_i()` both work", {
   expect_identical(resolve_rows_i(1, exibble_gt_2), 1L)
   expect_identical(resolve_rows_i("apricot", exibble_gt_2), c(1L, 9L))
   expect_identical(resolve_rows_i(NULL, exibble_gt_2), 1:16)
+
+  expect_identical(resolve_rows_i(is.na(char), exibble_gt_1), 5L)
+  expect_identical(resolve_rows_i(num > 4, exibble_gt_1), c(3:5, 7:8))
+  expect_identical(resolve_rows_i(fctr == "six", exibble_gt_1), 6L)
+  expect_identical(resolve_rows_i(currency < 5 && char == "grapefruit", exibble_gt_1), integer(0))
+  expect_identical(resolve_rows_i(currency == 20, exibble_gt_1), integer(0))
+  expect_identical(resolve_rows_i(currency < 5 | char == "grapefruit", exibble_gt_1), c(3L, 7L, 8L))
+  expect_identical(resolve_rows_i(currency < 5 || char == "grapefruit", exibble_gt_1), integer(0))
 })
