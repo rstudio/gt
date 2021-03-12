822a447e5065754a8e1815ab2f50951339a77225
Richard Iannone <riannone@me.com>
2021-03-12 09:42:13 -0500

    Fix `as_raw_html()` for standard case

--- R/export.R
+++ R/export.R
@@ -317,15 +317,19 @@ as_raw_html <- function(data,
   # Perform input object validation
   stop_if_not_gt(data = data)
 
-  # Generation of the HTML table
-  html_table <- render_as_html(data = data)
-
   if (inline_css) {
+
+    # Generation of the HTML table
+    html_table <- render_as_html(data = data)
 
     # Create inline styles
     html_table <-
       html_table %>%
       inline_html_styles(css_tbl = get_css_tbl(data))
+
+  } else {
+
+    html_table <- as.character(as.tags.gt_tbl(data))
   }
 
   htmltools::HTML(html_table)
