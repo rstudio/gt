c3ed5520e58407dca34ea5e7823d169c8915816a
Richard Iannone <riannone@me.com>
2021-03-17 14:35:10 -0400

    Remove unnecessary `tagList()`

--- R/print.R
+++ R/print.R
@@ -82,18 +82,16 @@ as.tags.gt_tbl <- function(x, ...) {
 
   # Attach the dependency to the HTML table
   html_tbl <-
-    htmltools::tagList(
-      htmltools::tags$div(
-        id = id,
-        htmltools::tags$style(htmltools::HTML(css)),
-        style = htmltools::css(
-          `overflow-x` = container_overflow_x,
-          `overflow-y` = container_overflow_y,
-          width = container_width,
-          height = container_height
-        ),
-        htmltools::HTML(html_table)
-      )
+    htmltools::tags$div(
+      id = id,
+      htmltools::tags$style(htmltools::HTML(css)),
+      style = htmltools::css(
+        `overflow-x` = container_overflow_x,
+        `overflow-y` = container_overflow_y,
+        width = container_width,
+        height = container_height
+      ),
+      htmltools::HTML(html_table)
     )
 
   html_tbl
