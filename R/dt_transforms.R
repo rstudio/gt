2403fdd62f81eb7b7c557fa1a6a42a53ca736a62
Richard Iannone <riannone@me.com>
2021-03-25 13:24:13 -0400

    Add class to resolved text transform object

--- R/dt_transforms.R
+++ R/dt_transforms.R
@@ -21,15 +21,18 @@ dt_transforms_add <- function(data, loc, fn) {
   existing_transforms <- dt_transforms_get(data = data)
   resolved <- resolve_location(loc = loc, data = data)
 
+  added_transform <-
+    list(
+      resolved = resolved,
+      fn = fn
+    )
+
+  class(added_transform) <- class(loc)[1]
+
   transforms <-
     c(
       existing_transforms,
-      list(
-        list(
-          resolved = resolved,
-          fn = fn
-        )
-      )
+      list(added_transform)
     )
 
   dt_transforms_set(data = data, transforms = transforms)
