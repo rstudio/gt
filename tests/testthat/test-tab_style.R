e4d1ac1820f9d6caf44317210071535ae4596a1e
Richard Iannone <riannone@me.com>
2021-04-05 14:23:50 -0400

    Add test for `tab_row_group()`'s `others_label`

--- tests/testthat/test-tab_style.R
+++ tests/testthat/test-tab_style.R
@@ -426,6 +426,13 @@ test_that("a gt table can store the correct style statements", {
     expect_equal(c(
       "data", NA_character_, "hp", "5", "1", NA_character_, "#FFFF00")
     )
+
+  # Check that `tab_row_group(others_label = ...)` still works but
+  # issues a warning
+  expect_equivalent(
+    expect_warning(data %>% tab_row_group(others_label = "Others1")),
+    data %>% tab_options(row_group.default_label = "Others1")
+  )
 })
 
 test_that("using fonts in `cell_text()` works", {
