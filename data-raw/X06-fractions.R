ea841b860a3a9569602fb3f8789a92dbbf13faac
Richard Iannone <riannone@me.com>
2021-05-04 10:22:24 -0400

    Update X06-fractions.R

--- data-raw/X06-fractions.R
+++ data-raw/X06-fractions.R
@@ -17,3 +17,39 @@ frac_100_d <-
     file = "data-raw/frac_100_d.csv",
     col_types = "iid"
   )
+
+frac_1_2 <-
+  readr::read_csv(
+    file = "data-raw/frac_1_2.csv",
+    col_types = "iidl"
+  )
+
+frac_1_4 <-
+  readr::read_csv(
+    file = "data-raw/frac_1_4.csv",
+    col_types = "iidl"
+  )
+
+frac_1_8 <-
+  readr::read_csv(
+    file = "data-raw/frac_1_8.csv",
+    col_types = "iidl"
+  )
+
+frac_1_16 <-
+  readr::read_csv(
+    file = "data-raw/frac_1_16.csv",
+    col_types = "iidl"
+  )
+
+frac_1_10 <-
+  readr::read_csv(
+    file = "data-raw/frac_1_10.csv",
+    col_types = "iidl"
+  )
+
+frac_1_100 <-
+  readr::read_csv(
+    file = "data-raw/frac_1_100.csv",
+    col_types = "iidl"
+  )
