d841c8d4f7f53f0ced554740861ecc26f6f2c529
Richard Iannone <riannone@me.com>
2021-05-04 10:22:28 -0400

    Update zz_process_datasets_ext.R

--- data-raw/zz_process_datasets_ext.R
+++ data-raw/zz_process_datasets_ext.R
@@ -12,5 +12,7 @@ usethis::use_data(
   currencies, currency_symbols, locales, palettes_strips,
   google_font_tbl, google_styles_tbl, google_axes_tbl,
   frac_1_d, frac_10_d, frac_100_d,
+  frac_1_2, frac_1_4, frac_1_8, frac_1_16,
+  frac_1_10, frac_1_100,
   internal = TRUE, overwrite = TRUE
 )
