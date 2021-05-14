2a5aa08677f9d24e9a0316dc7bb76743057bd3c9
Richard Iannone <riannone@me.com>
2021-05-14 14:46:41 -0400

    Fix rendering of summary rows

--- R/utils_render_common.R
+++ R/utils_render_common.R
@@ -478,7 +478,7 @@ create_summary_rows <- function(n_rows,
 
       summary_df <-
         list_of_summaries$summary_df_display_list[[group]] %>%
-        dplyr::select(-groups) %>%
+        dplyr::select(-group) %>%
         as.data.frame(stringsAsFactors = FALSE)
 
       body_content_summary <- as.vector(t(summary_df))
