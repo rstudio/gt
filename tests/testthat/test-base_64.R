ce2506ab7556ad358f069a1aee374023d2f94b20
Richard Iannone <riannone@me.com>
2021-06-17 17:06:03 -0400

    Merge test files/snapshots

--- tests/testthat/test-base_64.R
+++ tests/testthat/test-base_64.R
@@ -10,35 +10,4 @@ test_that("the `get_file_ext()` function works correctly", {
   get_file_ext(file = "file.gif") %>% expect_equal("gif")
 })
 
-test_that("the `get_mime_type()` function works correctly", {
 
-  # Expect that the `get_mime_type()` function returns
-  # a mime-type string for svg and jpg files
-  get_mime_type(file = "file.svg") %>% expect_equal("image/svg+xml")
-  get_mime_type(file = "file.jpg") %>% expect_equal("image/jpeg")
-})
-
-test_that("the `get_image_uri()` function works correctly", {
-
-  testthat::local_edition(3)
-
-  # Expect that the image URI string based on a PNG test image
-  # matches a known good image URI created from that image
-  expect_equal(
-    get_image_uri(file = system_file("./graphics/test_image.png")),
-    readLines(system_file("./graphics/test_image_png_uri.txt"), warn = FALSE))
-
-  # Expect that the beginning of the PNG-based image URI has the MIME
-  # type and the encoding type
-  expect_true(
-    grepl(
-      "^data:image/png;base64,",
-      get_image_uri(file = system_file("./graphics/test_image.png"))
-    )
-  )
-
-  # Expect that the image URIs for the included test image
-  # produce the same URI strings
-  expect_snapshot(get_image_uri(file = test_image(type = "png")))
-  expect_snapshot(get_image_uri(file = test_image(type = "svg")))
-})
--- tests/testthat/test-image.R
+++ tests/testthat/test-image.R
@@ -1,76 +1,105 @@
-context("Ensuring that the image-based functions work as expected")
-
 test_that("the `test_image()` function works correctly", {
 
   # Expect that the `test_image()` function returns paths for
   # either a PNG or SVG test image
-  test_image(type = "png") %>%
-    expect_match(".*/test_image.png")
+  test_image(type = "png") %>% expect_match(".*/test_image.png")
+  test_image(type = "svg") %>% expect_match(".*/test_image.svg")
+})
 
-  test_image(type = "svg") %>%
-    expect_match(".*/test_image.svg")
+test_that("the `get_mime_type()` function works correctly", {
+
+  # Expect that the `get_mime_type()` function returns
+  # a mime-type string for svg and jpg files
+  get_mime_type(file = "file.svg") %>% expect_equal("image/svg+xml")
+  get_mime_type(file = "file.jpg") %>% expect_equal("image/jpeg")
+})
+
+test_that("the `get_image_uri()` function works correctly", {
+
+  testthat::local_edition(3)
+
+  # Expect that the beginning of the PNG-based image URI has the
+  # correct MIME and encoding types
+  expect_true(
+    grepl(
+      "^data:image/png;base64,",
+      get_image_uri(file = system_file("./graphics/test_image.png"))
+    )
+  )
+
+  # Expect that the beginning of the SVG-based image URI has the
+  # correct MIME and encoding types
+  expect_true(
+    grepl(
+      "data:image/svg\\+xml;base64",
+      get_image_uri(file = system_file("./graphics/test_image.svg"))
+    )
+  )
+
+  # Expect that the image URIs for the included test image
+  # produce the same URI strings
+  expect_snapshot(get_image_uri(file = test_image(type = "png")))
+  expect_snapshot(get_image_uri(file = test_image(type = "svg")))
 })
 
 test_that("the `local_image()` function works correctly", {
 
-  # Expect that the `local_image()` function returns an image tag
-  # with the PNG encoded within a `src` attr
-  local_image(file = test_image(type = "png"), height = px(70)) %>%
-    expect_match("^<img cid=.*?test_image.png. src=.data:image.png;base64.*?height:70px.*")
+  testthat::local_edition(3)
 
-  # Expect that the `local_image()` function returns an image tag
-  # with the SVG encoded within a `src` attr
-  local_image(file = test_image(type = "svg"), height = px(70)) %>%
-    expect_match("^<img cid=.*?test_image.svg. src=.data:image.svg.xml;base64.*?height:70px.*")
-
-  # Expect that the `local_image()` function returns an image tag
-  # with the PNG encoded within a `src` attr (even if a number is provided
-  # to `height`)
-  local_image(file = test_image(type = "png"), height = 70) %>%
-    expect_match("^<img cid=.*?test_image.png. src=.data:image.png;base64.*?height:70px.*")
-
-  # # Create a `tbl_html` that contains local images in column `b`
-  # tbl_html <-
-  #   dplyr::tibble(
-  #     a = 1:5,
-  #     b = c(1.6, 9.6, 5.3, 3.5, 6.3)) %>%
-  #   gt() %>%
-  #   text_transform(
-  #     locations = cells_body(columns = b),
-  #     fn = function(x) {
-  #       x <- as.numeric(x)
-  #       ifelse(
-  #         x >= 5,
-  #         local_image(file = test_image(type = "svg"), height = px(x * 10)),
-  #         local_image(file = test_image(type = "png"), height = px(x * 10)))
-  #     })
-  #
-  # # Expect that PNG image tags are in the rendered table
-  # tbl_html %>%
-  #   render_as_html() %>%
-  #   expect_match("<img cid=.*?test_image.png. src=.data:image.png;base64.*?")
-  #
-  # # Expect that SVG image tags are in the rendered table
-  # tbl_html %>%
-  #   render_as_html() %>%
-  #   expect_match("<img cid=.*?test_image.svg. src=.data:image.svg.xml;base64.*?")
+  # Expect that the image tags generated for the included test image
+  # are correctly formed
+  expect_snapshot(local_image(filename = test_image(type = "png")))
+  expect_snapshot(local_image(filename = test_image(type = "svg")))
 })
 
 test_that("the `web_image()` function works correctly", {
 
-  url <- "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Googlelogo.png/320px-Googlelogo.png"
+  # Expect that the image tag is correctly formed
+  expect_equal(
+    web_image(url = "example.png"),
+    "<img src=\"example.png\" style=\"height:30px;\">"
+  )
 
   # Expect that the `web_image()` function returns an image tag
-  web_image(url = url, height = px(200)) %>%
-    expect_match(paste0("^<img src=.", url, ". style=.height:200px;.>"))
+  expect_equal(
+    web_image(url = "example.com/img.png", height = px(200)),
+    "<img src=\"example.com/img.png\" style=\"height:200px;\">"
+  )
 
-  # Expect that the `web_image()` function returns an image tag
-  # (even if a number is provided to `height`)
-  web_image(url = url, height = 200) %>%
-    expect_match(paste0("^<img src=.", url, ". style=.height:200px;.>"))
+  # Expect that the `web_image()` function returns the same image tag
+  # even if `height` is provided in different ways
+  expect_equal(
+    web_image(url = "example.com/img.png", height = px(200)),
+    web_image(url = "example.com/img.png", height = 200)
+  )
 
-  # Expect that the `web_image()` function returns an image tag
-  # (even if a string is provided to `height`)
-  web_image(url = url, height = "200px") %>%
-    expect_match(paste0("^<img src=.", url, ". style=.height:200px;.>"))
+  expect_equal(
+    web_image(url = "example.com/img.png", height = 200),
+    web_image(url = "example.com/img.png", height = "200px")
+  )
+
+  # Expect that `web_image()` can return multiple image tags
+  expect_equal(
+    web_image(
+      url = c("example.com/img1.png", "example.com/img2.png"),
+      height = px(200)
+    ),
+    paste0("<img src=\"example.com/img", 1:2, ".png\" style=\"height:200px;\">")
+  )
 })
+
+# # Create a `tbl_html` that contains local images in column `b`
+# tbl_html <-
+#   dplyr::tibble(
+#     a = 1:5,
+#     b = c(1.6, 9.6, 5.3, 3.5, 6.3)) %>%
+#   gt() %>%
+#   text_transform(
+#     locations = cells_body(columns = b),
+#     fn = function(x) {
+#       x <- as.numeric(x)
+#       ifelse(
+#         x >= 5,
+#         local_image(file = test_image(type = "svg"), height = px(x * 10)),
+#         local_image(file = test_image(type = "png"), height = px(x * 10)))
+#     })
