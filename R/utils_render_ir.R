583e9d537e002e4910e6e08877601aba4b2af2b8
Richard Iannone <riannone@me.com>
2021-07-29 13:30:43 -0400

    Add the `create_heading_ir()` function

--- R/utils_render_ir.R
+++ R/utils_render_ir.R
@@ -57,4 +57,119 @@ create_col_group_ir <- function(data) {
 
   htmltools::tagList(colgroup_element)
 }
+
+create_heading_ir <- function(data) {
+
+  # If there is no title or heading component, then return an empty string
+  if (!dt_heading_has_title(data = data)) {
+    return(htmltools::tagList())
+  }
+  heading <- dt_heading_get(data = data)
+  footnotes_tbl <- dt_footnotes_get(data = data)
+  styles_tbl <- dt_styles_get(data = data)
+  stub_components <- dt_stub_components(data = data)
+  subtitle_defined <- dt_heading_has_subtitle(data = data)
+
+  # Get the footnote marks for the title
+  if ("title" %in% footnotes_tbl$locname) {
+
+    footnote_title_marks <-
+      coalesce_marks(
+        fn_tbl = footnotes_tbl,
+        locname = "title"
+      )
+
+    footnote_title_marks <-
+      footnote_mark_to_html(mark = footnote_title_marks$fs_id_c)
+
+  } else {
+    footnote_title_marks <- ""
+  }
+
+  # Get the style attrs for the title
+  if ("title" %in% styles_tbl$locname) {
+
+    title_style_rows <- dplyr::filter(styles_tbl, locname == "title")
+
+    if (nrow(title_style_rows) > 0) {
+      title_styles <- title_style_rows$html_style
+    } else {
+      title_styles <- NULL
+    }
+
+  } else {
+    title_styles <- NA_character_
+  }
+
+  # Get the footnote marks for the subtitle
+  if (subtitle_defined && "subtitle" %in% footnotes_tbl$locname) {
+
+    footnote_subtitle_marks <-
+      coalesce_marks(
+        fn_tbl = footnotes_tbl,
+        locname = "subtitle"
+      )
+
+    footnote_subtitle_marks <-
+      footnote_mark_to_html(mark = footnote_subtitle_marks$fs_id_c)
+
+  } else {
+    footnote_subtitle_marks <- ""
+  }
+
+  # Get the style attrs for the subtitle
+  if (subtitle_defined && "subtitle" %in% styles_tbl$locname) {
+
+    subtitle_style_rows <- dplyr::filter(styles_tbl, locname == "subtitle")
+
+    if (nrow(subtitle_style_rows) > 0) {
+      subtitle_styles <- subtitle_style_rows$html_style
+    } else {
+      subtitle_styles <- NULL
+    }
+
+  } else {
+    subtitle_styles <- NA_character_
+  }
+
+  # title_classes <- c("gt_heading", "gt_title", "gt_font_normal")
+  #
+  # subtitle_classes <- title_classes %>% tidy_sub("title", "subtitle")
+  #
+  # if (!subtitle_defined) {
+  #   title_classes <- c(title_classes, "gt_bottom_border")
+  # } else {
+  #   subtitle_classes <- c(subtitle_classes, "gt_bottom_border")
+  # }
+
+  if (!subtitle_defined) {
+
+    caption_element <-
+      htmltools::tagList(
+        htmltools::tags$caption(
+          htmltools::tags$p(
+            style = if (!is.na(title_styles)) title_styles else NULL,
+            htmltools::HTML(paste0(heading$title, footnote_title_marks))
+          )
+        )
+      )
+
+  } else {
+
+    caption_element <-
+      htmltools::tagList(
+        htmltools::tags$caption(
+          htmltools::tags$p(
+            htmltools::HTML(paste0(heading$title, footnote_title_marks))
+          ),
+          htmltools::tags$p(
+            htmltools::HTML(paste0(heading$subtitle, footnote_subtitle_marks))
+          )
+        )
+      )
+
+  }
+
+  caption_element
+}
 }
