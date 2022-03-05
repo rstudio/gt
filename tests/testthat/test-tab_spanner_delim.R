c2e03411ff8fa6a8f6b2ff7c07d827d47a1c42dc
Richard Iannone <riannone@me.com>
2022-03-04 23:21:46 -0500

    Add testthat tests and snapshots

--- tests/testthat/test-tab_spanner_delim.R
+++ tests/testthat/test-tab_spanner_delim.R
@@ -4,16 +4,6 @@ skip_on_cran()
 # Create a shortened version of `iris`
 iris_short <- iris[1:5, ]
 
-# Create a summarized version of `iris`
-iris_summary <-
-  iris %>%
-  dplyr::group_by(Species) %>%
-  dplyr::summarize(
-    dplyr::across(dplyr::everything(), ~mean(.), .names = "M.{col}"),
-    dplyr::across(dplyr::everything(), ~sd(.), .names = "SD.{col}"),
-    .groups = "drop"
-  )
-
 # Function to skip tests if Suggested packages not available on system
 check_suggests <- function() {
   skip_if_not_installed("rvest")
@@ -43,7 +33,7 @@ test_that("the `tab_spanner_delim()` function works correctly", {
     dt_spanners_get() %>%
     .$spanner_label %>%
     unlist() %>%
-    expect_equal(c("Petal", "Sepal"))
+    expect_equal(c("Sepal", "Petal"))
 
   # Expect that the columns with a colspan of `2` have the same
   # ordering in the rendered table
@@ -67,45 +57,34 @@ test_that("the `tab_spanner_delim()` function works correctly", {
 
   # Create a `tbl_html` object with `gt()`; split the column
   # names into spanner headings and column labels, this time
-  # ensuring that the non-default `split` option (`"first"`) has
-  # no effect when there is only one delimiter to split on
-  tbl_html <-
+  # ensuring that the non-default `split` option (`"first"`) will
+  # result splitting spanner names from the LHS
+  tbl_html_first <-
     gt(iris_short) %>%
     tab_spanner_delim(delim = ".", split = "first")
 
   # Expect a particular ordering of column labels in `_boxh`
-  tbl_html %>%
+  tbl_html_first %>%
     dt_boxhead_get() %>%
     .$column_label %>%
     unlist() %>%
-    expect_equal(c("Length", "Width", "Length", "Width", "Species"))
+    expect_equal(c("Sepal", "Sepal", "Petal", "Petal", "Species"))
 
   # Expect a particular ordering of spanner labels in `_spanners`
-  tbl_html %>%
+  tbl_html_first %>%
     dt_spanners_get() %>%
     .$spanner_label %>%
     unlist() %>%
-    expect_equal(c("Petal", "Sepal"))
+    expect_equal(c("Length", "Width", "Length", "Width"))
 
-  # Expect that the columns with a colspan of `2` have the same
-  # ordering in the rendered table
-  tbl_html %>%
+  # Expect that no cells should have a colspan value of `2`
+  tbl_html_first %>%
     render_as_html() %>%
     xml2::read_html() %>%
     rvest::html_nodes("[colspan='2']") %>%
     rvest::html_text() %>%
-    grepl("Sepal|Petal", .) %>%
-    all() %>%
-    expect_true()
-
-  # Expect that the columns with a colspan of `1` have the same
-  # ordering in the rendered table
-  tbl_html %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[colspan='1']") %>%
-    rvest::html_text() %>%
-    expect_equal(c("Species", "Length", "Width", "Length", "Width"))
+    length() %>%
+    expect_equal(0)
 
   # Create a `tbl_html` object with `gt()`; split the column
   # names into spanner headings and column labels but constrain
@@ -182,318 +161,90 @@ test_that("the `tab_spanner_delim()` function works correctly", {
       c("Petal.Length", "Petal.Width", "Species", "Length", "Width")
     )
 
-  # Create a `tbl_html` object with `gt()`, this time with the
-  # summarized version of `iris` that has column names with multiple
-  # instances of the delimiter character `.`; split the column
-  # names into spanner headings and column labels using the default
-  # `split` option (`"last"`)
-  tbl_html <-
-    gt(iris_summary) %>%
-    tab_spanner_delim(delim = ".", split = "last")
-
-  # Expect a particular ordering of column labels in `_boxh`
-  tbl_html %>%
-    dt_boxhead_get() %>%
-    .$column_label %>%
-    unlist() %>%
-    expect_equal(c("Species", rep(c("Length", "Width"), 6)))
-
-  # Expect a particular ordering of spanner labels in `_spanners`
-  # TODO: No longer working as before
-  # tbl_html %>%
-  #   dt_spanners_get() %>%
-  #   .$spanner_label %>%
-  #   unlist() %>%
-  #   expect_equal(
-  #     c(
-  #       "Petal", "M.Sepal", "SD.M.Petal",
-  #       "SD.M.Sepal", "SD.Petal", "SD.Sepal"
-  #     )
-  #   )
-
-  # Expect that the columns with a colspan of `2` have the same
-  # ordering in the rendered table
-  # TODO: No longer working as before
-  # tbl_html %>%
-  #   render_as_html() %>%
-  #   xml2::read_html() %>%
-  #   rvest::html_nodes("[colspan='2']") %>%
-  #   rvest::html_text() %>%
-  #   gsub("(\n|\\s)+?", "", .) %>%
-  #   expect_equal(
-  #     c(
-  #       "M.Sepal", "M.Petal", "SD.Sepal", "SD.Petal",
-  #       "SD.M.Sepal", "SD.M.Petal"
-  #     )
-  #   )
-
-  # Expect that the columns with a colspan of `1` have the same
-  # ordering in the rendered table
-  # TODO: No longer working as before
-  # tbl_html %>%
-  #   render_as_html() %>%
-  #   xml2::read_html() %>%
-  #   rvest::html_nodes("[colspan='1']") %>%
-  #   rvest::html_text() %>%
-  #   expect_equal(c("Species", rep(c("Length", "Width"), 6)))
-
-  # Create a `tbl_html` object with `gt()`, this time with the
-  # summarized version of `iris` that has column names with multiple
-  # instances of the delimiter character `.`; split the column
-  # names into spanner headings and column labels using the non-default
-  # `split` option (`"first"`)
-  tbl_html <-
-    gt(iris_summary) %>%
-    tab_spanner_delim(delim = ".", split = "first")
-
-  # Expect a particular ordering of column labels in `_boxh`
-  tbl_html %>%
-    dt_boxhead_get() %>%
-    .$column_label %>%
-    unlist() %>%
-    expect_equal(
-      c(
-        "Species", "Sepal.Length", "Sepal.Width", "Petal.Length",
-        "Petal.Width", "Sepal.Length", "Sepal.Width",
-        "Petal.Length", "Petal.Width", "M.Sepal.Length", "M.Sepal.Width",
-        "M.Petal.Length", "M.Petal.Width"
-      )
-    )
-
-  # Expect a particular ordering of spanner labels in `_spanners`
-  tbl_html %>%
-    dt_spanners_get() %>%
-    .$spanner_label %>%
-    unlist() %>%
-    expect_equal(c("M", "SD"))
-
-  # Expect that the columns with a colspan of `4` belongs to `"M"`
-  tbl_html %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[colspan='4']") %>%
-    rvest::html_text() %>%
-    gsub("(\n|\\s)+?", "", .) %>%
-    expect_equal("M")
-
-  # Expect that the columns with a colspan of `8` belongs to `"SD"`
-  tbl_html %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[colspan='8']") %>%
-    rvest::html_text() %>%
-    gsub("(\n|\\s)+?", "", .) %>%
-    expect_equal("SD")
-
-  # Expect that the columns with a colspan of `1` have the same
-  # ordering in the rendered table
-  tbl_html %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[colspan='1']") %>%
-    rvest::html_text() %>%
-    expect_equal(
-      c(
-        "Species", "Sepal.Length", "Sepal.Width", "Petal.Length",
-        "Petal.Width", "Sepal.Length", "Sepal.Width",
-        "Petal.Length", "Petal.Width", "M.Sepal.Length", "M.Sepal.Width",
-        "M.Petal.Length", "M.Petal.Width"
-      )
-    )
-})
-
-test_that("`tab_spanner_delim()` gathers columns as necessary", {
-
   tbl_a <- dplyr::tibble(A_X = 1, B_X = 2, A_Y = 3, B_Y = 4)
-
   tbl_b <- dplyr::tibble(A_X = 1, B_X = 2, A_Y = 3, B_Y = 4, Z = 5)
-
   tbl_c <- dplyr::tibble(Z = 0.5, A_X = 1, B_X = 2, A_Y = 3, B_Y = 4)
 
   tbl_html_1 <-
     gt(tbl_a) %>%
-    tab_spanner_delim(
-      delim = "_",
-      gather = TRUE
-    )
+    tab_spanner_delim(delim = "_")
 
   tbl_html_1 %>%
     dt_spanners_get() %>%
     .$spanner_label %>%
     unlist() %>%
-    expect_equal(c("A", "B"))
+    expect_equal(c("A", "B", "A", "B"))
 
   tbl_html_1 %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_column_spanner']") %>%
-    rvest::html_text() %>%
-    expect_equal(c("A", "B"))
-
-  tbl_html_1 %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") %>%
-    rvest::html_text() %>%
-    expect_equal(c("X", "Y", "X", "Y"))
-
-  tbl_html_2 <-
-    gt(tbl_a) %>%
-    tab_spanner_delim(
-      delim = "_",
-      gather = FALSE
-    )
-
-  tbl_html_2 %>%
-    dt_spanners_get() %>%
-    .$spanner_label %>%
-    unlist() %>%
-    expect_equal(c("A", "B"))
-
-  tbl_html_2 %>%
     render_as_html() %>%
     xml2::read_html() %>%
     rvest::html_nodes("[class='gt_column_spanner']") %>%
     rvest::html_text() %>%
     expect_equal(c("A", "B", "A", "B"))
 
-  tbl_html_2 %>%
+  tbl_html_1 %>%
     render_as_html() %>%
     xml2::read_html() %>%
     rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") %>%
     rvest::html_text() %>%
     expect_equal(c("X", "X", "Y", "Y"))
 
-  tbl_html_3 <-
+  tbl_html_2 <-
     gt(tbl_b) %>%
-    tab_spanner_delim(
-      delim = "_",
-      gather = TRUE
-    )
+    tab_spanner_delim(delim = "_")
 
-  tbl_html_3 %>%
+  tbl_html_2 %>%
     dt_spanners_get() %>%
     .$spanner_label %>%
     unlist() %>%
-    expect_equal(c("A", "B"))
+    expect_equal(c("A", "B", "A", "B"))
 
-  tbl_html_3 %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_column_spanner']") %>%
-    rvest::html_text() %>%
-    expect_equal(c("A", "B"))
-
-  tbl_html_3 %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") %>%
-    rvest::html_text() %>%
-    expect_equal(c("Z", "X", "Y", "X", "Y"))
-
-  tbl_html_3 %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_row gt_right']") %>%
-    rvest::html_text() %>%
-    expect_equal(c("1", "3", "2", "4", "5"))
-
-  tbl_html_4 <-
-    gt(tbl_b) %>%
-    tab_spanner_delim(
-      delim = "_",
-      gather = FALSE
-    )
-
-  tbl_html_4 %>%
-    dt_spanners_get() %>%
-    .$spanner_label %>%
-    unlist() %>%
-    expect_equal(c("A", "B"))
-
-  tbl_html_4 %>%
+  tbl_html_2 %>%
     render_as_html() %>%
     xml2::read_html() %>%
     rvest::html_nodes("[class='gt_column_spanner']") %>%
     rvest::html_text() %>%
     expect_equal(c("A", "B", "A", "B"))
 
-  tbl_html_4 %>%
+  tbl_html_2 %>%
     render_as_html() %>%
     xml2::read_html() %>%
     rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") %>%
     rvest::html_text() %>%
     expect_equal(c("Z", "X", "X", "Y", "Y"))
 
-  tbl_html_4 %>%
+  tbl_html_2 %>%
     render_as_html() %>%
     xml2::read_html() %>%
     rvest::html_nodes("[class='gt_row gt_right']") %>%
     rvest::html_text() %>%
     expect_equal(c("1", "2", "3", "4", "5"))
 
-  tbl_html_5 <-
+  tbl_html_3 <-
     gt(tbl_c) %>%
-    tab_spanner_delim(
-      delim = "_",
-      gather = TRUE
-    )
+    tab_spanner_delim(delim = "_")
 
-  tbl_html_5 %>%
+  tbl_html_3 %>%
     dt_spanners_get() %>%
     .$spanner_label %>%
     unlist() %>%
-    expect_equal(c("A", "B"))
+    expect_equal(c("A", "B", "A", "B"))
 
-  tbl_html_5 %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_column_spanner']") %>%
-    rvest::html_text() %>%
-    expect_equal(c("A", "B"))
-
-  tbl_html_5 %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") %>%
-    rvest::html_text() %>%
-    expect_equal(c("Z", "X", "Y", "X", "Y"))
-
-  tbl_html_5 %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_row gt_right']") %>%
-    rvest::html_text() %>%
-    expect_equal(c("0.5", "1", "3", "2", "4"))
-
-  tbl_html_6 <-
-    gt(tbl_c) %>%
-    tab_spanner_delim(
-      delim = "_",
-      gather = FALSE
-    )
-
-  tbl_html_6 %>%
-    dt_spanners_get() %>%
-    .$spanner_label %>%
-    unlist() %>%
-    expect_equal(c("A", "B"))
-
-  tbl_html_6 %>%
+  tbl_html_3 %>%
     render_as_html() %>%
     xml2::read_html() %>%
     rvest::html_nodes("[class='gt_column_spanner']") %>%
     rvest::html_text() %>%
     expect_equal(c("A", "B", "A", "B"))
 
-  tbl_html_6 %>%
+  tbl_html_3 %>%
     render_as_html() %>%
     xml2::read_html() %>%
     rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") %>%
     rvest::html_text() %>%
     expect_equal(c("Z", "X", "X", "Y", "Y"))
 
-  tbl_html_6 %>%
+  tbl_html_3 %>%
     render_as_html() %>%
     xml2::read_html() %>%
     rvest::html_nodes("[class='gt_row gt_right']") %>%
@@ -513,10 +264,7 @@ test_that("`tab_spanner_delim()` works on higher-order spanning", {
 
   gt_tbl_1 <-
     gt(tbl_1) %>%
-    tab_spanner_delim(
-      delim = ".",
-      gather = TRUE
-    )
+    tab_spanner_delim(delim = ".")
 
   # Take snapshots of `gt_tbl_1`
   gt_tbl_1 %>% render_as_html() %>% expect_snapshot()
@@ -531,10 +279,7 @@ test_that("`tab_spanner_delim()` works on higher-order spanning", {
 
   gt_tbl_2 <-
     gt(tbl_2) %>%
-    tab_spanner_delim(
-      delim = ".",
-      gather = TRUE
-    )
+    tab_spanner_delim(delim = ".")
 
   # Take snapshots of `gt_tbl_2`
   gt_tbl_2 %>% render_as_html() %>% expect_snapshot()
@@ -549,10 +294,7 @@ test_that("`tab_spanner_delim()` works on higher-order spanning", {
 
   gt_tbl_3 <-
     gt(tbl_3) %>%
-    tab_spanner_delim(
-      delim = ".",
-      gather = TRUE
-    )
+    tab_spanner_delim(delim = ".")
 
   # Take snapshots of `gt_tbl_3`
   gt_tbl_3 %>% render_as_html() %>% expect_snapshot()
@@ -567,10 +309,7 @@ test_that("`tab_spanner_delim()` works on higher-order spanning", {
 
   gt_tbl_4 <-
     gt(tbl_4) %>%
-    tab_spanner_delim(
-      delim = ".",
-      gather = TRUE
-    )
+    tab_spanner_delim(delim = ".")
 
   # Take snapshots of `gt_tbl_4`
   gt_tbl_4 %>% render_as_html() %>% expect_snapshot()
@@ -585,10 +324,7 @@ test_that("`tab_spanner_delim()` works on higher-order spanning", {
 
   gt_tbl_5a <-
     gt(tbl_5) %>%
-    tab_spanner_delim(
-      delim = ".",
-      gather = TRUE
-    )
+    tab_spanner_delim(delim = ".")
 
   # Take snapshots of `gt_tbl_5a`
   gt_tbl_5a %>% render_as_html() %>% expect_snapshot()
@@ -597,7 +333,6 @@ test_that("`tab_spanner_delim()` works on higher-order spanning", {
     gt(tbl_5) %>%
     tab_spanner_delim(
       delim = ".",
-      gather = TRUE,
       columns = c(all.W.A, all.X.B)
     )
 
@@ -608,7 +343,6 @@ test_that("`tab_spanner_delim()` works on higher-order spanning", {
     gt(tbl_5) %>%
     tab_spanner_delim(
       delim = ".",
-      gather = TRUE,
       columns = c(all.W.A, all.Z.B)
     )
 
@@ -653,12 +387,24 @@ test_that("`tab_spanner_delim()` works on higher-order spanning", {
   # Take snapshots of `gt_tbl_spanner_3`
   gt_tbl_spanner_3 %>% render_as_html() %>% expect_snapshot()
 
+  # Expect that a replacement of a spanner won't work
+  # because `replace = FALSE` by default
+  expect_error(
+    gt_tbl_spanner_3 %>%
+      tab_spanner(
+        label = md("**REPLACED**"),
+        spanners = everything(),
+        level = 3
+      )
+  )
+
   gt_tbl_spanner_4 <-
     gt_tbl_spanner_3 %>%
     tab_spanner(
       label = md("**REPLACED**"),
       spanners = everything(),
-      level = 3
+      level = 3,
+      replace = TRUE
     )
 
   # Take snapshots of `gt_tbl_spanner_4`
@@ -684,11 +430,17 @@ test_that("`tab_spanner_delim()` works on higher-order spanning", {
       label = "Data", columns = everything()
     )
 
+  # Take snapshots of `gt_tbl_spanner_A_1`
+  gt_tbl_spanner_A_1 %>% render_as_html() %>% expect_snapshot()
+
   gt_tbl_spanner_A_2 <-
     gt_tbl_1 %>%
     tab_spanner(
       label = "Data", spanners = everything()
     )
+
+  # Take snapshots of `gt_tbl_spanner_A_1`
+  gt_tbl_spanner_A_2 %>% render_as_html() %>% expect_snapshot()
 
   expect_equal(
     gt_tbl_spanner_A_1 %>% render_as_html(),
@@ -699,12 +451,47 @@ test_that("`tab_spanner_delim()` works on higher-order spanning", {
   # Highly specific placements of spanners
   #
 
+  # These won't work because `replace = FALSE` by default
+  expect_error(
+    gt_tbl_spanner_A_1 %>%
+      tab_spanner(
+        label = "Cut In",
+        columns = 1,
+        level = 3
+      )
+  )
+  expect_error(
+    gt_tbl_spanner_A_1 %>%
+      tab_spanner(
+        label = "Cut In",
+        columns = c(1, 2),
+        level = 3
+      )
+  )
+  expect_error(
+    gt_tbl_spanner_A_1 %>%
+      tab_spanner(
+        label = "Cut In",
+        columns = c(1, 2, 3),
+        level = 3
+      )
+  )
+  expect_error(
+    gt_tbl_spanner_A_1 %>%
+      tab_spanner(
+        label = "Cut In",
+        columns = c(1, 2, 3, 4),
+        level = 3
+      )
+  )
+
   gt_tbl_spanner_B <-
     gt_tbl_spanner_A_1 %>%
     tab_spanner(
       label = "Cut In",
       columns = c(1, 2),
-      level = 3
+      level = 3,
+      replace = TRUE
     )
 
   # Take snapshots of `gt_tbl_spanner_B`
@@ -743,11 +530,10 @@ test_that("`tab_spanner_delim()` works on higher-order spanning", {
 
   gt_tbl_1 <-
     gt(tbl_1) %>%
-    tab_spanner_delim(
-      delim = ".",
-      gather = TRUE,
-      split = "last"
-    )
+    tab_spanner_delim(delim = ".", split = "last")
+
+  # Take snapshots of `gt_tbl_1`
+  gt_tbl_1 %>% render_as_html() %>% expect_snapshot()
 
   tbl_1_rev <-
     dplyr::tibble(
@@ -759,15 +545,82 @@ test_that("`tab_spanner_delim()` works on higher-order spanning", {
 
   gt_tbl_1_rev <-
     gt(tbl_1_rev) %>%
-    tab_spanner_delim(
-      delim = ".",
-      gather = TRUE,
-      split = "first"
+    tab_spanner_delim(delim = ".", split = "first")
+
+  # Take snapshots of `gt_tbl_1_rev`
+  gt_tbl_1_rev %>% render_as_html() %>% expect_snapshot()
+
+  # Both tables should be equivalently laid out
+  expect_equal(
+    gt_tbl_1 %>% render_as_html(),
+    gt_tbl_1_rev %>% render_as_html()
+  )
+
+  tbl_6 <-
+    dplyr::tibble(
+      A.B.C.D.E = 1,
+      A.B.C.D = 2,
+      A.B.C = 3,
+      A.B = 4,
+      A = 5
     )
 
-  # TODO: This should pass but it doesn't currently
-  # expect_equal(
-  #   gt_tbl_1 %>% render_as_html(),
-  #   gt_tbl_1_rev %>% render_as_html()
-  # )
+  gt_tbl_6_first <-
+    gt(tbl_6) %>%
+    tab_spanner_delim(delim = ".", split = "first")
+
+  # Take snapshots of `gt_tbl_6_first`
+  gt_tbl_6_first %>% render_as_html() %>% expect_snapshot()
+
+  gt_tbl_6_last <-
+    gt(tbl_6) %>%
+    tab_spanner_delim(delim = ".", split = "last")
+
+  # Take snapshots of `gt_tbl_6_last`
+  gt_tbl_6_last %>% render_as_html() %>% expect_snapshot()
+
+  # Expect an error should a `id` value be reused (here,
+  # it's `"**Z**"` that's used twice)
+  expect_error(
+    gt_tbl_6_last %>%
+      tab_spanner(label = md("**Z**"), columns = 5, level = 4) %>%
+      tab_spanner(label = md("**Z**"), columns = 5, level = 3)
+  )
+
+  gt_tbl_6_z <-
+    gt_tbl_6_last %>%
+    tab_spanner(label = md("**Z**"), columns = 5, level = 4, id = "Z4") %>%
+    tab_spanner(label = md("**Z**"), columns = 5, level = 3, id = "Z3") %>%
+    tab_spanner(label = md("**Z**"), columns = 5, level = 2, id = "Z2") %>%
+    tab_spanner(label = md("**Z**"), columns = 5, level = 1, id = "Z1")
+
+  # Take snapshots of `gt_tbl_6_z`
+  gt_tbl_6_z %>% render_as_html() %>% expect_snapshot()
+
+  # A replacement of an spanner with the same `id` doesn't work (even
+  # though the duplicated ID does not result in duplicate IDs afterward)
+  expect_error(
+    gt_tbl_6_z %>%
+      tab_spanner(
+        label = md("**Y**"),
+        columns = 5,
+        level = 4,
+        id = "Z4",
+        replace = TRUE
+      )
+  )
+
+  # Moving to a unique ID works
+  gt_tbl_6_yzzz <-
+    gt_tbl_6_z %>%
+    tab_spanner(
+      label = md("**Y**"),
+      columns = 5,
+      level = 4,
+      id = "Y4",
+      replace = TRUE
+    )
+
+  # Take snapshots of `gt_tbl_6_yzzz`
+  gt_tbl_6_yzzz %>% render_as_html() %>% expect_snapshot()
 })
