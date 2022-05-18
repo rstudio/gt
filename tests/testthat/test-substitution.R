5f10ce829473716dd898c1c83f54092db4ad7a8e
Richard Iannone <riannone@me.com>
2022-05-17 23:15:46 -0400

    Add several testthat tests

--- tests/testthat/test-substitution.R
+++ tests/testthat/test-substitution.R
@@ -46,9 +46,16 @@ test_that("the `sub_missing()` function works correctly", {
 
   expect_equal(
     (tab %>%
-       sub_missing(columns = "num_1", missing_text = "--") %>%
-       render_formats_test(context = "html"))[["num_1"]],
-    c("&ndash;", "74", "&ndash;", "93", "&ndash;", "76", "&ndash;")
+       sub_missing(columns = "num_1") %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c("—", "74", "—", "93", "—", "76", "—")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1") %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c("\\'97", "74", "\\'97", "93", "\\'97", "76", "\\'97")
   )
 
   expect_equal(
@@ -60,9 +67,23 @@ test_that("the `sub_missing()` function works correctly", {
 
   expect_equal(
     (tab %>%
-       sub_missing(columns = "num_1", missing_text = "a") %>%
+       sub_missing(columns = "num_1", missing_text = "--") %>%
        render_formats_test(context = "html"))[["num_1"]],
-    c("a", "74", "a", "93", "a", "76", "a")
+    c("&ndash;", "74", "&ndash;", "93", "&ndash;", "76", "&ndash;")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1", missing_text = "--") %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c("–", "74", "–", "93", "–", "76", "–")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1", missing_text = "--") %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c("\\'96", "74", "\\'96", "93", "\\'96", "76", "\\'96")
   )
 
   expect_equal(
@@ -70,6 +91,105 @@ test_that("the `sub_missing()` function works correctly", {
        sub_missing(columns = "num_2") %>%
        render_formats_test(context = "default"))[["num_2"]],
     c("34", "74", "23", "93", "35", "76", "57")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1", missing_text = "a") %>%
+       render_formats_test(context = "default"))[["num_1"]],
+    c("a", "74", "a", "93", "a", "76", "a")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1", missing_text = "a") %>%
+       render_formats_test(context = "html"))[["num_1"]],
+    c("a", "74", "a", "93", "a", "76", "a")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1", missing_text = "a") %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c("a", "74", "a", "93", "a", "76", "a")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1", missing_text = "a") %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c("a", "74", "a", "93", "a", "76", "a")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1", missing_text = md("**a** *b*")) %>%
+       render_formats_test(context = "default"))[["num_1"]],
+    c("**a** *b*", "74", "**a** *b*", "93", "**a** *b*", "76", "**a** *b*")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1", missing_text = md("**a** *b*")) %>%
+       render_formats_test(context = "html"))[["num_1"]],
+    c(
+      "<strong>a</strong> <em>b</em>", "74", "<strong>a</strong> <em>b</em>",
+      "93", "<strong>a</strong> <em>b</em>", "76", "<strong>a</strong> <em>b</em>"
+    )
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1", missing_text = md("**a** *b*")) %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c(
+      "\\textbf{a} \\emph{b}", "74", "\\textbf{a} \\emph{b}", "93",
+      "\\textbf{a} \\emph{b}", "76", "\\textbf{a} \\emph{b}"
+    )
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1", missing_text = md("**a** *b*")) %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c(
+      "{\\b a} {\\i b}", "74", "{\\b a} {\\i b}", "93", "{\\b a} {\\i b}",
+      "76", "{\\b a} {\\i b}"
+    )
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1", missing_text = "<a> & <b>") %>%
+       render_formats_test(context = "html"))[["num_1"]],
+    c(
+      "&lt;a&gt; &amp; &lt;b&gt;", "74", "&lt;a&gt; &amp; &lt;b&gt;",
+      "93", "&lt;a&gt; &amp; &lt;b&gt;", "76", "&lt;a&gt; &amp; &lt;b&gt;"
+    )
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1", missing_text = " ~$ {a} & {b} $~") %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c(
+      " \\textasciitilde{}\\$ \\{a\\} \\& \\{b\\} \\$\\textasciitilde{}",
+      "74", " \\textasciitilde{}\\$ \\{a\\} \\& \\{b\\} \\$\\textasciitilde{}",
+      "93", " \\textasciitilde{}\\$ \\{a\\} \\& \\{b\\} \\$\\textasciitilde{}",
+      "76", " \\textasciitilde{}\\$ \\{a\\} \\& \\{b\\} \\$\\textasciitilde{}"
+    )
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = "num_1", missing_text = "\\emdash\\emdash\\emdash\\emdash") %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c(
+      "\\'5cemdash\\'5cemdash\\'5cemdash\\'5cemdash", "74",
+      "\\'5cemdash\\'5cemdash\\'5cemdash\\'5cemdash",
+      "93", "\\'5cemdash\\'5cemdash\\'5cemdash\\'5cemdash", "76",
+      "\\'5cemdash\\'5cemdash\\'5cemdash\\'5cemdash"
+    )
   )
 
   expect_equal(
@@ -93,6 +213,28 @@ test_that("the `sub_missing()` function works correctly", {
     c("&mdash;", "74.000", "&mdash;", "93.000", "&mdash;", "76.000", "&mdash;")
   )
 
+  expect_equal(
+    (tab %>%
+       fmt_number(
+         columns = everything(),
+         decimals = 3
+       ) %>%
+       sub_missing(columns = everything()) %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c("—", "$74.000$", "—", "$93.000$", "—", "$76.000$", "—")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(
+         columns = everything(),
+         decimals = 3
+       ) %>%
+       sub_missing(columns = everything()) %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c("\\'97", "74.000", "\\'97", "93.000", "\\'97", "76.000", "\\'97")
+  )
+
   # Reverse the ordering: use `sub_missing()` first
   # then `fmt_number()`; expect the same output as before
   expect_equal(
@@ -104,6 +246,28 @@ test_that("the `sub_missing()` function works correctly", {
        ) %>%
        render_formats_test(context = "html"))[["num_1"]],
     c("&mdash;", "74.000", "&mdash;", "93.000", "&mdash;", "76.000", "&mdash;")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = everything()) %>%
+       fmt_number(
+         columns = everything(),
+         decimals = 3
+       ) %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c("—", "$74.000$", "—", "$93.000$", "—", "$76.000$", "—")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_missing(columns = everything()) %>%
+       fmt_number(
+         columns = everything(),
+         decimals = 3
+       ) %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c("\\'97", "74.000", "\\'97", "93.000", "\\'97", "76.000", "\\'97")
   )
 })
 
@@ -140,9 +304,37 @@ test_that("the `sub_zero()` function works correctly", {
 
   expect_equal(
     (tab %>%
+       sub_zero(columns = "num_1") %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c("NA", "74.0000", "NA", "nil", "NA", "0.0001", "NA")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_zero(columns = "num_1") %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c("NA", "74.0000", "NA", "nil", "NA", "0.0001", "NA")
+  )
+
+  expect_equal(
+    (tab %>%
        sub_zero(columns = "num_1", zero_text = md("*nil*")) %>%
        render_formats_test(context = "html"))[["num_1"]],
     c("NA", "74.0000", "NA", "<em>nil</em>", "NA", "0.0001", "NA")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_zero(columns = "num_1", zero_text = md("*nil*")) %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c("NA", "74.0000", "NA", "\\emph{nil}", "NA", "0.0001", "NA")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_zero(columns = "num_1", zero_text = md("*nil*")) %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c("NA", "74.0000", "NA", "{\\i nil}", "NA", "0.0001", "NA")
   )
 
   expect_equal(
@@ -205,6 +397,20 @@ test_that("the `sub_small_vals()` function works correctly", {
 
   expect_equal(
     (tab %>%
+       sub_small_vals(columns = "num_1") %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c("-1.000e-04", "7.400e+01", "NA", "0.000e+00", "5.000e+12", "<0.01", "8.434e+01")
+  )
+
+  expect_equal(
+    (tab %>%
+       sub_small_vals(columns = "num_1") %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c("-1.000e-04", "7.400e+01", "NA", "0.000e+00", "5.000e+12", "<0.01", "8.434e+01")
+  )
+
+  expect_equal(
+    (tab %>%
        fmt_number(columns = num_1) %>%
        sub_small_vals(columns = "num_1") %>%
        render_formats_test(context = "html"))[["num_1"]],
@@ -214,9 +420,41 @@ test_that("the `sub_small_vals()` function works correctly", {
   expect_equal(
     (tab %>%
        fmt_number(columns = num_1) %>%
+       sub_small_vals(columns = "num_1") %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c("$0.00$", "$74.00$", "NA", "$0.00$", "$5,000,000,000,000.00$", "<0.01", "$84.34$")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_small_vals(columns = "num_1") %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c("0.00", "74.00", "NA", "0.00", "5,000,000,000,000.00", "<0.01", "84.34")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
        sub_small_vals(columns = "num_1", threshold = 100) %>%
        render_formats_test(context = "html"))[["num_1"]],
     c("0.00", "&lt;100", "NA", "0.00", "5,000,000,000,000.00", "&lt;100", "&lt;100")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_small_vals(columns = "num_1", threshold = 100) %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c("$0.00$", "<100", "NA", "$0.00$", "$5,000,000,000,000.00$", "<100", "<100")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_small_vals(columns = "num_1", threshold = 100) %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c("0.00", "<100", "NA", "0.00", "5,000,000,000,000.00", "<100", "<100")
   )
 
   expect_equal(
@@ -233,6 +471,28 @@ test_that("the `sub_small_vals()` function works correctly", {
   expect_equal(
     (tab %>%
        fmt_number(columns = num_1) %>%
+       sub_small_vals(columns = "num_1", sign = "-") %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c(
+      "\\textless{}\\emph{abs}(-0.01)", "$74.00$", "NA", "$0.00$",
+      "$5,000,000,000,000.00$", "$0.00$", "$84.34$"
+    )
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_small_vals(columns = "num_1", sign = "-") %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c(
+      "<{\\i abs}(-0.01)", "74.00", "NA", "0.00", "5,000,000,000,000.00",
+      "0.00", "84.34"
+    )
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
        sub_small_vals(columns = "num_1", small_pattern = "smol", sign = "-") %>%
        render_formats_test(context = "html"))[["num_1"]],
     c("smol", "74.00", "NA", "0.00", "5,000,000,000,000.00", "0.00", "84.34")
@@ -241,10 +501,48 @@ test_that("the `sub_small_vals()` function works correctly", {
   expect_equal(
     (tab %>%
        fmt_number(columns = num_1) %>%
-       sub_small_vals(columns = "num_1", small_pattern = "(({x}))", sign = "-") %>%
+       sub_small_vals(columns = "num_1", small_pattern = "smol", sign = "-") %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c("smol", "$74.00$", "NA", "$0.00$", "$5,000,000,000,000.00$", "$0.00$", "$84.34$")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_small_vals(columns = "num_1", small_pattern = "smol", sign = "-") %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c("smol", "74.00", "NA", "0.00", "5,000,000,000,000.00", "0.00", "84.34")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_small_vals(columns = "num_1", small_pattern = "<<{x}>>", sign = "-") %>%
        render_formats_test(context = "html"))[["num_1"]],
     c(
-      "((0.01))", "74.00", "NA", "0.00", "5,000,000,000,000.00",
+      "&lt;&lt;0.01&gt;&gt;", "74.00", "NA", "0.00",
+      "5,000,000,000,000.00", "0.00", "84.34"
+    )
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_small_vals(columns = "num_1", small_pattern = "{{{x}}}", sign = "-") %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c(
+      "\\{\\{0.01\\}\\}", "$74.00$", "NA", "$0.00$", "$5,000,000,000,000.00$",
+      "$0.00$", "$84.34$"
+    )
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_small_vals(columns = "num_1", small_pattern = "\\d{x}\\d", sign = "-") %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c(
+      "\\'5cd0.01\\'5cd", "74.00", "NA", "0.00", "5,000,000,000,000.00",
       "0.00", "84.34"
     )
   )
@@ -314,9 +612,41 @@ test_that("the `sub_large_vals()` function works correctly", {
   expect_equal(
     (tab %>%
        fmt_number(columns = num_1) %>%
+       sub_large_vals(columns = "num_1") %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c("$0.00$", "$74.00$", "NA", "$0.00$", "$\\geq$1e+12", "$0.00$", "$84.34$")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_large_vals(columns = "num_1") %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c("0.00", "74.00", "NA", "0.00", ">=1e+12", "0.00", "84.34")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
        sub_large_vals(columns = "num_1", threshold = 100) %>%
        render_formats_test(context = "html"))[["num_1"]],
     c("0.00", "74.00", "NA", "0.00", "≥100", "0.00", "84.34")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_large_vals(columns = "num_1", threshold = 100) %>%
+       render_formats_test(context = "latex"))[["num_1"]],
+    c("$0.00$", "$74.00$", "NA", "$0.00$", "$\\geq$100", "$0.00$", "$84.34$")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_large_vals(columns = "num_1", threshold = 100) %>%
+       render_formats_test(context = "rtf"))[["num_1"]],
+    c("0.00", "74.00", "NA", "0.00", ">=100", "0.00", "84.34")
   )
 
   expect_equal(
@@ -330,6 +660,22 @@ test_that("the `sub_large_vals()` function works correctly", {
   expect_equal(
     (tab %>%
        fmt_number(columns = num_1) %>%
+       sub_large_vals(columns = "int_1", threshold = 500, sign = "-") %>%
+       render_formats_test(context = "latex"))[["int_1"]],
+    c("1", "$\\leq$-500", "800", "5", "NA", "0", "-32")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_large_vals(columns = "int_1", threshold = 500, sign = "-") %>%
+       render_formats_test(context = "rtf"))[["int_1"]],
+    c("1", "<=-500", "800", "5", "NA", "0", "-32")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
        sub_large_vals(columns = "int_1", threshold = 400, large_pattern = "big", sign = "-") %>%
        render_formats_test(context = "html"))[["int_1"]],
     c("1", "big", "800", "5", "NA", "0", "-32")
@@ -338,9 +684,41 @@ test_that("the `sub_large_vals()` function works correctly", {
   expect_equal(
     (tab %>%
        fmt_number(columns = num_1) %>%
-       sub_large_vals(columns = "int_1", threshold = 300, large_pattern = "(({x}))", sign = "-") %>%
+       sub_large_vals(columns = "int_1", threshold = 400, large_pattern = "big", sign = "-") %>%
+       render_formats_test(context = "latex"))[["int_1"]],
+    c("1", "big", "800", "5", "NA", "0", "-32")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_large_vals(columns = "int_1", threshold = 400, large_pattern = "big", sign = "-") %>%
+       render_formats_test(context = "rtf"))[["int_1"]],
+    c("1", "big", "800", "5", "NA", "0", "-32")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_large_vals(columns = "int_1", threshold = 300, large_pattern = "<<{x}>>", sign = "-") %>%
        render_formats_test(context = "html"))[["int_1"]],
-    c("1", "((300))", "800", "5", "NA", "0", "-32")
+    c("1", "&lt;&lt;300&gt;&gt;", "800", "5", "NA", "0", "-32")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_large_vals(columns = "int_1", threshold = 300, large_pattern = "{{{x}}}", sign = "-") %>%
+       render_formats_test(context = "latex"))[["int_1"]],
+    c("1", "\\{\\{300\\}\\}", "800", "5", "NA", "0", "-32")
+  )
+
+  expect_equal(
+    (tab %>%
+       fmt_number(columns = num_1) %>%
+       sub_large_vals(columns = "int_1", threshold = 300, large_pattern = "\\d{x}\\d", sign = "-") %>%
+       render_formats_test(context = "rtf"))[["int_1"]],
+    c("1", "\\'5cd300\\'5cd", "800", "5", "NA", "0", "-32")
   )
 
   # Expect that the order of `fmt_*()` and `sub_*()` functions shouldn't
