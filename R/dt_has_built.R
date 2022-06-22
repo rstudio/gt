bd995770e1d40e898d76618bdaf3aecf65e7eca0
Richard Iannone <riannone@me.com>
2022-06-22 14:37:17 -0400

    Restyle fns in `dt_has_built.R`

--- R/dt_has_built.R
+++ R/dt_has_built.R
@@ -1,22 +1,18 @@
 .dt_has_built_key <- "_has_built"
 
 dt_has_built_get <- function(data) {
-
   dt__get(data, .dt_has_built_key)
 }
 
 dt_has_built_set <- function(data, value) {
-
   dt__set(data, .dt_has_built_key, value)
 }
 
 dt_has_built_init <- function(data) {
-
   dt_has_built_set(data = data, value = FALSE)
 }
 
 dt_has_built <- function(data) {
-
   isTRUE(dt_has_built_get(data = data))
 }
 
