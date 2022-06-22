15bf746c083444a1ac216f3f541aeaa53264dd6e
Richard Iannone <riannone@me.com>
2022-06-22 17:01:43 -0400

    Make correction to `paste_between()` stmt

--- R/helpers.R
+++ R/helpers.R
@@ -2293,11 +2293,9 @@ cell_style_structure <- function(name, obj, subclass = name) {
 #' @export
 google_font <- function(name) {
 
-  name <- gsub(" ", "+", name)
-
   import_stmt <-
     paste_between(
-      name,
+      gsub(" ", "+", name),
       c(
         "@import url('https://fonts.googleapis.com/css2?family=",
         ":ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');"
