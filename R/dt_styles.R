b6b569e4d55ad6215fcbc9ad55e9364c9cc5c6a3
Richard Iannone <riannone@me.com>
2022-06-22 15:24:08 -0400

    Refactor/restyle fns in `dt_styles.R`

--- R/dt_styles.R
+++ R/dt_styles.R
@@ -1,27 +1,27 @@
 .dt_styles_key <- "_styles"
 
 dt_styles_get <- function(data) {
-
   dt__get(data, .dt_styles_key)
 }
 
 dt_styles_set <- function(data, styles) {
-
   dt__set(data, .dt_styles_key, styles)
 }
 
 dt_styles_init <- function(data) {
 
-  dplyr::tibble(
-    locname = character(0),
-    grpname = character(0),
-    colname = character(0),
-    locnum = numeric(0),
-    rownum = integer(0),
-    colnum = integer(0),
-    styles = list()
-  ) %>%
-    dt_styles_set(styles = ., data = data)
+  styles_tbl <-
+    dplyr::tibble(
+      locname = character(0),
+      grpname = character(0),
+      colname = character(0),
+      locnum = numeric(0),
+      rownum = integer(0),
+      colnum = integer(0),
+      styles = list()
+    )
+
+  dt_styles_set(data = data, styles = styles_tbl)
 }
 
 dt_styles_add <- function(
@@ -33,31 +33,41 @@ dt_styles_add <- function(
     rownum,
     styles
 ) {
-  grid <- expand.grid(
-    colname = unique(colname),
-    grpname = unique(grpname),
-    rownum = unique(rownum),
-    stringsAsFactors = FALSE
+
+  grid <-
+    expand.grid(
+      colname = unique(colname),
+      grpname = unique(grpname),
+      rownum = unique(rownum),
+      stringsAsFactors = FALSE
+    )
+
+  result <-
+    dplyr::tibble(
+      locname = locname,
+      grpname = grid$grpname,
+      colname = grid$colname,
+      locnum = locnum,
+      colnum = NA_integer_,
+      rownum = grid$rownum,
+      styles = list(styles)
+    )
+
+  dt_styles_set(
+    data = data,
+    styles = dplyr::bind_rows(dt_styles_get(data = data), result)
   )
-  result <- dplyr::tibble(
-    locname = locname,
-    grpname = grid$grpname,
-    colname = grid$colname,
-    locnum = locnum,
-    colnum = NA_integer_,
-    rownum = grid$rownum,
-    styles = list(styles)
-  )
-  dt_styles_set(styles = dplyr::bind_rows(dt_styles_get(data), result), data = data)
 }
 
-dt_styles_pluck <- function(styles_tbl,
-  locname = missing_arg(),
-  grpname = missing_arg(),
-  colname = missing_arg(),
-  locnum = missing_arg(),
-  rownum = missing_arg(),
-  grprow = missing_arg()) {
+dt_styles_pluck <- function(
+    styles_tbl,
+    locname = missing_arg(),
+    grpname = missing_arg(),
+    colname = missing_arg(),
+    locnum = missing_arg(),
+    rownum = missing_arg(),
+    grprow = missing_arg()
+) {
 
   if (is.null(styles_tbl$html_style)) {
     cli::cli_abort(
