8f8c8dc08383f2b4196f1fe94ba0d44c9a455b97
Richard Iannone <riannone@me.com>
2022-06-23 14:03:11 -0400

    Simplify testing of `as_raw_html()`

--- tests/testthat/test-as_raw_html.R
+++ tests/testthat/test-as_raw_html.R
@@ -5,7 +5,7 @@ test_that("`as_raw_html()` produces the same table every time", {
   gt(exibble) %>%
     as_raw_html(inline_css = TRUE) %>%
     nchar() %>%
-    expect_snapshot()
+    expect_equal(35730)
 
   gt(
     mtcars,
@@ -109,5 +109,5 @@ test_that("`as_raw_html()` produces the same table every time", {
     ) %>%
     as_raw_html(inline_css = TRUE) %>%
     nchar() %>%
-    expect_snapshot()
+    expect_equal(162967)
 })
