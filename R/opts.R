46a352744c10171975e5e92859c179770a96d3cc
Richard Iannone <riannone@me.com>
2022-07-01 15:28:50 -0400

    Update opts.R

--- R/opts.R
+++ R/opts.R
@@ -917,6 +917,298 @@ opt_table_font <- function(
   data
 }
 
+#' Stylize your table with a colorful look
+#'
+#' This function applies a set of styles related to background colors,
+#' line colors, and line styles. There are six `style`s to choose from, they
+#' largely vary in the extent of coloring applied to different locations. Some
+#' have table borders applied, some apply darker colors to the table stub and
+#' summary sections, and, some even have vertical lines. In addition to `style`,
+#' there are seven `color` variations that each use a range of five color tints.
+#' Each of the color tints have been fine-tuned to maximize the contrast between
+#' text and its background. In total, There are 42 combinations of `style` and
+#' `color`. We can use the [info_preset_colorized()] function to get an
+#' information table which has the names and previews for each of the color list
+#' keywords.
+#'
+#' @inheritParams fmt_number
+#' @param style Six numbered styles are available. Simply provide a number from
+#'   `1` (the default) to `6` to choose a distinct look.
+#' @param color There are seven color variations: `"blue"` (the default),
+#'   `"orange"`, `"aqua"`, `"pink"`, `"green"`, `"red"`, and `"gray"`.
+#'
+#' @return an object of class `gt_tbl`.
+#'
+#' @examples
+#' # Use `exibble` to create a gt table with
+#' # a number of table parts added; then, use
+#' # the `preset_colorized()` function to give
+#' # the table some additional style (using
+#' # the `"aqua"` color variation and style
+#' # number `6`)
+#' tab_1 <-
+#'   exibble %>%
+#'   gt(rowname_col = "row", groupname_col = "group") %>%
+#'   summary_rows(
+#'     groups = "grp_a",
+#'     columns = vars(num, currency),
+#'     fns = list(
+#'       min = ~min(., na.rm = TRUE),
+#'       max = ~max(., na.rm = TRUE)
+#'     )) %>%
+#'   grand_summary_rows(
+#'     columns = vars(currency),
+#'     fns = list(
+#'       total = ~sum(., na.rm = TRUE)
+#'     )) %>%
+#'   tab_source_note(source_note = "This is a source note.") %>%
+#'   tab_footnote(
+#'     footnote = "This is a footnote.",
+#'     locations = cells_data(columns = 1, rows = 1)
+#'   ) %>%
+#'   tab_header(
+#'     title = "The title of the table",
+#'     subtitle = "The table's subtitle"
+#'   ) %>%
+#'   preset_colorized(style = 6, color = "aqua")
+#'
+#' @family Table Option Functions
+#' @section Function ID:
+#' 9-10
+#'
+#' @export
+opt_stylize <- function(
+    data,
+    style = 1,
+    color = "blue"
+) {
+
+  if (!(length(style) == 1 && style %in% 1:6)) {
+    stop("The `style` value must be a number in the range of 1 to 6",
+         call. = FALSE)
+  }
+
+  if (!(length(color) == 1 && color %in% names(preset_colors))) {
+    stop("The `color` value must be one of seven specific colors:\n",
+         " * `\"blue\"`, `\"orange\"`, `\"aqua\"`, `\"pink\"`, `\"green\"`, ",
+         "`\"red\"`, or `\"gray\"`",
+         call. = FALSE)
+  }
+
+  # Get the set of parameters based on the `style` and `color` values
+  params <-
+    get_colorized_params(
+      styles_colors_params = styles_colors_params,
+      style = style,
+      color = color
+    )
+
+  # TODO: develop a function that performs a table reset; it
+  #       only resets the table style parameters in `_options`
+  tbl_colorized <-
+    data %>%
+    dt_options_init() %>%
+    tab_options(
+      table.border.top.color = params$table_hlines_color,
+      table.border.bottom.color = params$table_hlines_color,
+      heading.border.bottom.color = params$location_hlines_color,
+      column_labels.border.top.color = params$location_hlines_color,
+      column_labels.border.bottom.color = params$location_hlines_color,
+      row_group.border.top.color = params$location_hlines_color,
+      row_group.border.bottom.color = params$location_hlines_color,
+      summary_row.border.color = params$location_hlines_color,
+      grand_summary_row.border.color = params$location_hlines_color,
+      column_labels.background.color = params$column_labels_background_color,
+      stub.background.color = params$stub_background_color,
+      stub.border.style = params$stub_border_style,
+      stub.border.color = params$stub_border_color,
+      table_body.border.top.color = params$location_hlines_color,
+      table_body.border.bottom.color = params$location_hlines_color,
+      table_body.hlines.style = params$data_hlines_style,
+      table_body.hlines.color = params$data_hlines_color,
+      table_body.vlines.style = params$data_vlines_style,
+      table_body.vlines.color = params$data_vlines_color,
+      summary_row.background.color = params$summary_row_background_color,
+      grand_summary_row.background.color = params$grand_summary_row_background_color,
+      row.striping.background_color = params$row_striping_background_color
+    )
+
+  if (!is.na(params$table_outline_color)) {
+
+    tbl_colorized <-
+      tbl_colorized %>%
+      opt_add_table_outline(color = params$table_outline_color, width = px(3))
+  }
+
+  tbl_colorized
+}
+
+# Colors for the `preset_colorized()` function
+preset_colors <-
+  list(
+    gray =    c("#F4F4F4", "#D5D5D5", "#929292", "#5F5F5F", "#000000"),
+    blue =    c("#EDF7FC", "#89D3FE", "#00A1FF", "#0076BA", "#004D80"),
+    orange =  c("#FFFFDB", "#FEFC66", "#FAE231", "#F8BA00", "#FF9400"),
+    aqua =    c("#EBFBF9", "#A5FEF2", "#14E6CF", "#01A99D", "#007C77"),
+    pink =    c("#FCF2F7", "#FF8CC6", "#EF5FA7", "#CB2A7B", "#99195F"),
+    green =   c("#EDF6E8", "#9AFD69", "#60D937", "#1EB100", "#027101"),
+    red =     c("#FEEDEC", "#FF968D", "#FF644E", "#ED220D", "#B51800")
+  )
+
+# Function for getting a vector of preset colors at a specified 'level'
+get_all_preset_colors_at_index <- function(index) {
+
+  preset_colors %>%
+    vapply(
+      FUN.VALUE = character(1),
+      USE.NAMES = FALSE,
+      FUN = function(x) x[[index]]
+    )
+}
+
+# A tibble that contains all style and color parameters for the
+# themed tables generated by the `preset_colorized()` function
+styles_colors_params <-
+  dplyr::tibble() %>%
+  dplyr::bind_rows(
+    dplyr::tibble( # style 1
+      color = names(preset_colors),
+      style = 1,
+      table_hlines_color = get_all_preset_colors_at_index(5),
+      location_hlines_color = get_all_preset_colors_at_index(4),
+      column_labels_background_color = "#FFFFFF",
+      stub_background_color = get_all_preset_colors_at_index(4),
+      stub_border_style = "solid",
+      stub_border_color = get_all_preset_colors_at_index(4),
+      data_hlines_style = "none",
+      data_hlines_color = get_all_preset_colors_at_index(2),
+      data_vlines_style = "none",
+      data_vlines_color = get_all_preset_colors_at_index(2),
+      summary_row_background_color = "#FFFFFF",
+      grand_summary_row_background_color = get_all_preset_colors_at_index(2),
+      row_striping_background_color = preset_colors$gray[[1]],
+      table_outline_color = NA_character_
+    )
+  ) %>%
+  dplyr::bind_rows(
+    dplyr::tibble( # style 2
+      color = names(preset_colors),
+      style = 2,
+      table_hlines_color = preset_colors$gray[[2]],
+      location_hlines_color = preset_colors$gray[[2]],
+      column_labels_background_color = get_all_preset_colors_at_index(5),
+      stub_background_color = "#FFFFFF",
+      stub_border_style = "solid",
+      stub_border_color = preset_colors$gray[[4]],
+      data_hlines_style = "solid",
+      data_hlines_color = get_all_preset_colors_at_index(2),
+      data_vlines_style = "solid",
+      data_vlines_color = get_all_preset_colors_at_index(2),
+      summary_row_background_color = get_all_preset_colors_at_index(2),
+      grand_summary_row_background_color = get_all_preset_colors_at_index(3),
+      row_striping_background_color = preset_colors$gray[[1]],
+      table_outline_color = preset_colors$gray[[2]]
+    )
+  ) %>%
+  dplyr::bind_rows(
+    dplyr::tibble( # style 3
+      color = names(preset_colors),
+      style = 3,
+      table_hlines_color = preset_colors$gray[[3]],
+      location_hlines_color = preset_colors$gray[[3]],
+      column_labels_background_color = get_all_preset_colors_at_index(5),
+      stub_background_color = preset_colors$gray[[2]],
+      stub_border_style = "none",
+      stub_border_color = "#FFFFFF",
+      data_hlines_style = "dashed",
+      data_hlines_color = preset_colors$gray[[3]],
+      data_vlines_style = "none",
+      data_vlines_color = preset_colors$gray[[3]],
+      summary_row_background_color = "#FFFFFF",
+      grand_summary_row_background_color = preset_colors$gray[[2]],
+      row_striping_background_color = preset_colors$gray[[1]],
+      table_outline_color = NA_character_
+    )
+  ) %>%
+  dplyr::bind_rows(
+    dplyr::tibble( # style 4
+      color = names(preset_colors),
+      style = 4,
+      table_hlines_color = preset_colors$gray[[2]],
+      location_hlines_color = preset_colors$gray[[2]],
+      column_labels_background_color = get_all_preset_colors_at_index(4),
+      stub_background_color = preset_colors$gray[[3]],
+      stub_border_style = "dashed",
+      stub_border_color = preset_colors$gray[[2]],
+      data_hlines_style = "dashed",
+      data_hlines_color = preset_colors$gray[[2]],
+      data_vlines_style = "dashed",
+      data_vlines_color = preset_colors$gray[[2]],
+      summary_row_background_color = "#FFFFFF",
+      grand_summary_row_background_color = get_all_preset_colors_at_index(4),
+      row_striping_background_color = preset_colors$gray[[1]],
+      table_outline_color = preset_colors$gray[[2]]
+    )
+  ) %>%
+  dplyr::bind_rows(
+    dplyr::tibble( # style 5
+      color = names(preset_colors),
+      style = 5,
+      table_hlines_color = preset_colors$gray[[2]],
+      location_hlines_color = preset_colors$gray[[2]],
+      column_labels_background_color = get_all_preset_colors_at_index(5),
+      stub_background_color = preset_colors$gray[[3]],
+      stub_border_style = "solid",
+      stub_border_color = preset_colors$gray[[2]],
+      data_hlines_style = "solid",
+      data_hlines_color = preset_colors$gray[[2]],
+      data_vlines_style = "solid",
+      data_vlines_color = preset_colors$gray[[2]],
+      summary_row_background_color = preset_colors$gray[[4]],
+      grand_summary_row_background_color = preset_colors$gray[[3]],
+      row_striping_background_color = preset_colors$gray[[1]],
+      table_outline_color = preset_colors$gray[[2]]
+    )
+  ) %>%
+  dplyr::bind_rows(
+    dplyr::tibble( # style 6
+      color = names(preset_colors),
+      style = 6,
+      table_hlines_color = preset_colors$gray[[4]],
+      location_hlines_color = preset_colors$gray[[4]],
+      column_labels_background_color = get_all_preset_colors_at_index(4),
+      stub_background_color = get_all_preset_colors_at_index(2),
+      stub_border_style = "solid",
+      stub_border_color = preset_colors$gray[[2]],
+      data_hlines_style = "none",
+      data_hlines_color = preset_colors$gray[[2]],
+      data_vlines_style = "none",
+      data_vlines_color = preset_colors$gray[[2]],
+      summary_row_background_color = "#FFFFFF",
+      grand_summary_row_background_color = preset_colors$gray[[2]],
+      row_striping_background_color = get_all_preset_colors_at_index(1),
+      table_outline_color = NA_character_
+    )
+  )
+
+get_colorized_params <- function(
+    styles_colors_params,
+    style,
+    color
+) {
+
+  style_filter <- style
+  color_filter <- color
+
+  as.list(
+    dplyr::filter(
+      styles_colors_params,
+      style == style_filter,
+      color == color_filter
+    )
+  )
+}
+
 #' Option to add custom CSS for the table
 #'
 #' @description
