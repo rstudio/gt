d366ea748470e3ae230240ff077c8cce9d3e0fe2
Richard Iannone <riannone@me.com>
2022-09-08 12:38:53 -0400

    Update test-extract_cells.R

--- tests/testthat/test-extract_cells.R
+++ tests/testthat/test-extract_cells.R
@@ -7,7 +7,7 @@ test_that("Extraction of table cells works well", {
   # extraction to occur from the top to bottom of column, left to
   # right columns
   expect_equal(
-    gt_tbl %>% extract_cells(columns = everything()),
+    gt_tbl %>% extract_cells(columns = everything(), output = "plain"),
     c(
       "1.111e-01", "2.222e+00", "3.333e+01", "4.444e+02", "5.550e+03",
       "NA", "7.770e+05", "8.880e+06", "apricot", "banana", "coconut",
@@ -29,14 +29,14 @@ test_that("Extraction of table cells works well", {
   expect_equal(
     gt_tbl %>%
       cols_move_to_end(columns = num) %>%
-      extract_cells(columns = everything()),
+      extract_cells(columns = everything(), output = "plain"),
     gt_tbl %>%
-      extract_cells(columns = everything())
+      extract_cells(columns = everything(), output = "plain")
   )
 
   # Expect that a single column of cells can be extracted
   expect_equal(
-    gt_tbl %>% extract_cells(columns = char),
+    gt_tbl %>% extract_cells(columns = char, output = "plain"),
     c(
       "apricot", "banana", "coconut", "durian", NA,
       "fig", "grapefruit", "honeydew"
@@ -46,15 +46,15 @@ test_that("Extraction of table cells works well", {
   # Expect that `rows` works to get a subset of cells
   # from one or more columns
   expect_equal(
-    gt_tbl %>% extract_cells(columns = char, rows = c(1, 3)),
+    gt_tbl %>% extract_cells(columns = char, rows = c(1, 3), output = "plain"),
     c("apricot", "coconut")
   )
   expect_equal(
-    gt_tbl %>% extract_cells(columns = c(char, fctr), rows = c(1, 3)),
+    gt_tbl %>% extract_cells(columns = c(char, fctr), rows = c(1, 3), output = "plain"),
     c("apricot", "coconut", "one", "three")
   )
   expect_equal(
-    gt_tbl %>% extract_cells(columns = everything(), rows = 1),
+    gt_tbl %>% extract_cells(columns = everything(), rows = 1, output = "plain"),
     c(
       "1.111e-01", "apricot", "one", "2015-01-15", "13:35",
       "2018-01-01 02:22", "49.950", "row_1", "grp_a"
@@ -65,13 +65,13 @@ test_that("Extraction of table cells works well", {
   expect_equal(
     gt_tbl %>%
       fmt_number(columns = num, decimals = 5) %>%
-      extract_cells(columns = num, rows = c(1, 3)),
+      extract_cells(columns = num, rows = c(1, 3), output = "plain"),
     c("0.11110","33.33000")
   )
   expect_equal(
     gt_tbl %>%
       fmt_number(columns = num, rows = 1, decimals = 5) %>%
-      extract_cells(columns = num, rows = c(1, 3)),
+      extract_cells(columns = num, rows = c(1, 3), output = "plain"),
     c("0.11110","3.333e+01")
   )
   expect_equal(
@@ -80,7 +80,7 @@ test_that("Extraction of table cells works well", {
       fmt_number(columns = num, decimals = 4, rows = 1:2) %>%
       fmt_number(columns = num, decimals = 3, rows = 3) %>%
       fmt_number(columns = currency, decimals = 10) %>%
-      extract_cells(columns = num, rows = c(1, 3)),
+      extract_cells(columns = num, rows = c(1, 3), output = "plain"),
     c("0.1111","33.330")
   )
 
@@ -89,7 +89,7 @@ test_that("Extraction of table cells works well", {
     gt_tbl %>%
       fmt_number(columns = num, decimals = 5) %>%
       tab_footnote(footnote = "Footnote", locations = cells_body(columns = num)) %>%
-      extract_cells(columns = num, rows = c(1, 3)),
+      extract_cells(columns = num, rows = c(1, 3), output = "plain"),
     c("0.11110","33.33000")
   )
 
@@ -98,12 +98,6 @@ test_that("Extraction of table cells works well", {
     dplyr::tibble(a = c(0.1, 0.3, 0.9, -1.3, -2.233)) %>%
     gt()
 
-  expect_equal(
-    gt_tbl_2 %>%
-      fmt_fraction(columns = a, layout = "diagonal") %>%
-      extract_cells(columns = a),
-    c("1/9", "2/7", "8/9", "-1 2/7", "-2 2/9")
-  )
   expect_equal(
     gt_tbl_2 %>%
       fmt_fraction(columns = a, layout = "diagonal") %>%
@@ -154,30 +148,30 @@ test_that("Extraction of table cells works well", {
   expect_equal(
     gt_tbl_3 %>%
       fmt_number(columns = num, decimals = 5) %>%
-      extract_cells(columns = num, rows = c("row_1", "row_3")),
+      extract_cells(columns = num, rows = c("row_1", "row_3"), output = "plain"),
     c("0.11110", "33.33000")
   )
   expect_equal(
     gt_tbl_3 %>%
-      extract_cells(columns = row, rows = c("row_1", "row_3")),
+      extract_cells(columns = row, rows = c("row_1", "row_3"), output = "plain"),
     c("row_1", "row_3")
   )
   expect_null(
     gt_tbl_3 %>%
-      extract_cells(columns = group, rows = c("row_1", "row_3"))
+      extract_cells(columns = group, rows = c("row_1", "row_3"), output = "plain")
   )
   expect_equal(
     dplyr::tibble(a = c("*hey*", "**there**"), b = 1:2) %>%
       gt(rowname_col = "a") %>%
       fmt_markdown(columns = a) %>%
-      extract_cells(columns = a),
+      extract_cells(columns = a, output = "plain"),
     c("hey", "there")
   )
   expect_equal(
     dplyr::tibble(a = c("*hey*", "**there**"), b = 1:2) %>%
       gt(rowname_col = "a") %>%
       fmt_markdown(columns = stub()) %>%
-      extract_cells(columns = stub()),
+      extract_cells(columns = stub(), output = "plain"),
     c("hey", "there")
   )
   expect_equal(
@@ -190,13 +184,45 @@ test_that("Extraction of table cells works well", {
       "<div class='gt_from_md'><p><strong>there</strong></p>\n</div>"
     )
   )
+  expect_equal(
+    dplyr::tibble(a = c("*hey*", "**there**"), b = 1:2) %>%
+      gt(rowname_col = "a") %>%
+      fmt_markdown(columns = a) %>%
+      extract_cells(columns = a, output = "rtf"),
+    c("{\\i hey}", "{\\b there}")
+  )
+  expect_equal(
+    dplyr::tibble(a = c("*hey*", "**there**"), b = 1:2) %>%
+      gt(rowname_col = "a") %>%
+      fmt_markdown(columns = a) %>%
+      extract_cells(columns = a, output = "word"),
+    c(
+      "<w:rPr><w:i>true</w:i></w:rPr>hey<w:rPr><w:i>false</w:i></w:rPr>",
+      "<w:rPr><w:b w:val=\"true\"></w:b></w:rPr>there<w:rPr><w:b w:val=\"false\"></w:b></w:rPr>"
+    )
+  )
+
+  # Expect that text transforms are incorporated in the extracted vectors
+  expect_equal(
+    gt_tbl_3 %>%
+      fmt_number(columns = num, decimals = 5) %>%
+      text_transform(
+        locations = cells_body(columns = num),
+        fn = function(x) paste0(x, "!")
+      ) %>%
+      extract_cells(columns = num, output = "plain"),
+    c(
+      "0.11110!", "2.22200!", "33.33000!", "444.40000!", "5,550.00000!",
+      "NA!", "777,000.00000!", "8,880,000.00000!"
+    )
+  )
 
   # Expect that cells from hidden columns can be extracted
   expect_equal(
     gt_tbl_3 %>%
       fmt_number(columns = num, decimals = 5) %>%
       cols_hide(columns = num) %>%
-      extract_cells(columns = num, rows = c("row_1", "row_3")),
+      extract_cells(columns = num, rows = c("row_1", "row_3"), output = "plain"),
     c("0.11110", "33.33000")
   )
 
@@ -204,16 +230,16 @@ test_that("Extraction of table cells works well", {
   expect_error(
     gt_tbl_3 %>%
       fmt_number(columns = num, decimals = 5) %>%
-      extract_cells(columns = num, rows = "row_50")
+      extract_cells(columns = num, rows = "row_50", output = "plain")
   )
   expect_error(
     gt_tbl_3 %>%
       fmt_number(columns = num, decimals = 5) %>%
-      extract_cells(columns = num, rows = c("row_2", "row_50"))
+      extract_cells(columns = num, rows = c("row_2", "row_50"), output = "plain")
   )
   expect_error(
     gt_tbl_3 %>%
       fmt_number(columns = num, decimals = 5) %>%
-      extract_cells(columns = 10)
+      extract_cells(columns = 10, output = "plain")
   )
 })
