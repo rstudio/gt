093d66de3a2e72afd67790fa1d3dc572d2cb903f
Richard Iannone <riannone@me.com>
2022-11-04 16:38:41 -0400

    Update tab_info.R

--- R/tab_info.R
+++ R/tab_info.R
@@ -7,13 +7,14 @@
 #' [tab_spanner_delim()], etc.) while ensuring that duplicate ID values aren't
 #' produced. For the latter case, it is impossible to know what those ID values
 #' are unless one were to carefully examine to correct component of the `gt_tbl`
-#' object. Because it's essential to know these ID values for targeting purposes
+#' object.
+#'
+#' Because it's so essential to know these ID values for targeting purposes
 #' (when styling with [tab_style()], adding footnote marks with
 #' [tab_footnote()], etc.), the `tab_info()` function can help with all of this.
-#' It summarizes (by location) the ID values and their labels and provides
-#' additional pieces of useful information where needed. The product is an
-#' informational **gt** table, designed for easy retrieval of the necessary
-#' values.
+#' It summarizes (by location) all of the table's ID values and their associated
+#' labels. The product is an informational **gt** table, designed for easy
+#' retrieval of the necessary values.
 #'
 #' @inheritParams fmt_number
 #'
@@ -26,54 +27,50 @@
 #' @export
 tab_info <- function(data) {
 
-  data <- build_data(data = data, context = "html")
-
-  boxhead <- dt_boxhead_get(data = data)
-  stub_df <- dt_stub_df_get(data = data)
-  span_df <- dt_spanners_get(data = data)
-  groups_rows_df <- dt_groups_rows_get(data = data)
-
-  stub_layout <- get_stub_layout(data = data)
-
   empty_tbl <-
     dplyr::tibble(
       id = character(0),
       i = integer(0),
       label = character(0),
-      markdown = logical(0),
       type = character(0),
       location = character(0)
     )
+
+  built_data <- build_data(data, context = "html")
 
   #
   # Columns
   #
 
+  boxhead <- dt_boxhead_get(data = data)
+
   columns <- dplyr::select(boxhead, id = var, label = column_label, type)
   columns <- dplyr::filter(columns, type %in% c("default", "stub", "hidden"))
   columns <- dplyr::mutate(columns, label = unlist(label))
-  columns <- dplyr::mutate(columns, markdown = FALSE)
   columns <- dplyr::mutate(columns, location = "Columns")
   columns <- dplyr::mutate(columns, i = seq_len(nrow(columns)))
-  columns <- dplyr::select(columns, id, i, label, markdown, type, location)
+  columns <- dplyr::select(columns, id, i, label, type, location)
 
   #
   # Rows
   #
 
+  stub_df <- dt_stub_df_get(data = data)
+  stub_layout <- get_stub_layout(data = built_data)
+
   if ("rowname" %in% stub_layout) {
 
-    body_df <- dt_body_get(data = data)
+    data_df <- dt_data_get(data = data)
 
     rowname_col <- dt_boxhead_get_var_stub(data = data)
 
-    row_name_vals <- dplyr::pull(dplyr::select(body_df, dplyr::all_of(rowname_col)), 1)
+    row_name_vals <- dplyr::pull(dplyr::select(data_df, dplyr::all_of(rowname_col)), 1)
 
     rownames <- dplyr::select(stub_df, id = row_id, i = rownum_i)
     rownames <- dplyr::mutate(rownames, label = row_name_vals)
-    rownames <- dplyr::mutate(rownames, type = NA_character_, markdown = FALSE)
+    rownames <- dplyr::mutate(rownames, type = NA_character_)
     rownames <- dplyr::mutate(rownames, location = "Rows")
-    rownames <- dplyr::select(rownames, id, i, label, markdown, type, location)
+    rownames <- dplyr::select(rownames, id, i, label, type, location)
 
   } else if (nrow(stub_df) == 1) {
 
@@ -82,7 +79,6 @@ tab_info <- function(data) {
         id = "<< Single index value of 1 >>",
         i = NA_integer_,
         label = NA_character_,
-        markdown = NA,
         type = NA_character_,
         location = "Rows"
       )
@@ -94,7 +90,6 @@ tab_info <- function(data) {
         id = "<< No rows in table >>",
         i = NA_integer_,
         label = NA_character_,
-        markdown = NA,
         type = NA_character_,
         location = "Rows"
       )
@@ -110,7 +105,6 @@ tab_info <- function(data) {
         id = rownum_desc,
         i = NA_integer_,
         label = NA_character_,
-        markdown = NA,
         type = NA_character_,
         location = "Rows"
       )
@@ -122,13 +116,14 @@ tab_info <- function(data) {
 
   if (dt_spanners_exists(data = data)) {
 
+    span_df <- dt_spanners_get(data = data)
+
     spanners <- dplyr::select(span_df, id = spanner_id, label = spanner_label, i = spanner_level)
-    spanners <- dplyr::mutate(spanners, type = NA_character_, markdown = FALSE)
+    spanners <- dplyr::mutate(spanners, type = NA_character_)
     spanners <- dplyr::mutate(spanners, i = as.integer(i))
     spanners <- dplyr::mutate(spanners, label = unlist(label))
-    spanners <- dplyr::mutate(spanners, markdown = FALSE)
     spanners <- dplyr::mutate(spanners, location = "Spanners")
-    spanners <- dplyr::select(spanners, id, i, label, markdown, type, location)
+    spanners <- dplyr::select(spanners, id, i, label, type, location)
 
   } else {
     spanners <- empty_tbl
@@ -146,10 +141,10 @@ tab_info <- function(data) {
     row_groups <- dplyr::group_by(row_groups, id)
     row_groups <- dplyr::filter(row_groups, dplyr::row_number() == 1)
     row_groups <- dplyr::mutate(row_groups, i = which(groups_rows %in% id))
-    row_groups <- dplyr::mutate(row_groups, type = NA_character_, markdown = FALSE)
+    row_groups <- dplyr::mutate(row_groups, type = NA_character_)
     row_groups <- dplyr::mutate(row_groups, label = unlist(label))
     row_groups <- dplyr::mutate(row_groups, location = "Row Groups")
-    row_groups <- dplyr::select(row_groups, id, i, label, markdown, type, location)
+    row_groups <- dplyr::select(row_groups, id, i, label, type, location)
 
   } else {
     row_groups <- empty_tbl
@@ -164,6 +159,7 @@ tab_info <- function(data) {
   if (summaries_present) {
 
     list_of_summaries <- extract_summary(data = data)
+    groups_rows_df <- dt_groups_rows_get(data = built_data)
 
     # Group Summaries
     if (grand_summary_col %in% names(list_of_summaries$summary_df_data_list)) {
@@ -179,13 +175,21 @@ tab_info <- function(data) {
             list_of_summaries$summary_df_data_list[[group_id]][["rowname"]]
 
           group_summary_i <-
-            dplyr::tibble(
-              id = group_summary_row_id,
-              i = seq_len(length(group_summary_row_id)),
-              label = group_summary_row_id,
-              markdown = NA,
-              type = group_id,
-              location = "Group Summary"
+            dplyr::bind_rows(
+              dplyr::tibble(
+                id = group_id,
+                i = NA_integer_,
+                label = NA_character_,
+                type = "::group_id::",
+                location = "Group Summary"
+              ),
+              dplyr::tibble(
+                id = group_summary_row_id,
+                i = seq_len(length(group_summary_row_id)),
+                label = group_summary_row_id,
+                type = group_id,
+                location = "Group Summary"
+              )
             )
 
           group_summary <-
@@ -211,7 +215,6 @@ tab_info <- function(data) {
           id = grand_summary_row_id,
           i = seq_len(length(grand_summary_row_id)),
           label = grand_summary_row_id,
-          markdown = NA,
           type = NA_character_,
           location = "Grand Summary"
         )
@@ -224,10 +227,13 @@ tab_info <- function(data) {
         group_summary,
         grand_summary
       )
-
+  } else {
+    summaries <- empty_tbl
   }
 
-
+  #
+  # Combine the tables for each part together
+  #
 
   combined_tbl <-
     dplyr::bind_rows(
@@ -249,7 +255,6 @@ tab_info <- function(data) {
       .data = gt_tbl,
       label = "Label",
       i = md("*Idx*  \n*Lvl*"),
-      markdown = "",
       type = ""
     )
   gt_tbl <-
@@ -266,9 +271,18 @@ tab_info <- function(data) {
       style = cell_text(font = google_font("IBM Plex Mono"), size = px(14)),
       locations = list(cells_body(columns = c(i, label)), cells_stub())
     )
+  gt_tbl <-
+    tab_style(
+      data = gt_tbl,
+      style = cell_borders(sides = c("top", "bottom"), color = "lightblue"),
+      locations = list(
+        cells_stub(rows = type == "::group_id::"),
+        cells_body(rows = type == "::group_id::")
+      )
+    )
   gt_tbl <- opt_all_caps(data = gt_tbl, locations = c("row_group", "column_labels"))
   gt_tbl <- sub_missing(data = gt_tbl, columns = i, missing_text = "")
-  gt_tbl <- sub_missing(data = gt_tbl, columns = c(id, label))
+  gt_tbl <- sub_missing(data = gt_tbl, columns = c(id, label), missing_text = "")
   gt_tbl <-
     tab_header(
       data = gt_tbl,
@@ -276,6 +290,7 @@ tab_info <- function(data) {
     )
   gt_tbl <- opt_align_table_header(data = gt_tbl, align = "left")
   gt_tbl <- opt_table_lines(data = gt_tbl, extent = "none")
+  gt_tbl <- cols_hide(data = gt_tbl, columns = type)
   gt_tbl <-
     tab_options(
       data = gt_tbl,
@@ -288,11 +303,14 @@ tab_info <- function(data) {
       row_group.border.top.style = "solid",
       row_group.border.top.width = px(1),
       row_group.border.bottom.width = px(1),
+      table_body.border.bottom.style = "solid",
+      table_body.border.bottom.width = px(1),
       table.border.bottom.style = "solid",
       table.border.bottom.width = px(1),
       table.border.bottom.color = "#F7F7F7",
       source_notes.font.size = px(10),
       source_notes.padding = px(6)
     )
+
   gt_tbl
 }
