c83ac1b114c62a1dda670806310ecb45e5d8a296
Richard Iannone <riannone@me.com>
2022-11-08 20:39:03 -0500

    Update tab_style_specific.R

--- R/tab_style_specific.R
+++ R/tab_style_specific.R
@@ -40,14 +40,153 @@
 #'   target of each cell. By default, this is a vector just containing `"cell"`.
 #'   However, the keywords `"row"` and `"column"` may be used separately or in
 #'   combination to style the target cells' associated rows or columns.
-#' @param extents A vector of broad locations to project styling. By default,
-#'   this is a vector just containing `"body"`, whereby styled rows or columns
+#' @param extents A vector of locations to project styling. By default, this is
+#'   a vector just containing `"body"`, whereby styled rows or columns
 #'   (facilitated via inclusion of the `"row"` and `"column"` keywords in
-#'   `targets`) will not permeate into the stub or summary sections. Using the
-#'   keywords `"stub"` and `"summary"` may be used separately or in combination
-#'   to expand the styling into those locations.
+#'   `targets`) will not permeate into the stub. The additional keyword `"stub"`
+#'   may be used alone or in conjunction with `"body"` to project or expand the
+#'   styling into the stub.
 #'
 #' @return An object of class `gt_tbl`.
+#'
+#' @section Examples:
+#'
+#' Use `exibble` to create a **gt** table with a stub and row groups. This
+#' contains an assortment of values that could potentially undergo some styling
+#' via `tab_style_body()`.
+#'
+#' ```r
+#' gt_tbl <-
+#'   exibble %>%
+#'   gt(
+#'     rowname_col = "row",
+#'     groupname_col = "group"
+#'   )
+#' ```
+#'
+#' Cells in the table body can be styled through specification of literal values
+#' in the `values` argument of `tab_style_body()`. It's okay to search for
+#' numerical, character, or logical values across all columns. Let's target the
+#' values `49.95` and `33.33` and style those cells with an orange fill.
+#'
+#' ```r
+#' gt_tbl %>%
+#'   tab_style_body(
+#'     style = cell_fill(color = "orange"),
+#'     values = c(49.95, 33.33)
+#'   )
+#' ```
+#'
+#' \if{html}{\out{
+#' `r man_get_image_tag(file = "man_tab_style_body_1.png")`
+#' }}
+#'
+#' Multiple styles can be combined in a `list`, here's an example of that using
+#' the same cell targets:
+#'
+#' ```r
+#' gt_tbl %>%
+#'   tab_style_body(
+#'     style = list(
+#'       cell_text(font = google_font("Inter"), color = "white"),
+#'       cell_fill(color = "red"),
+#'       cell_borders(
+#'         sides = c("left", "right"),
+#'         color = "steelblue",
+#'         weight = px(4)
+#'       )
+#'     ),
+#'     values = c(49.95, 33.33)
+#'   )
+#' ```
+#'
+#' \if{html}{\out{
+#' `r man_get_image_tag(file = "man_tab_style_body_2.png")`
+#' }}
+#'
+#' You can opt to color entire rows or columns (or both, should you want to)
+#' with those specific keywords in the `targets` argument. For the `49.95` value
+#' we will style the entire row and with `33.33` the entire column will get the
+#' same styling.
+#'
+#' ```r
+#' gt_tbl %>%
+#'   tab_style_body(
+#'     style = cell_fill(color = "lightblue"),
+#'     values = 49.95,
+#'     targets = "row"
+#'   ) %>%
+#'   tab_style_body(
+#'     style = cell_fill(color = "lightblue"),
+#'     values = 33.33,
+#'     targets = "column"
+#'   )
+#' ```
+#'
+#' \if{html}{\out{
+#' `r man_get_image_tag(file = "man_tab_style_body_3.png")`
+#' }}
+#'
+#' In a minor variation to the prior example, it's possible to extend the
+#' styling to other locations, or, entirely project the styling elsewhere. This
+#' is done with the `extents` argument. Valid keywords that can be included in
+#' the vector are: `"body"` (the default) and `"stub"`. Let's take the previous
+#' example and extend the styling of the row into the stub.
+#'
+#' ```r
+#' gt_tbl %>%
+#'   tab_style_body(
+#'     style = cell_fill(color = "lightblue"),
+#'     values = 49.95,
+#'     targets = "row",
+#'     extents = c("body", "stub")
+#'   ) %>%
+#'   tab_style_body(
+#'     style = cell_fill(color = "lightblue"),
+#'     values = 33.33,
+#'     targets = "column"
+#'   )
+#' ```
+#'
+#' \if{html}{\out{
+#' `r man_get_image_tag(file = "man_tab_style_body_4.png")`
+#' }}
+#'
+#' We can also use the `pattern` argument to target cell values in
+#' `character`-based columns. The `"fctr"` column is skipped because it is in
+#' fact a factor-based column.
+#'
+#' ```r
+#' gt_tbl %>%
+#'   tab_style_body(
+#'     style = cell_fill(color = "green"),
+#'     pattern = "ne|na"
+#'   )
+#' ```
+#'
+#' \if{html}{\out{
+#' `r man_get_image_tag(file = "man_tab_style_body_5.png")`
+#' }}
+#'
+#' For the most flexibility in targeting, it's best to use the `fn` argument.
+#' The function you give to `fn` will be invoked separately on all cells so the
+#' `columns` argument of `tab_style_body()` might be useful to limit which cells
+#' should be evaluated. For this next example, the supplied function should only
+#' be used on numeric values and we can make sure of this by using `columns =
+#' where(is.numeric)`.
+#'
+#' ```r
+#' gt_tbl %>%
+#'   tab_style_body(
+#'     columns = where(is.numeric),
+#'     style = cell_fill(color = "pink"),
+#'     fn = function(x) x >= 0 && x < 50
+#'   )
+#' ```
+#'
+#' \if{html}{\out{
+#' `r man_get_image_tag(file = "man_tab_style_body_6.png")`
+#' }}
 #'
 #' @family part creation/modification functions
 #' @section Function ID:
@@ -79,7 +218,7 @@ tab_style_body <- function(
   extents <-
     rlang::arg_match(
       extents,
-      values = c("body", "stub", "summary"),
+      values = c("body", "stub"),
       multiple = TRUE
     )
 
@@ -157,12 +296,16 @@ tab_style_body <- function(
       for (row in x_idx) {
 
         locations_list <-
-          generate_locations_list(
-            col = {{ col }},
-            row = {{ row }},
-            targets = targets,
-            extents = extents
+          list(
+            if ("cell" %in% targets && "body" %in% extents) cells_body(columns = {{ col }}, rows = {{ row }}),
+            if ("cell" %in% targets && "stub" %in% extents) cells_stub(rows = {{ row }}),
+            if ("row" %in% targets && "body" %in% extents) cells_body(rows = {{ row }}),
+            if ("row" %in% targets && "stub" %in% extents) cells_stub(rows = {{ row }}),
+            if ("column" %in% targets && "body" %in% extents) cells_body(columns = {{ col }})
           )
+
+        # Remove NULL elements from `locations_list`
+        locations_list <- Filter(Negate(is.null), locations_list)
 
         data <-
           tab_style(
@@ -175,26 +318,4 @@ tab_style_body <- function(
   }
 
   data
-}
-
-generate_locations_list <- function(
-  col,
-  row,
-  targets,
-  extents
-) {
-
-  locations_list <-
-    list(
-      if ("cell" %in% targets && "body" %in% extents) cells_body(columns = {{ col }}, rows = {{ row }}),
-      if ("cell" %in% targets && "stub" %in% extents) cells_stub(rows = {{ row }}),
-      if ("row" %in% targets && "body" %in% extents) cells_body(columns = everything(), rows = {{ row }}),
-      if ("row" %in% targets && "stub" %in% extents) cells_stub(rows = {{ row }}),
-      if ("column" %in% targets && "body" %in% extents) cells_body(columns = {{ col }}, rows = everything()),
-      if ("column" %in% targets && "summary" %in% extents) cells_summary(groups = everything(), columns = {{ col }}, rows = everything()),
-      if ("column" %in% targets && "summary" %in% extents) cells_grand_summary(columns = {{ col }}, rows = everything())
-    )
-
-  # Remove NULL elements from `locations_list`
-  Filter(Negate(is.null), locations_list)
 }
