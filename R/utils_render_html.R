9ba7ad3f53bd61da3778d88040872d5ebf43e65a
Richard Iannone <riannone@me.com>
2022-11-10 00:58:52 -0500

    Update utils_render_html.R

--- R/utils_render_html.R
+++ R/utils_render_html.R
@@ -26,7 +26,7 @@ styles_to_html <- function(styles) {
       FUN.VALUE = character(1), USE.NAMES = FALSE,
       FUN = function(x) {
         if (any(is.null(names(x)))) {
-          style <- gsub(":", ": ", x, fixed = TRUE)
+          style <- x
         } else if (all(names(x) != "")) {
           x <- cell_style_to_html(x)
           style <- tidy_gsub(paste0(names(x), ": ", x, ";", collapse = " "), ";;", ";")
