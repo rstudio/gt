ff807bc32b189edd0e40fa88fb2432f14699be87
Alex_Lauer <alexandra.lauer@merckgroup.com>
2022-11-14 19:53:24 +0100

    Renamed SUBJID to USUBJID, created draft for gt_adsl

--- data-raw/07-gt_adpc.R
+++ data-raw/07-gt_adpc.R
@@ -8,10 +8,10 @@ gt_subjects <- paste0("GT", 1001:1018)
 gt_timepoints <- c(0, 0.5, 1, 1.5, 2, 4, 8, 12, 24)
 gt_treatments <- c("Placebo", "Treatment 1", "Treatment 2")
 
-gt_adpc <- expand.grid(SUBJID = gt_subjects,
+gt_adpc <- expand.grid(USUBJID = gt_subjects,
                        PKFL = "Y", 
                        ATPTN = gt_timepoints) |> 
-  dplyr::arrange(SUBJID, ATPTN) |> 
+  dplyr::arrange(USUBJID, ATPTN) |> 
   dplyr::mutate(ATPT = ifelse(ATPTN == 0, "predose", paste0(ATPTN, "hrs after administration")),
                 TRTAN = rep(1:3, each = dplyr::n()/3),
                 TRTA = factor(rep(gt_treatments, each = dplyr::n()/3),
@@ -20,6 +20,16 @@ gt_adpc <- expand.grid(SUBJID = gt_subjects,
                 AVALC = ifelse(AVAL == 0, "<LLOQ", signif(AVAL, digits = 3))
                 )
 
+set.seed(7)
 
-
+gt_adsl <- gt_adpc |> 
+  dplyr::select(USUBJID, TRTAN, TRTA) |> 
+  dplyr::distinct() |> 
+  dplyr::mutate(STUDYID = "GT01",
+                SEX = sample(c("F", "M", "U"), dplyr::n(), replace = TRUE, prob = c(.45, .52, .03)),
+                AAGE = runif(dplyr::n(), min = 22, max = 65),
+                AGE = round(AAGE),
+                BLBMI = runif(dplyr::n(), min = 16, max = 35),
+                PKFL = "Y",
+                ITTFL = sample(c("Y", "N"), dplyr::n(), replace = TRUE, prob = c(.9, .1)))
 
