7f01ca9bb533c058bada90f627c06ad58a0e219b
Richard Iannone <riannone@me.com>
2023-01-17 16:30:06 -0500

    Update helper-gt_attr_expectations.R

--- tests/testthat/helper-gt_attr_expectations.R
+++ tests/testthat/helper-gt_attr_expectations.R
@@ -107,7 +107,7 @@ expect_tab <- function(tab, df) {
 
   dt_options_get(data = tab) %>%
     dim() %>%
-    expect_equal(c(171, 5))
+    expect_equal(c(178, 5))
 
   dt_transforms_get(data = tab) %>%
     length() %>%
