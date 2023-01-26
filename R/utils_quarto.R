46a42f795597a3a8740fb22751ee78d7e648f0e3
Richard Iannone <riannone@me.com>
2023-01-26 11:37:31 -0500

    Add `quarto_api_send()` util function

--- R/utils_quarto.R
+++ R/utils_quarto.R
@@ -1,3 +1,35 @@
 check_quarto <- function() {
   Sys.getenv("QUARTO_MESSAGES_FILE") != ""
 }
+
+check_knitr <- function() {
+
+  if (!requireNamespace("knitr", quietly = TRUE)) {
+
+    cli::cli_abort(c(
+      "Please install the knitr package.",
+      "*" = "Use `install.packages(\"knitr\")`."
+    ))
+  }
+}
+
+quarto_api_send <- function(message, ...) {
+
+  # Ensure that the knitr package is available
+  check_knitr()
+
+  # Generate a JSON-formatted line
+  line <-
+    jsonlite::toJSON(
+      list(
+        message = message,
+        cell_name = knitr::opts_current$get("label"),
+        payload = list(...)
+      ),
+      auto_unbox = TRUE
+    )
+
+  file <- Sys.getenv("QUARTO_MESSAGES_FILE")
+
+  write(line, file, append = TRUE)
+}
