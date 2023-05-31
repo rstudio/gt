5ad92a4740885268166f0ed67ca97df0f71d5ec3
Richard Iannone <riannone@me.com>
2023-05-31 17:28:54 -0400

    Update shiny.R

--- R/shiny.R
+++ R/shiny.R
@@ -44,17 +44,22 @@
 #' @param expr An expression that creates a **gt** table object. For sake of
 #'   convenience, a data frame or tibble can be used here (it will be
 #'   automatically introduced to [gt()] with its default options).
+#'
 #' @param width,height The width and height of the table's container. Either can
 #'   be specified as a single-length character with units of pixels or as a
 #'   percentage. If provided as a single-length numeric vector, it is assumed
 #'   that the value is given in units of pixels. The [px()] and [pct()] helper
 #'   functions can also be used to pass in numeric values and obtain values as
 #'   pixel or percent units.
+#'
 #' @param align The alignment of the table in its container. By default, this is
 #'   `"center"`. Other options are `"left"` and `"right"`.
+#'
 #' @param env The environment in which to evaluate the `expr`.
+#'
 #' @param quoted Is `expr` a quoted expression (with `quote()`)? This is useful
 #'   if you want to save an expression in a variable.
+#'
 #' @param outputArgs A list of arguments to be passed through to the implicit
 #'   call to [gt_output()] when `render_gt()` is used in an interactive R
 #'   Markdown document.
