25c337a8f1095c38a9314f405f2ce64c7404ef7b
Richard Iannone <riannone@me.com>
2023-06-22 14:36:17 -0400

    Update test-utils_units.R

--- tests/testthat/test-utils_units.R
+++ tests/testthat/test-utils_units.R
@@ -2,7 +2,7 @@ generate_html_units <- function(input) {
   input %>% generate_token_list() %>% units_to_html()
 }
 
-test_that("Units are rendered properly", {
+test_that("Units are rendered properly in HTML", {
 
   expect_equal(
     generate_html_units("{{ kg }}"),
@@ -67,5 +67,25 @@ test_that("Units are rendered properly", {
   expect_equal(
     generate_html_units("m_0 s_1"),
     "m<span style=\"white-space:nowrap;\"><sub>0</sub></span> s<span style=\"white-space:nowrap;\"><sub>1</sub></span>"
+  )
+  expect_equal(
+    generate_html_units("m_0^2 s_1"),
+    "m<span style=\"white-space:nowrap;\"><sub>0</sub></span><span style=\"white-space:nowrap;\"><sup>2</sup></span> s<span style=\"white-space:nowrap;\"><sub>1</sub></span>"
+  )
+  expect_equal(
+    generate_html_units("m[_0^2]"),
+    "m<span style=\"display:inline-block;line-height:1em;text-align:left;font-size:60%;vertical-align:-0.25em;margin-left:0.1em;\">2<br>0</span>"
+  )
+  expect_equal(
+    generate_html_units("x10^-3"),
+    "&times;10<span style=\"white-space:nowrap;\"><sup>&minus;3</sup></span>"
+  )
+  expect_equal(
+    generate_html_units("( W / m^2 )"),
+    "(W / m<span style=\"white-space:nowrap;\"><sup>2</sup></span>)"
+  )
+  expect_equal(
+    generate_html_units("[ W / m^2 ]"),
+    "[W / m<span style=\"white-space:nowrap;\"><sup>2</sup></span>]"
   )
 })
