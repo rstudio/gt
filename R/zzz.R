becdba3220e238df2d6765b94fa5435f4d751f88
Richard Iannone <riannone@me.com>
2023-06-26 15:50:56 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -24,7 +24,13 @@
 
 #nocov start
 
-register_s3_method <- function(pkg, generic, class, fun = NULL) {
+register_s3_method <- function(
+    pkg,
+    generic,
+    class,
+    fun = NULL
+) {
+
   stopifnot(is.character(pkg), length(pkg) == 1)
   stopifnot(is.character(generic), length(generic) == 1)
   stopifnot(is.character(class), length(class) == 1)
