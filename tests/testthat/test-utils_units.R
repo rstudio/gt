c9fee3f9f01df86378b7522d93ad4d483c4418af
Richard Iannone <riannone@me.com>
2023-06-27 13:12:56 -0400

    Update test-utils_units.R

--- tests/testthat/test-utils_units.R
+++ tests/testthat/test-utils_units.R
@@ -65,24 +65,40 @@ test_that("Units are rendered properly in HTML", {
     "m<span style=\"white-space:nowrap;\"><sub>0</sub></span> s"
   )
   expect_equal(
+    generate_html_units("*m*_0 s"),
+    "<em>m</em><span style=\"white-space:nowrap;\"><sub>0</sub></span> s"
+  )
+  expect_equal(
+    generate_html_units("*m*_*0* s"),
+    "<em>m</em><span style=\"white-space:nowrap;\"><sub><em>0</em></sub></span> s"
+  )
+  expect_equal(
+    generate_html_units("*m*_*0*^2.3 s"),
+    "<em>m</em><span style=\"white-space:nowrap;\"><sub><em>0</em></sub></span><span style=\"white-space:nowrap;\"><sup>2.3</sup></span> s"
+  )
+  expect_equal(
+    generate_html_units("*m*_*0*^*n+2* s"),
+    "<em>m</em><span style=\"white-space:nowrap;\"><sub><em>0</em></sub></span><span style=\"white-space:nowrap;\"><sup><em>n+2</em></sup></span> s"
+  )
+  expect_equal(
     generate_html_units("m a.s.l."),
     "m a.s.l."
   )
   expect_equal(
     generate_html_units("degC"),
-    "&deg;C"
+    "°C"
   )
   expect_equal(
     generate_html_units("degF"),
-    "&deg;F"
+    "°F"
   )
   expect_equal(
     generate_html_units(":degree:F"),
-    "&deg;F"
+    "°F"
   )
   expect_equal(
     generate_html_units(":degrees:F"),
-    "&deg;F"
+    "°F"
   )
   expect_equal(
     generate_html_units("m_0 s_1"),
@@ -96,13 +112,15 @@ test_that("Units are rendered properly in HTML", {
     generate_html_units("ng m^−3 Na^+"),
     "ng m<span style=\"white-space:nowrap;\"><sup>−3</sup></span> Na<span style=\"white-space:nowrap;\"><sup>+</sup></span>"
   )
-  expect_equal(
-    generate_html_units("ng m^−3 *total salt*"),
-    "ng m<span style=\"white-space:nowrap;\"><sup>−3</sup></span> *total salt*"
+  expect_true(
+    grepl(
+      "ng m<span style=\"white-space:nowrap;\"><sup>.3</sup></span>.*?<em>total.*?salt</em>",
+      generate_html_units("ng m^−3 *total:space:salt*")
+    )
   )
   expect_equal(
     generate_html_units("5.5 :plusminus: 7.2 ng m^−3"),
-    "5.5 &plusmn; 7.2 ng m<span style=\"white-space:nowrap;\"><sup>−3</sup></span>"
+    "5.5 ± 7.2 ng m<span style=\"white-space:nowrap;\"><sup>−3</sup></span>"
   )
   expect_equal(
     generate_html_units("10^−12 cm^3 molecule^−1 s^−1"),
@@ -114,7 +132,7 @@ test_that("Units are rendered properly in HTML", {
   )
   expect_equal(
     generate_html_units("x10^-3"),
-    "&times;10<span style=\"white-space:nowrap;\"><sup>&minus;3</sup></span>"
+    "×10<span style=\"white-space:nowrap;\"><sup>&minus;3</sup></span>"
   )
   expect_equal(
     generate_html_units("( W / m^2 )"),
@@ -126,51 +144,51 @@ test_that("Units are rendered properly in HTML", {
   )
   expect_equal(
     generate_html_units("SO[_4^2-] **h** 3"),
-    "SO<span style=\"display:inline-block;line-height:1em;text-align:left;font-size:60%;vertical-align:-0.25em;margin-left:0.1em;\">2&minus;<br>4</span> **h** 3"
+    "SO<span style=\"display:inline-block;line-height:1em;text-align:left;font-size:60%;vertical-align:-0.25em;margin-left:0.1em;\">2&minus;<br>4</span> <strong>h</strong> 3"
   )
   expect_equal(
     generate_html_units("-2a"),
-    "&minus;2a"
+    "−2a"
   )
   expect_equal(
     generate_html_units("um / s"),
-    "&micro;m/s"
+    "µm/s"
   )
   expect_equal(
     generate_html_units("um^2 /s"),
-    "&micro;m<span style=\"white-space:nowrap;\"><sup>2</sup></span> s<span style=\"white-space:nowrap;\"><sup>&minus;1</sup></span>"
+    "µm<span style=\"white-space:nowrap;\"><sup>2</sup></span> s<span style=\"white-space:nowrap;\"><sup>&minus;1</sup></span>"
   )
   expect_equal(
     generate_html_units(":micro:N"),
-    "&micro;N"
+    "µN"
   )
   expect_equal(
     generate_html_units(":mu:J /min"),
-    "&micro;J min<span style=\"white-space:nowrap;\"><sup>&minus;1</sup></span>"
+    "µJ min<span style=\"white-space:nowrap;\"><sup>&minus;1</sup></span>"
   )
   expect_equal(
     generate_html_units(":micro:g m^−3 NO_*x*"),
-    "&micro;g m<span style=\"white-space:nowrap;\"><sup>−3</sup></span> NO<span style=\"white-space:nowrap;\"><sub>*x*</sub></span>"
+    "µg m<span style=\"white-space:nowrap;\"><sup>−3</sup></span> NO<span style=\"white-space:nowrap;\"><sub><em>x</em></sub></span>"
   )
   expect_equal(
     generate_html_units(":micro:gC /g /h"),
-    "&micro;gC g<span style=\"white-space:nowrap;\"><sup>&minus;1</sup></span> h<span style=\"white-space:nowrap;\"><sup>&minus;1</sup></span>"
+    "µgC g<span style=\"white-space:nowrap;\"><sup>&minus;1</sup></span> h<span style=\"white-space:nowrap;\"><sup>&minus;1</sup></span>"
   )
   expect_equal(
     generate_html_units("ug C /g /h"),
-    "&micro;g C g<span style=\"white-space:nowrap;\"><sup>&minus;1</sup></span> h<span style=\"white-space:nowrap;\"><sup>&minus;1</sup></span>"
+    "µg C g<span style=\"white-space:nowrap;\"><sup>&minus;1</sup></span> h<span style=\"white-space:nowrap;\"><sup>&minus;1</sup></span>"
   )
   expect_equal(
     generate_html_units("ohm_i^2"),
-    "&#8486;<span style=\"white-space:nowrap;\"><sub>i</sub></span><span style=\"white-space:nowrap;\"><sup>2</sup></span>"
+    "Ω<span style=\"white-space:nowrap;\"><sub>i</sub></span><span style=\"white-space:nowrap;\"><sup>2</sup></span>"
   )
   expect_equal(
     generate_html_units(":ohm:_i^2"),
-    "&#8486;<span style=\"white-space:nowrap;\"><sub>i</sub></span><span style=\"white-space:nowrap;\"><sup>2</sup></span>"
+    "Ω<span style=\"white-space:nowrap;\"><sub>i</sub></span><span style=\"white-space:nowrap;\"><sup>2</sup></span>"
   )
   expect_equal(
     generate_html_units("m:ohm:_i^2"),
-    "m&#8486;<span style=\"white-space:nowrap;\"><sub>i</sub></span><span style=\"white-space:nowrap;\"><sup>2</sup></span>"
+    "mΩ<span style=\"white-space:nowrap;\"><sub>i</sub></span><span style=\"white-space:nowrap;\"><sup>2</sup></span>"
   )
   expect_equal(
     generate_html_units("( GeV^2 / ( c^2 + 6) )^-2"),
@@ -178,11 +196,11 @@ test_that("Units are rendered properly in HTML", {
   )
   expect_equal(
     generate_html_units("J / degC"),
-    "J/&deg;C"
+    "J/°C"
   )
   expect_equal(
     generate_html_units("1 / degF"),
-    "1/&deg;F"
+    "1/°F"
   )
   expect_equal(
     generate_html_units("m / s"),
@@ -190,31 +208,31 @@ test_that("Units are rendered properly in HTML", {
   )
   expect_equal(
     generate_html_units("25:degree:C"),
-    "25&deg;C"
+    "25°C"
   )
   expect_equal(
     generate_html_units(":degree:N"),
-    "&deg;N"
+    "°N"
   )
   expect_equal(
     generate_html_units(":angstrom: / s"),
-    "&#8491;/s"
+    "Å/s"
   )
   expect_equal(
     generate_html_units(":permil:"),
-    "&permil;"
+    "‰"
   )
   expect_equal(
     generate_html_units(":permille:"),
-    "&permil;"
+    "‰"
   )
   expect_equal(
     generate_html_units("*:permil:*"),
-    "*&permil;*"
+    "<em>‰</em>"
   )
   expect_equal(
     generate_html_units("3:times:3"),
-    "3&times;3"
+    "3×3"
   )
   expect_equal(
     generate_html_units("1:5"),
@@ -230,11 +248,11 @@ test_that("Units are rendered properly in HTML", {
   )
   expect_equal(
     generate_html_units("umol %C25H52% / uL"),
-    "&micro;mol C<span style=\"white-space:nowrap;\"><sub>25</sub></span>H<span style=\"white-space:nowrap;\"><sub>52</sub></span> / &micro;L"
+    "µmol C<span style=\"white-space:nowrap;\"><sub>25</sub></span>H<span style=\"white-space:nowrap;\"><sub>52</sub></span> / µL"
   )
   expect_equal(
     generate_html_units(":mu:mol %C25H52% / :micro:L"),
-    "&micro;mol C<span style=\"white-space:nowrap;\"><sub>25</sub></span>H<span style=\"white-space:nowrap;\"><sub>52</sub></span> / &micro;L"
+    "µmol C<span style=\"white-space:nowrap;\"><sub>25</sub></span>H<span style=\"white-space:nowrap;\"><sub>52</sub></span> / µL"
   )
   expect_equal(
     generate_html_units("mol %Ca3(PO4)2%"),
