ed7723723c5888bc41613c66614cac09455e88c7
Richard Iannone <riannone@me.com>
2023-07-17 16:56:16 -0400

    Update dt_data.R

--- R/dt_data.R
+++ R/dt_data.R
@@ -171,8 +171,8 @@ dt_data_add_rows <- function(
     if (is.null(stub_df_add[i, ][["group_label"]][[1]])) {
 
       if (
-        stub_df_add[i, ][["group_id"]] %in%
-        stub_df_group_lookup[["group_id"]]
+        length(stub_df_group_lookup[["group_id"]]) > 0 &&
+        stub_df_add[i, ][["group_id"]] %in% stub_df_group_lookup[["group_id"]]
       ) {
 
         # This case is where a `group_id` in the added rows already exists
@@ -185,9 +185,12 @@ dt_data_add_rows <- function(
 
       } else {
 
-        # Migrate the text from a new group ID to a group label
-        stub_df_add[i, ][["group_label"]] <-
-          as.list(stub_df_add[i, ][["group_id"]])
+        if (!is.na(stub_df_add[i, ][["group_id"]])) {
+
+          # Migrate the text from a new group ID to a group label
+          stub_df_add[i, ][["group_label"]] <-
+            as.list(stub_df_add[i, ][["group_id"]])
+        }
       }
     }
   }
