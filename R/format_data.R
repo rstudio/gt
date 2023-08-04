2103d6611047e500796694dc7bd6cd42d0622568
Richard Iannone <riannone@me.com>
2023-08-04 12:24:05 -0400

    Update `fmt_datetime()`

--- R/format_data.R
+++ R/format_data.R
@@ -6371,6 +6371,70 @@ fmt_datetime <- function(
   # Perform input object validation
   stop_if_not_gt_tbl(data = data)
 
+  #
+  # Begin support for `from_column()` objects passed to compatible arguments
+  #
+
+  # Supports parameters:
+  #
+  # - date_style
+  # - time_style
+  # - sep
+  # - format
+  # - tz
+  # - pattern
+  # - locale
+
+  arg_vals <-
+    mget(
+      get_arg_names(
+        function_name = get(as.character(match.call()[[1]])),
+        all_args_except = c("data", "columns", "rows")
+      )
+    )
+
+  if (args_have_gt_column_obj(arg_vals = arg_vals)) {
+
+    # Resolve the row numbers using the `resolve_vars` function
+    resolved_rows_idx <-
+      resolve_rows_i(
+        expr = {{ rows }},
+        data = data
+      )
+
+    param_tbl <-
+      generate_param_tbl(
+        data = data,
+        arg_vals = arg_vals,
+        resolved_rows_idx = resolved_rows_idx
+      )
+
+    for (i in seq_len(nrow(param_tbl))) {
+
+      p_i <- as.list(param_tbl[i, ])
+
+      data <-
+        fmt_datetime(
+          data = data,
+          columns = {{ columns }},
+          rows = resolved_rows_idx[i],
+          date_style = p_i$date_style %||% date_style,
+          time_style = p_i$time_style %||% time_style,
+          sep = p_i$sep %||% sep,
+          format = p_i$format %||% format,
+          tz = p_i$tz %||% tz,
+          pattern = p_i$pattern %||% pattern,
+          locale = p_i$locale %||% locale
+        )
+    }
+
+    return(data)
+  }
+
+  #
+  # End support for `gt_column()` objects passed to compatible arguments
+  #
+
   # Declare formatting function compatibility
   compat <- c("Date", "POSIXct", "character")
 
