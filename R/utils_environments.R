6e4eea1f7a6ee73f6752dd7262260e325b505dbd
Richard Iannone <riannone@me.com>
2023-08-31 20:56:58 -0400

    Create utils_environments.R

---
+++ R/utils_environments.R
@@ -0,0 +1,31 @@
+#------------------------------------------------------------------------------#
+#
+#                /$$
+#               | $$
+#     /$$$$$$  /$$$$$$
+#    /$$__  $$|_  $$_/
+#   | $$  \ $$  | $$
+#   | $$  | $$  | $$ /$$
+#   |  $$$$$$$  |  $$$$/
+#    \____  $$   \___/
+#    /$$  \ $$
+#   |  $$$$$$/
+#    \______/
+#
+#  This file is part of the 'rstudio/gt' project.
+#
+#  Copyright (c) 2018-2023 gt authors
+#
+#  For full copyright and license information, please look at
+#  https://gt.rstudio.com/LICENSE.html
+#
+#------------------------------------------------------------------------------#
+
+
+check_quarto <- function() {
+  Sys.getenv("QUARTO_BIN_PATH") != ""
+}
+
+check_dbnb <- function() {
+  Sys.getenv("DATABRICKS_RUNTIME_VERSION") != ""
+}
