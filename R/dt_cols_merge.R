85e185f39a5e522d03f32b0aecc213722d8f7cd1
Richard Iannone <riannone@me.com>
2023-09-25 14:28:56 -0400

    Update dt_cols_merge.R

--- R/dt_cols_merge.R
+++ R/dt_cols_merge.R
@@ -44,7 +44,10 @@ dt_col_merge_add <- function(data, col_merge) {
 dt_col_merge_entry <- function(vars, rows, type, pattern = NULL, ...) {
 
   # Assert type is one of the supported values
-  arg_match0(type, values = c("merge", "merge_range", "merge_uncert", "merge_n_pct"))
+  rlang::arg_match0(
+    type,
+    values = c("merge", "merge_range", "merge_uncert", "merge_n_pct")
+  )
 
   list(
     vars = vars,
