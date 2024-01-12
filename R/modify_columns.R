bee3fc1d5fe992e0bcadcfaa4cd1c4633f977d08
Richard Iannone <riannone@me.com>
2024-01-12 15:13:23 -0500

    Add `autohide` arg to `cols_nanoplot()`

--- R/modify_columns.R
+++ R/modify_columns.R
@@ -2733,6 +2733,7 @@ cols_nanoplot <- function(
     plot_height = "2em",
     missing_vals = c("gap", "zero", "remove"),
     autoscale = FALSE,
+    autohide = TRUE,
     columns_x_vals = NULL,
     reference_line = NULL,
     reference_area = NULL,
