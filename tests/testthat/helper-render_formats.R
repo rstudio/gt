58ba0103e2380c2ac73bd36ab786001c02c257c2
olivroy <52606734+olivroy@users.noreply.github.com>
2024-04-25 13:55:22 -0400

    oops

--- tests/testthat/helper-render_formats.R
+++ tests/testthat/helper-render_formats.R
@@ -2,7 +2,7 @@
 # FIXME works with warning on Windows
 render_formats_test <- function(data,
                                 context) {
-  if (.Platform$OS.type == "windows")) {
+  if (.Platform$OS.type == "windows") {
     # file not-existing related warnings
     # only delete on Windows
     suppressWarnings(build_data(data, context = context)$`_body`)
