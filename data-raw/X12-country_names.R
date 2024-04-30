80b40814d5d554ddfca5266838672a99b4763dce
Richard Iannone <riannone@me.com>
2024-04-29 22:54:47 -0400

    Update X12-country_names.R

--- data-raw/X12-country_names.R
+++ data-raw/X12-country_names.R
@@ -3,5 +3,6 @@ library(tidyverse)
 country_names <-
   readr::read_csv(
     file = "data-raw/country_names.csv",
-    col_types = cols(.default = col_character())
+    col_types = cols(.default = col_character()),
+    na = ""
   )
