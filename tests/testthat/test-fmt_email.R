22b5e5a172fa43fd58cabd7d37d2942ddf76cb09
Richard Iannone <riannone@me.com>
2024-05-02 17:01:26 -0400

    Update test-fmt_email.R

--- tests/testthat/test-fmt_email.R
+++ tests/testthat/test-fmt_email.R
@@ -37,7 +37,7 @@ test_that("The `fmt_email()` function works correctly", {
   )
   expect_equal(
     (tab %>%
-       fmt_email(columns = a, label = "static label") %>%
+       fmt_email(columns = a, display_name = "static label") %>%
        render_formats_test(context = "html"))[["a"]],
     c(
       "<span style=\"white-space: pre;\"><a href=\"mailto:example@example.com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#008B8B;text-decoration:underline;text-underline-position: under;display: inline-block;\">static label</a></span>",
@@ -172,6 +172,80 @@ test_that("The `fmt_email()` function works correctly", {
     c(
       "<span style=\"white-space: pre;\"><a href=\"mailto:example@example.com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#000000;text-decoration:none;display: inline-block;background-color: #FAD5EF;padding: 8px 12px;outline-style: none; outline-color: #DFDFDF; outline-width: 2px;\">example@example.com</a></span>",
       "<span style=\"white-space: pre;\"><a href=\"mailto:example_1@example.com,example_2@example_com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#000000;text-decoration:none;display: inline-block;background-color: #FAD5EF;padding: 8px 12px;outline-style: none; outline-color: #DFDFDF; outline-width: 2px;\">example_1@example.com,example_2@example_com</a></span>",
+      NA
+    )
+  )
+
+
+  # Create an alternate data frame with email addresses and labels
+  data_tbl <-
+    data.frame(
+      a = c(
+        "example@example.com",
+        "example_1@example.com,example_2@example_com",
+        "example_3_solo@example.com example_4_solo@example_com",
+        NA
+      ),
+      label = c(
+        "Example 1",
+        "Example Group",
+        "<Three-Solo> <Four-Solo>",
+        "missing"
+      ),
+      stringsAsFactors = FALSE
+    )
+
+  # Create a `gt_tbl` object with `gt()` and the `data_tbl` dataset
+  tab <- gt(data = data_tbl)
+
+  # Format the `a` column to have 'mailto:' emails
+  expect_equal(
+    (tab %>%
+       fmt_email(columns = a) %>%
+       render_formats_test(context = "html"))[["a"]],
+    c(
+      "<span style=\"white-space: pre;\"><a href=\"mailto:example@example.com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#008B8B;text-decoration:underline;text-underline-position: under;display: inline-block;\">example@example.com</a></span>",
+      "<span style=\"white-space: pre;\"><a href=\"mailto:example_1@example.com,example_2@example_com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#008B8B;text-decoration:underline;text-underline-position: under;display: inline-block;\">example_1@example.com,example_2@example_com</a></span>",
+      "<span style=\"white-space: pre;\"><a href=\"mailto:example_3_solo@example.com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#008B8B;text-decoration:underline;text-underline-position: under;display: inline-block;\">example_3_solo@example.com</a></span> <span style=\"white-space: pre;\"><a href=\"mailto:example_4_solo@example_com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#008B8B;text-decoration:underline;text-underline-position: under;display: inline-block;\">example_4_solo@example_com</a></span>",
+      NA
+    )
+  )
+
+  # Use a static display name (text)
+  expect_equal(
+    (tab %>%
+       fmt_email(columns = a, display_name = "Email") %>%
+       render_formats_test(context = "html"))[["a"]],
+    c(
+      "<span style=\"white-space: pre;\"><a href=\"mailto:example@example.com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#008B8B;text-decoration:underline;text-underline-position: under;display: inline-block;\">Email</a></span>",
+      "<span style=\"white-space: pre;\"><a href=\"mailto:example_1@example.com,example_2@example_com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#008B8B;text-decoration:underline;text-underline-position: under;display: inline-block;\">Email</a></span>",
+      "<span style=\"white-space: pre;\"><a href=\"mailto:example_3_solo@example.com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#008B8B;text-decoration:underline;text-underline-position: under;display: inline-block;\">Email</a></span> <span style=\"white-space: pre;\"><a href=\"mailto:example_4_solo@example_com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#008B8B;text-decoration:underline;text-underline-position: under;display: inline-block;\">Email</a></span>",
+      NA
+    )
+  )
+
+  # Use a static display name (icon)
+  expect_equal(
+    (tab %>%
+       fmt_email(columns = a, display_name = fontawesome::fa("envelope"), color = "orange") %>%
+       render_formats_test(context = "html"))[["a"]],
+    c(
+      "<span style=\"white-space: pre;\"><a href=\"mailto:example@example.com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#FFA500;text-decoration:underline;text-underline-position: under;display: inline-block;\"><svg aria-hidden=\"true\" role=\"img\" viewBox=\"0 0 512 512\" style=\"height:1em;width:1em;vertical-align:-0.125em;margin-left:auto;margin-right:auto;font-size:inherit;fill:currentColor;overflow:visible;position:relative;\"><path d=\"M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z\"/></svg></a></span>",
+      "<span style=\"white-space: pre;\"><a href=\"mailto:example_1@example.com,example_2@example_com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#FFA500;text-decoration:underline;text-underline-position: under;display: inline-block;\"><svg aria-hidden=\"true\" role=\"img\" viewBox=\"0 0 512 512\" style=\"height:1em;width:1em;vertical-align:-0.125em;margin-left:auto;margin-right:auto;font-size:inherit;fill:currentColor;overflow:visible;position:relative;\"><path d=\"M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z\"/></svg></a></span>",
+      "<span style=\"white-space: pre;\"><a href=\"mailto:example_3_solo@example.com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#FFA500;text-decoration:underline;text-underline-position: under;display: inline-block;\"><svg aria-hidden=\"true\" role=\"img\" viewBox=\"0 0 512 512\" style=\"height:1em;width:1em;vertical-align:-0.125em;margin-left:auto;margin-right:auto;font-size:inherit;fill:currentColor;overflow:visible;position:relative;\"><path d=\"M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z\"/></svg></a></span> <span style=\"white-space: pre;\"><a href=\"mailto:example_4_solo@example_com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#FFA500;text-decoration:underline;text-underline-position: under;display: inline-block;\"><svg aria-hidden=\"true\" role=\"img\" viewBox=\"0 0 512 512\" style=\"height:1em;width:1em;vertical-align:-0.125em;margin-left:auto;margin-right:auto;font-size:inherit;fill:currentColor;overflow:visible;position:relative;\"><path d=\"M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z\"/></svg></a></span>",
+      NA
+    )
+  )
+
+  # Use display names from an adjacent column
+  expect_equal(
+    (tab %>%
+       fmt_email(columns = a, display_name = from_column(column = "label")) %>%
+       render_formats_test(context = "html"))[["a"]],
+    c(
+      "<span style=\"white-space: pre;\"><a href=\"mailto:example@example.com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#008B8B;text-decoration:underline;text-underline-position: under;display: inline-block;\">Example 1</a></span>",
+      "<span style=\"white-space: pre;\"><a href=\"mailto:example_1@example.com,example_2@example_com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#008B8B;text-decoration:underline;text-underline-position: under;display: inline-block;\">Example Group</a></span>",
+      "<span style=\"white-space: pre;\"><a href=\"mailto:example_3_solo@example.com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#008B8B;text-decoration:underline;text-underline-position: under;display: inline-block;\">Three-Solo</a></span> <span style=\"white-space: pre;\"><a href=\"mailto:example_4_solo@example_com\" target=\"_blank\" rel=\"noopener noreferrer\" style=\"color:#008B8B;text-decoration:underline;text-underline-position: under;display: inline-block;\">Four-Solo</a></span>",
       NA
     )
   )
