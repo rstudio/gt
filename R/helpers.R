b680d285244b55e46b715cea379d65c6580d5970
olivroy <olivierroy71@hotmail.com>
2024-05-12 08:37:11 -0400

    Move `locations` helper to own topic (example)

--- R/helpers.R
+++ R/helpers.R
@@ -1326,46 +1326,6 @@ cells_stubhead <- function() {
 #' @return A list object with the classes `cells_column_spanners` and
 #' `location_cells`.
 #'
-#' @section Overview of location helper functions:
-#'
-#' Location helper functions can be used to target cells with virtually any
-#' function that has a `locations` argument. Here is a listing of all of the
-#' location helper functions, with locations corresponding roughly from top to
-#' bottom of a table:
-#'
-#' - [cells_title()]: targets the table title or the table subtitle depending on
-#' the value given to the `groups` argument (`"title"` or `"subtitle"`).
-#' - [cells_stubhead()]: targets the stubhead location, a cell of which is only
-#' available when there is a stub; a label in that location can be created by
-#' using the [tab_stubhead()] function.
-#' - [cells_column_spanners()]: targets the spanner column labels with the
-#' `spanners` argument; spanner column labels appear above the column labels.
-#' - [cells_column_labels()]: targets the column labels with its `columns`
-#' argument.
-#' - [cells_row_groups()]: targets the row group labels in any available row
-#' groups using the `groups` argument.
-#' - [cells_stub()]: targets row labels in the table stub using the `rows`
-#' argument.
-#' - [cells_body()]: targets data cells in the table body using intersections of
-#' `columns` and `rows`.
-#' - [cells_summary()]: targets summary cells in the table body using the
-#' `groups` argument and intersections of `columns` and `rows`.
-#' - [cells_grand_summary()]: targets cells of the table's grand summary using
-#' intersections of `columns` and `rows`
-#' - [cells_stub_summary()]: targets summary row labels in the table stub using
-#' the `groups` and `rows` arguments.
-#' - [cells_stub_grand_summary()]: targets grand summary row labels in the table
-#' stub using the `rows` argument.
-#' - [cells_footnotes()]: targets all footnotes in the table footer (cannot be
-#' used with [tab_footnote()]).
-#' - [cells_source_notes()]: targets all source notes in the table footer
-#' (cannot be used with [tab_footnote()]).
-#'
-#' When using any of the location helper functions with an appropriate function
-#' that has a `locations` argument (e.g., [tab_style()]), multiple locations
-#' can be targeted by enclosing several `cells_*()` helper functions in a
-#' `list()` (e.g., `list(cells_body(), cells_grand_summary())`).
-#'
 #' @section Examples:
 #'
 #' Use the [`exibble`] dataset to create a **gt** table. We'll add a spanner
