125bcb8271ced5c44b7b515dc1bf933d84d979c5
Richard Iannone <riannone@me.com>
2024-05-16 13:52:32 -0400

    Update 14-nuclides.R

--- data-raw/14-nuclides.R
+++ data-raw/14-nuclides.R
@@ -7,9 +7,32 @@ nuclides <-
       cols(
         z = col_integer(),
         n = col_integer(),
-        name = col_character(),
-        halflife = col_double(),
-        mode = col_character()
+        symbol = col_character(),
+        radius = col_double(),
+        radius_uncert = col_double(),
+        abundance = col_double(),
+        abundance_uncert = col_double(),
+        is_stable = col_logical(),
+        half_life_sec = col_double(),
+        half_life_uncert = col_double(),
+        isospin = col_character(),
+        decay_1 = col_character(),
+        decay_1_pct = col_double(),
+        decay_1_pct_uncert = col_double(),
+        decay_2 = col_character(),
+        decay_2_pct = col_double(),
+        decay_2_pct_uncert = col_double(),
+        decay_3 = col_character(),
+        decay_3_pct = col_double(),
+        decay_3_pct_uncert = col_double(),
+        magnetic_dipole = col_double(),
+        magnetic_dipole_uncert = col_double(),
+        electric_quadrupole = col_double(),
+        electric_quadrupole_uncert = col_double(),
+        atomic_mass = col_double(),
+        atomic_mass_uncert = col_double(),
+        mass_excess = col_double(),
+        mass_excess_uncert = col_double()
       )
   )
 
