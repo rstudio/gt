349f8564aeb335813ff0121de4251fe1e8926f25
Richard Iannone <riannone@me.com>
2024-05-24 13:04:18 -0400

    Fix documentation issue in `fmt_chem()`

--- R/format_data.R
+++ R/format_data.R
@@ -9433,12 +9433,12 @@ fmt_units <- function(
 #' `r man_get_image_tag(file = "man_fmt_chem_1.png")`
 #' }}
 #'
-#' Taking just a few rows from the [`photolysis`] dataset, let's and create a
-#' new **gt** table. The `cmpd_formula` and `products` columns both contain
-#' text in chemistry notation (the first has compounds, and the second column
-#' has the products of photolysis reactions). These columns will be formatted by
-#' the `fmt_chem()` function. The compound formulas will be merged with the
-#' compound names via the [cols_merge()] function.
+#' Taking just a few rows from the [`photolysis`] dataset, let's create a new
+#' **gt** table. The `cmpd_formula` and `products` columns both contain text in
+#' chemistry notation (the first has compounds, and the second column has the
+#' products of photolysis reactions). These columns will be formatted by the
+#' `fmt_chem()` function. The compound formulas will be merged with the compound
+#' names via the [cols_merge()] function.
 #'
 #' ```r
 #' photolysis %>%
