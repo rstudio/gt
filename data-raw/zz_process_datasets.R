dc9d2e4de7112bf3a73a7c4422ff1f5b4dc90dba
Richard Iannone <riannone@me.com>
2024-05-24 23:18:02 -0400

    Update zz_process_datasets.R

--- data-raw/zz_process_datasets.R
+++ data-raw/zz_process_datasets.R
@@ -9,18 +9,19 @@ source("data-raw/06-exibble.R")
 source("data-raw/07-towny.R")
 source("data-raw/08-peeps.R")
 source("data-raw/09-metro.R")
-source("data-raw/10-constants.R")
-source("data-raw/11-illness.R")
-source("data-raw/12-reactions.R")
-source("data-raw/13-photolysis.R")
-source("data-raw/14-nuclides.R")
-source("data-raw/15-gt_clinical.R")
+source("data-raw/10-gibraltar.R")
+source("data-raw/11-constants.R")
+source("data-raw/12-illness.R")
+source("data-raw/13-reactions.R")
+source("data-raw/14-photolysis.R")
+source("data-raw/15-nuclides.R")
+source("data-raw/16-gt_clinical.R")
 
 # Create external datasets
 usethis::use_data(
   countrypops, sza, gtcars, sp500,
   pizzaplace, exibble, towny, peeps, metro,
-  constants, illness, reactions, photolysis,
-  nuclides, rx_adsl, rx_addv,
+  gibraltar, constants, illness, reactions,
+  photolysis, nuclides, rx_adsl, rx_addv,
   internal = FALSE, overwrite = TRUE
 )
