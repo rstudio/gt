3bb436ec0162bf05bdb18cf2b5af4d6ab4ab55fc
Richard Iannone <riannone@me.com>
2024-06-05 17:04:11 -0400

    Create 09-films.R

---
+++ data-raw/09-films.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+films <-
+  readr::read_csv(
+    file = "data-raw/films.csv",
+    col_types =
+      cols(
+        year = col_integer(),
+        title = col_character(),
+        original_title = col_character(),
+        director = col_character(),
+        languages = col_character(),
+        countries_of_origin = col_character(),
+        run_time = col_character(),
+        imdb_url = col_character()
+      )
+  )
