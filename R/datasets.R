6a4979bd53930b8a01cdec5da13977eeaa797752
Richard Iannone <riannone@me.com>
2024-09-03 18:16:14 -0400

    Update datasets.R

--- R/datasets.R
+++ R/datasets.R
@@ -22,7 +22,7 @@
 #------------------------------------------------------------------------------#
 
 
-#' Yearly populations of countries from 1960 to 2022
+#' Yearly populations of countries from 1960 to 2023
 #'
 #' @description
 #'
@@ -30,10 +30,11 @@
 #' population is based on counts of all residents regardless of legal status or
 #' citizenship. Country identifiers include the English-language country names,
 #' and the 2- and 3-letter ISO 3166-1 country codes. Each row contains a
-#' `population` value for a given `year` (from 1960 to 2022). Any `NA` values
-#' for `populations` indicate the non-existence of the entity during that year.
+#' `population` value for a given `year` (from 1960 to 2023). Any `NA` values
+#' within `population` indicate the non-existence of the entity during that
+#' year.
 #'
-#' @format A tibble with 13,545 rows and 5 variables:
+#' @format A tibble with 13,760 rows and 5 variables:
 #' \describe{
 #' \item{country_name}{The name of the country.}
 #' \item{country_code_2, country_code_3}{The 2- and 3-letter ISO 3166-1 country
