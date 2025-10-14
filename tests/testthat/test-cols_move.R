40d77f852a3ff5e28a7d60664bd8489a2f3ff04b
Richard Iannone <riannone@me.com>
2025-10-14 15:55:21 -0400

    Update test-cols_move.R

--- tests/testthat/test-cols_move.R
+++ tests/testthat/test-cols_move.R
@@ -11,108 +11,108 @@ test_that("cols_move() works correctly", {
   # Create a `tbl_html` object with `gt()`; the `mpg`,
   # `cyl`, and `drat` columns placed after `drat`
   tbl_html <-
-    gt(mtcars_short) %>%
+    gt(mtcars_short) |>
     cols_move(columns = c(mpg, cyl, disp), after = drat)
 
   # Expect a particular ordering of columns in `_boxh`
-  tbl_html %>%
-    dt_boxhead_get_vars() %>%
-    expect_equal(
-      c(
-        "hp", "drat", "mpg", "cyl", "disp", "wt",
-        "qsec", "vs", "am", "gear", "carb"
-      )
+  expect_equal(
+    tbl_html |>
+      dt_boxhead_get_vars(),
+    c(
+      "hp", "drat", "mpg", "cyl", "disp", "wt",
+      "qsec", "vs", "am", "gear", "carb"
     )
+  )
 
   # Expect that the columns have the same ordering in
   # the rendered table
-  tbl_html %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") %>%
-    rvest::html_text() %>%
-    expect_equal(
-      c(
-        "hp", "drat", "mpg", "cyl", "disp", "wt",
-        "qsec", "vs", "am", "gear", "carb"
-      )
+  expect_equal(
+    tbl_html |>
+      render_as_html() |>
+      xml2::read_html() |>
+      rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") |>
+      rvest::html_text(),
+    c(
+      "hp", "drat", "mpg", "cyl", "disp", "wt",
+      "qsec", "vs", "am", "gear", "carb"
     )
+  )
 
   # Create a `tbl_html` object with `gt()`; the `mpg`,
   # `cyl`, and `drat` columns placed after `drat` using vectors
   tbl_html <-
-    gt(mtcars_short) %>%
+    gt(mtcars_short) |>
     cols_move(columns = c("mpg", "cyl", "disp"), after = c("drat"))
 
   # Expect a particular ordering of columns in `_boxh`
-  tbl_html %>%
-    dt_boxhead_get_vars() %>%
-    expect_equal(
-      c(
-        "hp", "drat", "mpg", "cyl", "disp", "wt",
-        "qsec", "vs", "am", "gear", "carb"
-      )
+  expect_equal(
+    tbl_html |>
+      dt_boxhead_get_vars(),
+    c(
+      "hp", "drat", "mpg", "cyl", "disp", "wt",
+      "qsec", "vs", "am", "gear", "carb"
     )
+  )
 
   # Expect that the columns have the same ordering in
   # the rendered table
-  tbl_html %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") %>%
-    rvest::html_text() %>%
-    expect_equal(
-      c(
-        "hp", "drat", "mpg", "cyl", "disp", "wt",
-        "qsec", "vs", "am", "gear", "carb"
-      )
+  expect_equal(
+    tbl_html |>
+      render_as_html() |>
+      xml2::read_html() |>
+      rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") |>
+      rvest::html_text(),
+    c(
+      "hp", "drat", "mpg", "cyl", "disp", "wt",
+      "qsec", "vs", "am", "gear", "carb"
     )
+  )
 
   # Create a `tbl_html` object with `gt()`; the `mpg`,
   # `cyl`, and `drat` columns placed after `carb` (the end of the series)
   tbl_html <-
-    gt(mtcars_short) %>%
+    gt(mtcars_short) |>
     cols_move(columns = c(mpg, cyl, disp), after = carb)
 
   # Expect a particular ordering of columns in the internal `boxh_df`
-  tbl_html %>%
-    dt_boxhead_get_vars() %>%
-    expect_equal(
-      c(
-        "hp", "drat", "wt", "qsec", "vs", "am",
-        "gear", "carb", "mpg", "cyl", "disp"
-      )
+  expect_equal(
+    tbl_html |>
+      dt_boxhead_get_vars(),
+    c(
+      "hp", "drat", "wt", "qsec", "vs", "am",
+      "gear", "carb", "mpg", "cyl", "disp"
     )
+  )
 
   # Expect that the columns have the same ordering in
   # the rendered table
-  tbl_html %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") %>%
-    rvest::html_text() %>%
-    expect_equal(
-      c(
-        "hp", "drat", "wt", "qsec", "vs", "am",
-        "gear", "carb", "mpg", "cyl", "disp"
-      )
+  expect_equal(
+    tbl_html |>
+      render_as_html() |>
+      xml2::read_html() |>
+      rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") |>
+      rvest::html_text(),
+    c(
+      "hp", "drat", "wt", "qsec", "vs", "am",
+      "gear", "carb", "mpg", "cyl", "disp"
     )
+  )
 
   # Expect an error if more than one column provided in `after`
   expect_error(
-    gt(mtcars_short) %>%
+    gt(mtcars_short) |>
       cols_move(columns = c(mpg, cyl, disp), after = c(am, wt))
   )
 
   # Expect an error if the column provided in `after` doesn't exist
   expect_error(
-    gt(mtcars_short) %>%
+    gt(mtcars_short) |>
       cols_move(columns = c(mpg, cyl, disp), after = wts)
   )
 
   # Expect an error if any of the `columns` doesn't exist in `data_df`
   expect_error(
-    gt(mtcars_short) %>%
+    gt(mtcars_short) |>
       cols_move(columns = c(mpg, cyls, disp), after = wt)
   )
 })
@@ -125,66 +125,66 @@ test_that("cols_move_to_start() works correctly", {
   # Create a `tbl_html` object with `gt()`; the `gear`,
   # and `carb` columns placed at the start
   tbl_html <-
-    gt(mtcars_short) %>%
+    gt(mtcars_short) |>
     cols_move_to_start(columns = c(gear, carb))
 
   # Expect a particular ordering of columns in the internal `boxh_df`
-  tbl_html %>%
-    dt_boxhead_get_vars() %>%
-    expect_equal(
-      c(
-        "gear", "carb", "mpg", "cyl", "disp", "hp",
-        "drat", "wt", "qsec", "vs", "am"
-      )
+  expect_equal(
+    tbl_html |>
+      dt_boxhead_get_vars(),
+    c(
+      "gear", "carb", "mpg", "cyl", "disp", "hp",
+      "drat", "wt", "qsec", "vs", "am"
     )
+  )
 
   # Expect that the columns have the same ordering in
   # the rendered table
-  tbl_html %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") %>%
-    rvest::html_text() %>%
-    expect_equal(
-      c(
-        "gear", "carb", "mpg", "cyl", "disp", "hp",
-        "drat", "wt", "qsec", "vs", "am"
-      )
+  expect_equal(
+    tbl_html |>
+      render_as_html() |>
+      xml2::read_html() |>
+      rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") |>
+      rvest::html_text(),
+    c(
+      "gear", "carb", "mpg", "cyl", "disp", "hp",
+      "drat", "wt", "qsec", "vs", "am"
     )
+  )
 
   # Create a `tbl_html` object with `gt()`; the `gear`,
   # and `carb` columns placed at the start using vectors
   tbl_html <-
-    gt(mtcars_short) %>%
+    gt(mtcars_short) |>
     cols_move_to_start(columns = c("gear", "carb"))
 
   # Expect a particular ordering of columns in the internal `boxh_df`
-  tbl_html %>%
-    dt_boxhead_get_vars() %>%
-    expect_equal(
-      c(
-        "gear", "carb", "mpg", "cyl", "disp", "hp",
-        "drat", "wt", "qsec", "vs", "am"
-      )
+  expect_equal(
+    tbl_html |>
+      dt_boxhead_get_vars(),
+    c(
+      "gear", "carb", "mpg", "cyl", "disp", "hp",
+      "drat", "wt", "qsec", "vs", "am"
     )
+  )
 
   # Expect that the columns have the same ordering in
   # the rendered table
-  tbl_html %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") %>%
-    rvest::html_text() %>%
-    expect_equal(
-      c(
-        "gear", "carb", "mpg", "cyl", "disp", "hp",
-        "drat", "wt", "qsec", "vs", "am"
-      )
+  expect_equal(
+    tbl_html |>
+      render_as_html() |>
+      xml2::read_html() |>
+      rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") |>
+      rvest::html_text(),
+    c(
+      "gear", "carb", "mpg", "cyl", "disp", "hp",
+      "drat", "wt", "qsec", "vs", "am"
     )
+  )
 
   # Expect an error if any of the `columns` doesn't exist in `data_df`
   expect_error(
-    gt(mtcars_short) %>%
+    gt(mtcars_short) |>
       cols_move_to_start(columns = c(mpg, cyls, disp))
   )
 })
@@ -197,56 +197,56 @@ test_that("cols_move_to_end() works correctly", {
   # Create a `tbl_html` object with `gt()`; the `gear`,
   # and `carb` columns placed at the end
   tbl_html <-
-    gt(mtcars_short) %>%
+    gt(mtcars_short) |>
     cols_move_to_end(columns = c(gear, carb))
 
   # Expect a particular ordering of columns in the internal `boxh_df`
-  tbl_html %>%
-    dt_boxhead_get_vars() %>%
-    expect_equal(
-      c(
-        "mpg", "cyl", "disp", "hp", "drat", "wt",
-        "qsec", "vs", "am", "gear", "carb"
-      )
+  expect_equal(
+    tbl_html |>
+      dt_boxhead_get_vars(),
+    c(
+      "mpg", "cyl", "disp", "hp", "drat", "wt",
+      "qsec", "vs", "am", "gear", "carb"
     )
+  )
 
   # Expect that the columns have the same ordering in
   # the rendered table
-  tbl_html %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") %>%
-    rvest::html_text() %>%
-    expect_equal(
-      c(
-        "mpg", "cyl", "disp", "hp", "drat", "wt",
-        "qsec", "vs", "am", "gear", "carb"
-      )
+  expect_equal(
+    tbl_html |>
+      render_as_html() |>
+      xml2::read_html() |>
+      rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") |>
+      rvest::html_text(),
+    c(
+      "mpg", "cyl", "disp", "hp", "drat", "wt",
+      "qsec", "vs", "am", "gear", "carb"
     )
+  )
 
   # Create a `tbl_html` object with `gt()`; the `gear`,
   # and `carb` columns placed at the start using vectors
   tbl_html <-
-    gt(mtcars_short) %>%
+    gt(mtcars_short) |>
     cols_move_to_end(columns = c("gear", "carb"))
 
   # Expect a particular ordering of columns in the internal `boxh_df`
-  tbl_html %>%
-    dt_boxhead_get_vars() %>%
-    expect_equal(
-      c(
-        "mpg", "cyl", "disp", "hp", "drat", "wt",
-        "qsec", "vs", "am", "gear", "carb"
-      )
+  expect_equal(
+    tbl_html |>
+      dt_boxhead_get_vars(),
+    c(
+      "mpg", "cyl", "disp", "hp", "drat", "wt",
+      "qsec", "vs", "am", "gear", "carb"
     )
+  )
 
   # Expect that the columns have the same ordering in
   # the rendered table
-  tbl_html %>%
-    render_as_html() %>%
-    xml2::read_html() %>%
-    rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") %>%
-    rvest::html_text() %>%
+  tbl_html |>
+    render_as_html() |>
+    xml2::read_html() |>
+    rvest::html_nodes("[class='gt_col_heading gt_columns_bottom_border gt_right']") |>
+    rvest::html_text() |>
     expect_equal(
       c(
         "mpg", "cyl", "disp", "hp", "drat", "wt",
@@ -256,7 +256,7 @@ test_that("cols_move_to_end() works correctly", {
 
   # Expect an error if any of the `columns` doesn't exist in `data_df`
   expect_error(
-    gt(mtcars_short) %>%
+    gt(mtcars_short) |>
       cols_move_to_end(columns = c(mpg, cyls, disp))
   )
 })
@@ -265,55 +265,58 @@ test_that("check cols_move is applied gt_group", {
 
   # Create a `gt_group` object of two `gt_tbl`s
   # create gt group example
-  gt_tbl <- mtcars_short %>% gt()
+  gt_tbl <- gt(mtcars_short)
   gt_group <- gt_group(gt_tbl, gt_tbl)
 
   # apply move to table and group
-  move_gt_tbl <- gt_tbl %>%
+  move_gt_tbl <-
+    gt_tbl |>
     cols_move(columns = mpg, after = cyl)
 
-  move_gt_group <- gt_group %>%
+  move_gt_group <-
+    gt_group |>
     cols_move(columns = mpg, after = cyl)
 
   # Expect identical if function applied before or after group is constructed
   expect_identical(move_gt_group, gt_group(move_gt_tbl, move_gt_tbl))
-
 })
 
 test_that("check cols_move_to_start is applied gt_group", {
 
   # Create a `gt_group` object of two `gt_tbl`s
   # create gt group example
-  gt_tbl <- mtcars_short %>% gt()
+  gt_tbl <- gt(mtcars_short)
   gt_group <- gt_group(gt_tbl, gt_tbl)
 
   # apply move to table and group
-  move_gt_tbl <- gt_tbl %>%
+  move_gt_tbl <-
+    gt_tbl |>
     cols_move_to_start(columns = gear)
 
-  move_gt_group <- gt_group %>%
+  move_gt_group <-
+    gt_group |>
     cols_move_to_start(columns = gear)
 
   # Expect identical if function applied before or after group is constructed
   expect_identical(move_gt_group, gt_group(move_gt_tbl, move_gt_tbl))
-
 })
 
 test_that("check cols_move_to_end is applied gt_group", {
 
   # Create a `gt_group` object of two `gt_tbl`s
   # create gt group example
-  gt_tbl <- mtcars_short %>% gt()
+  gt_tbl <- gt(mtcars_short)
   gt_group <- gt_group(gt_tbl, gt_tbl)
 
   # apply move to table and group
-  move_gt_tbl <- gt_tbl %>%
+  move_gt_tbl <-
+    gt_tbl |>
     cols_move_to_end(columns = gear)
 
-  move_gt_group <- gt_group %>%
+  move_gt_group <-
+    gt_group |>
     cols_move_to_end(columns = gear)
 
   # Expect identical if function applied before or after group is constructed
   expect_identical(move_gt_group, gt_group(move_gt_tbl, move_gt_tbl))
-
 })
