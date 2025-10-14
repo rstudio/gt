fee9b0c78aaef95314e19caf9685dcefc16ceb95
Richard Iannone <riannone@me.com>
2025-10-14 17:12:10 -0400

    Update test-fmt_auto.R

--- tests/testthat/test-fmt_auto.R
+++ tests/testthat/test-fmt_auto.R
@@ -17,16 +17,16 @@ test_that("fmt_auto() works correctly", {
     )
 
   sp500_tbl <-
-    sp500 %>%
-    dplyr::filter(date >= "2015-01-05", date <= "2015-01-16") %>%
-    dplyr::arrange(date) %>%
+    sp500 |>
+    dplyr::filter(date >= "2015-01-05", date <= "2015-01-16") |>
+    dplyr::arrange(date) |>
     dplyr::mutate(week = paste0("W", strftime(date, format = "%V")))
 
   tbl_v_large <- dplyr::tibble(num = 10^(1:18))
 
   # Use `fmt_auto()` on a table having a variety of column types
   gt_tbl_1 <-
-    gt(tbl) %>%
+    gt(tbl) |>
     fmt_auto()
 
   # Take a snapshot of `gt_tbl_1`
@@ -35,7 +35,7 @@ test_that("fmt_auto() works correctly", {
   # Use `fmt_auto()` on a table having a variety of column types and
   # prefer to use large-number suffixing
   gt_tbl_2 <-
-    gt(tbl) %>%
+    gt(tbl) |>
     fmt_auto(lg_num_pref = "suf")
 
   # Take a snapshot of `gt_tbl_2`
@@ -43,7 +43,7 @@ test_that("fmt_auto() works correctly", {
 
   # Use `fmt_auto()` on a table having very small and very large values
   gt_tbl_3 <-
-    gt(tbl_big_small) %>%
+    gt(tbl_big_small) |>
     fmt_auto()
 
   # Take a snapshot of `gt_tbl_3`
@@ -52,7 +52,7 @@ test_that("fmt_auto() works correctly", {
   # Use `fmt_auto()` on a table having very small and very large values
   # and prefer to use large-number suffixing
   gt_tbl_4 <-
-    gt(tbl_big_small) %>%
+    gt(tbl_big_small) |>
     fmt_auto(lg_num_pref = "suf")
 
   # Take a snapshot of `gt_tbl_4`
@@ -60,7 +60,7 @@ test_that("fmt_auto() works correctly", {
 
   # Use `fmt_auto()` on a table having row groups and row labels
   gt_tbl_5 <-
-    gt(sp500_tbl, rowname_col = "date", groupname_col = "week") %>%
+    gt(sp500_tbl, rowname_col = "date", groupname_col = "week") |>
     fmt_auto()
 
   # Take a snapshot of `gt_tbl_5`
@@ -69,7 +69,7 @@ test_that("fmt_auto() works correctly", {
   # Use `fmt_auto()` on a table having row groups and row labels
   # and prefer to use large-number suffixing
   gt_tbl_6 <-
-    gt(sp500_tbl, rowname_col = "date", groupname_col = "week") %>%
+    gt(sp500_tbl, rowname_col = "date", groupname_col = "week") |>
     fmt_auto(lg_num_pref = "suf")
 
   # Take a snapshot of `gt_tbl_6`
@@ -77,7 +77,7 @@ test_that("fmt_auto() works correctly", {
 
   # Use `fmt_auto()` on a table having extremely large numbers
   gt_tbl_7 <-
-    gt(tbl_v_large) %>%
+    gt(tbl_v_large) |>
     fmt_auto()
 
   # Take a snapshot of `gt_tbl_7`
@@ -86,7 +86,7 @@ test_that("fmt_auto() works correctly", {
   # Use `fmt_auto()` on a table having extremely large numbers
   # and prefer to use large-number suffixing
   gt_tbl_8 <-
-    gt(tbl_v_large) %>%
+    gt(tbl_v_large) |>
     fmt_auto(lg_num_pref = "suf")
 
   # Take a snapshot of `gt_tbl_8`
@@ -94,7 +94,7 @@ test_that("fmt_auto() works correctly", {
 
   # Ensure that `fmt_auto()` can target specific columns
   gt_tbl_9 <-
-    gt(tbl) %>%
+    gt(tbl) |>
     fmt_auto(columns = c(integer, amt.usd))
 
   # Take a snapshot of `gt_tbl_9`
@@ -102,7 +102,7 @@ test_that("fmt_auto() works correctly", {
 
   # Ensure that `fmt_auto()` can target specific rows
   gt_tbl_10 <-
-    gt(tbl_v_large) %>%
+    gt(tbl_v_large) |>
     fmt_auto(rows = c(2, 4, 6, 8), lg_num_pref = "suf")
 
   # Take a snapshot of `gt_tbl_10`
