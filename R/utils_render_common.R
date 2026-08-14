6d0d6ba448e73258f88ad4054c4712cced9ef121
Richard Iannone <riannone@me.com>
2026-08-14 19:20:46 -0400

    Fix cols_merge with < > chars in cell values

--- R/utils_render_common.R
+++ R/utils_render_common.R
@@ -398,6 +398,8 @@ resolve_secondary_pattern <- function(x) {
 
     matched <- unlist(regmatches(x, m))[1]
 
+    if (is.na(matched)) break
+
     m_start <- as.integer(m[[1]])
     m_length <- attr(m[[1]], "match.length")
 
@@ -510,9 +512,20 @@ perform_col_merge <- function(data, context) {
           "i" = "Review {.arg pattern} provided to {.fn cols_merge}."
         ))
       }
+
+      has_secondary <- grepl("<<.*?>>", pattern)
+
+      if (has_secondary) {
+        glue_src_data <-
+          lapply(glue_src_data, function(vals) {
+            vals <- gsub("<", "\x01", vals, fixed = TRUE)
+            gsub(">", "\x02", vals, fixed = TRUE)
+          })
+      }
+
       glued_cols <- as.character(glue_gt(glue_src_data, pattern))
 
-      if (grepl("<<.*?>>", pattern)) {
+      if (has_secondary) {
 
         glued_cols <-
           vapply(
@@ -523,6 +536,8 @@ perform_col_merge <- function(data, context) {
           )
 
         glued_cols <- gsub("<<|>>", "", glued_cols)
+        glued_cols <- gsub("\x01", "<", glued_cols, fixed = TRUE)
+        glued_cols <- gsub("\x02", ">", glued_cols, fixed = TRUE)
       }
 
       glued_cols <- gsub(missing_val_token, "NA", glued_cols, fixed = TRUE)
